431996de6a4d5f301baa0da605b83e08c7d04ef8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-21 05:50:47 +0530

    Time: 55 ms (94.30%), Space: 14.5 MB (54.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,11 +3,13 @@ public:
     int coinChange(vector<int>& coins, int amount) {
     vector<int> need(amount+1, amount+1);
     need[0] = 0;
-    for (int c : coins)
+    for (int c : coins){
+         //cout<<" c = "<<c<<endl; 
         for (int a=c; a<=amount; a++)
-            need[a] = min(need[a], need[a-c] + 1);
-    
-    
+        {  need[a] = min(need[a], need[a-c] + 1);
+         //cout<<" need[a] = "<<need[a]<<endl; 
+        }
+    }
     return need.back() > amount ? -1 : need.back();
     }
 };
