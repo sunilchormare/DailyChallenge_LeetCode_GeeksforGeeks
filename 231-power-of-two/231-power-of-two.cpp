f75da924e0a43a401a7ccde919d5d70a06f1a236
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-21 06:10:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.61%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,8 +3,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;
+        // if(n==0) return false;
+        // while(n%2==0) n/=2;
+        // return n==1;
+        
+        
+           if(n<=0) return false;
+        return ((n&(n-1))==0);
     }
 };
