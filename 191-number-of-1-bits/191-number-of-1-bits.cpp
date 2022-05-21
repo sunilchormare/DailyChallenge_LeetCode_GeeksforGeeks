bb41b631d9933741f1d00386d24dc045250e8c33
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-21 06:17:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.26%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
          int count=0;
-            while (n!=0) {
-        n = n&(n - 1);
-        count++;
-    }
-    
-    return count;
-     
+           while (n!=0) 
+                n = n&(n - 1),count++;
+            
+            return count;
      
     }
 };
