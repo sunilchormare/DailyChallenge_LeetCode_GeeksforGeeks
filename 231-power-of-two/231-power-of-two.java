a2ac20ae17e71dd21eb47865b57b1d757fdcf128
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-21 06:07:13 +0530

    Time: 1 ms (97.42%), Space: 41.4 MB (46.52%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {   
+         //return n > 0 && NOT (n & n - 1);
+         if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1; 
+    }
+}
