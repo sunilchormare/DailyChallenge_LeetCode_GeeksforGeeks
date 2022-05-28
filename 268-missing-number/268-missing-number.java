a3b8a748f9f1bd47efa069e6337906e96c90936b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-28 06:06:41 +0530

    Time: 0 ms (100.00%), Space: 52.1 MB (8.57%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         int sum = nums.length;
+    for (int i = 0; i < nums.length; i++)
+        sum += i - nums[i];
+    return sum; 
+    }
+}
