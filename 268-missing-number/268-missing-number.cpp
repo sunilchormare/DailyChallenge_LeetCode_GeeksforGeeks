3dd69c296c7d0f5152d9a8f549b0395f06752356
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-28 06:06:14 +0530

    Time: 21 ms (75.63%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
       
-        int sum=0,sum1=0,n;
-        for(int i:nums)
-            sum+=i;
-         n=nums.size();
-         sum1=n*(n+1)/2;
-       // cout<<sum1;
-            return sum1-sum;
-        
+//         int sum=0,sum1=0,n;
+//         for(int i:nums)
+//             sum+=i;
+//          n=nums.size();
+//          sum1=n*(n+1)/2;
+//        // cout<<sum1;
+//             return sum1-sum;
+ 
+          int sum = nums.size();
+    for (int i = 0; i < nums.size(); i++)
+        sum += i - nums[i];
+    return sum;
     }
 };
