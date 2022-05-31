e39a3fbdc626fd2809d940a7a020708006960b30
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-31 06:44:25 +0530

    Time: 12 ms (85.68%), Space: 62.4 MB (76.42%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -19,9 +19,12 @@ class Solution {
         int[] res = new int[n];
         Arrays.fill(res,-1);
         int left = 0, right = 0, windowSize = 2*k+1;
-        while( right  < n ){
+       
+        while( right  < n )
+        {
             sum+=nums[right];
-            if(right - left + 1 == windowSize){
+            if(right - left + 1 == windowSize)
+            {
                 res[left+k] = (int) (sum/windowSize);
                 sum-=nums[left];
                 left++;
