bb833981aaaaebe8fc55f7490d8abd6818debaaa
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-05-31 06:07:13 +0530

    Time: 1591 ms (5.03%), Space: 76.3 MB (31.76%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -10,7 +10,7 @@ public:
 //         return (ss.size()==pow(2,k));
         
         
-          set<string> strs;
+         set<string> strs;
         for(int i=0;i+k<=s.size();i++) 
             strs.insert(s.substr(i, k));
         return strs.size()==pow(2, k);
