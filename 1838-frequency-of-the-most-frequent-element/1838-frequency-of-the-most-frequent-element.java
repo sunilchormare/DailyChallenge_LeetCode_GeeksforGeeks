a5d3a1c59c9da196f099db9d4a2e332ee35cd3fb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-01 06:22:20 +0530

    Time: 39 ms (45.40%), Space: 96.8 MB (19.44%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxFrequency(int[] A, long k) {
+       int i = 0, j;
+        Arrays.sort(A);
+        for (j = 0; j < A.length; ++j) {
+            k += A[j];
+            if (k < (long)A[j] * (j - i + 1))
+                k -= A[i++];
+        }
+        return j - i;  
+    }
+}
