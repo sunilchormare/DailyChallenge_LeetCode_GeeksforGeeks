93593987352606772f3cb6e1faca04f8f01068ae
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-08 05:59:30 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.size()==0) return 0;
+      int st=0,end=s.size()-1;
+      while(st<end){
+        if(s[st]==s[end]){
+          st++;end--;
+        }else
+        return 2;
+      }
+      return 1;
+    }
+};   
