b6d9fb06ab3b690b059f5b4d0be7563198f11980
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-12 06:11:59 +0530

    Time: 3 ms (70.75%), Space: 6.8 MB (17.92%) - LeetHub

---
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string S, char C) {
+         int n = S.size(), pos = -n;
+        vector<int> res(n, n);
+        for (int i = 0; i < n; ++i) 
+        {
+            if (S[i] == C) pos = i;
+            res[i] = i - pos;
+        }
+        
+        for (int i = pos - 1; i >= 0; --i) {
+            if (S[i] == C)  pos = i;
+            res[i] = min(res[i], pos - i);
+        }
+        return res; 
+    }
+};
