f0cc4b893667c5d7d50b90636f8d581f4c329c78
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-13 05:42:32 +0530

    Time: 6 ms (80.20%), Space: 8.6 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,11 +3,12 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
       int n = triangle.size();
     vector<int> minlen(triangle.back());
-    for (int layer = n-2; layer >= 0; layer--) // For each layer
+    for (int layer = n-2; layer >= 0; layer--)
     {
-        for (int i = 0; i <= layer; i++) // Check its every 'node'
+        
+        for (int i = 0; i <= layer; i++)
         {
-            // Find the lesser of its two children, and sum the current value in the triangle with it.
+           
             minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
         }
     }
