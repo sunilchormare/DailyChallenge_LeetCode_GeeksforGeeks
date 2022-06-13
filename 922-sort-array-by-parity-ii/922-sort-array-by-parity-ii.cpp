e2f147d1e25d7a529be3b9251734bee34992706b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-13 05:57:09 +0530

    Time: 4 ms (52.38%), Space: 55 MB (44.50%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int[] res = new int[A.length];
+        int j = 0, k = 1;
+       
+        for(int i = 0; i <A.length; i++) 
+        {
+            if(A[i] % 2 == 0)
+            {
+                res[j] = A[i];
+                j += 2;
+            }
+            else
+            {
+                res[k] = A[i];
+                k += 2;
+            }
+    }
+        return res;
+}
+}
