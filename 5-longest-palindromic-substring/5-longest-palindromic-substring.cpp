9878cdb67b9e692e165779e2c18a2c55b701904f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-16 06:00:24 +0530

    Time: 3 ms (99.74%), Space: 7.2 MB (81.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -6,18 +6,21 @@ public:
         int min_left=0;
         int max_len=1;
         int max_right=s.size()-1;
-        for (int mid=0;mid<s.size();){
+        for (int mid=0;mid<s.size();)
+        {
             int left=mid;
             int right=mid;
             while (right<max_right && s[right+1]==s[right])
                 right++; // Skip duplicate characters in the middle
             mid=right+1; //for next iter
-            while (right<max_right && left>0 && s[right+1]==s[left-1]){
+            while (right<max_right && left>0 && s[right+1]==s[left-1])
+            {
                 right++; 
                 left--;
             } // Expand the selection as long it is a palindrom
             int new_len=right-left+1; //record best palindro
-            if (new_len>max_len){ 
+            if (new_len>max_len)
+            { 
                 min_left=left; 
                 max_len=new_len; 
             }
