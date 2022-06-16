ce08d47b562bfc9060d274816d61cf85f87b027e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-16 06:03:19 +0530

    Time: 411 ms (24.26%), Space: 93.8 MB (16.48%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -12,7 +12,7 @@ int n = s.length();
       dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]); 
       
       if (dp[i][j] && (res == null || j - i + 1 > res.length()))
-        res = s.substring(i, j + 1);
+        res = s.substring(i, j+1);
     }
   }
     
