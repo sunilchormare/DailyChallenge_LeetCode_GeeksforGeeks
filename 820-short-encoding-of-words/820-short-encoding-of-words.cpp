31c45f0d0327edb91ad2d68c274d31c10bdd6814
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-20 05:43:42 +0530

    Time: 49 ms (94.06%), Space: 17.5 MB (73.27%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -6,7 +6,7 @@ public:
             for (int i = 1; i < w.size(); ++i)
                 s.erase(w.substr(i));
         int res = 0;
-        for (string w : s) res += w.size() + 1;
+        for (string w : s) res += w.size() +1;
         return res;
     }
 };
