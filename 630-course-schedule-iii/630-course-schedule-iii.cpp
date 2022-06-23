3f1a5f586d1057d3e65fc1f3aa650f423f51b393
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-23 06:06:26 +0530

    Time: 265 ms (85.25%), Space: 56.2 MB (58.52%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,14 +1,16 @@
 class Solution {
-    public int scheduleCourse(int[][] courses) {
-      Arrays.sort(courses,(a,b)->a[1]-b[1]); //Sort the courses by their deadlines (Greedy! We have to deal with courses with early deadlines first)
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
-        int time=0;
-        for (int[] c:courses) 
-        {
-            time+=c[0]; // add current course to a priority queue
-            pq.add(c[0]);
-            if (time>c[1]) time-=pq.poll(); //If time exceeds, drop the previous course which costs the most time. (That must be the best choice!)
-        }        
-        return pq.size();   
+public:
+    int scheduleCourse(vector<vector<int>>& C) {
+         sort(C.begin(), C.end(), [](auto &a, auto &b) {return a[1] < b[1];});
+        priority_queue<int> pq;
+        int total = 0;
+        for (auto &course : C) {
+            int dur = course[0], end = course[1];
+            if (dur + total <= end) 
+                total += dur, pq.push(dur);
+            else if (pq.size() && pq.top() > dur)
+                total += dur - pq.top(), pq.pop(), pq.push(dur);
+        }
+        return pq.size();
     }
-}
+};
