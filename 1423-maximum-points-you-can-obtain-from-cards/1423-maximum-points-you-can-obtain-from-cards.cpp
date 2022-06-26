6aedc29da51864ba0c2ca585f99ca35364e4e449
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-26 05:56:49 +0530

    Time: 908 ms (5.10%), Space: 42.3 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -5,13 +5,15 @@ public:
     
     for(int i=0; i<k; i++)
         sum+=arr[i];
-        cout<<"sum :" <<sum;
+        cout<<"sum :" <<sum<<endl;
         
     int maxi = sum; 
     for(int i=0; i<k; i++)
     {
         sum = sum - arr[k-i-1] + arr[n-i-1];
+        cout<<"i = "<<i<<endl<<" arr[k-i-1] : "<<arr[k-i-1]<<" arr[n-i-1] : "<<arr[n-i-1]<<" sum : "<<sum<<endl;
         maxi = max(maxi, sum);
+        cout<<" maxi : "<<maxi<<endl;
     }
     return maxi;    
     }
