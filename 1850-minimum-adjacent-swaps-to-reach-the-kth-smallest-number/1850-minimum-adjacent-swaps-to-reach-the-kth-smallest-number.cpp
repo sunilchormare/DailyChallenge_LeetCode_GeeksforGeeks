507ebb4a1be2a2bb3fe414f0974b1a338c64f743
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-27 06:22:03 +0530

    Time: 33 ms (79.63%), Space: 6.7 MB (49.43%) - LeetHub

---
+++ 1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getMinSwaps(string num, int k) {
+        string perm = num; 
+        while (k--) {
+            next_permutation(perm.begin(), perm.end()); 
+        }
+        
+        int ans = 0; 
+        for (int i = 0; i < num.size(); ++i) {
+            int ii = i+1; 
+            while (num[i] != perm[i]) {
+                ++ans; 
+                swap(perm[i], perm[ii++]); 
+            }
+        }
+        return ans;         
+    }
+};
