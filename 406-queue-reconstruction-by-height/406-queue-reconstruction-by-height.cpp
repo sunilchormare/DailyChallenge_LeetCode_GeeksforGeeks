db0fbd7379861fd85bbdb0de45b410538fe37651
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-29 06:52:20 +0530

    Time: 178 ms (56.76%), Space: 11.9 MB (80.19%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,11 +1,14 @@
 class Solution {
-    public int[][] reconstructQueue(int[][] people) {
-         Arrays.sort(people, (a, b) -> (b[0] == a[0] ? a[1] - b[1] : b[0] - a[0]));
-        
-        List<int[]> res = new ArrayList<>();
-        for (int[] p : people) {
-            res.add(p[1], p);
-        }
-        return res.toArray(new int[0][0]);
+public:
+     static bool comp(vector<int> &a, vector<int> &b) {
+        return (a[0] == b[0]) ? (a[1] < b[1]) : (a[0] > b[0]);
     }
-}
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& a) {
+        sort(a.begin(), a.end(), comp);
+        vector<vector<int>> ans;
+        for(int i = 0 ; i < a.size(); i++) {
+            ans.insert(ans.begin() + a[i][1], a[i]);
+        }
+        return ans;
+    }
+};
