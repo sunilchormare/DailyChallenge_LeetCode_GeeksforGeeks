8ee527203599af01219d2515b41b6e03f0c50965
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-29 07:40:50 +0530

    Time: 8 ms (40.00%), Space: 11.1 MB (66.72%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.cpp
+++ 969-pancake-sorting/969-pancake-sorting.cpp
@@ -1,21 +1,17 @@
 class Solution {
-    public List<Integer> pancakeSort(int[] A) {
-        List<Integer> res = new ArrayList<>();
-        for (int x = A.length, i; x > 0; --x) {
+public:
+    vector<int> pancakeSort(vector<int>& A) {
+      vector<int> res;
+        int x,i;
+        
+        for (x = A.size(); x > 0; --x) 
+        {
             for (i = 0; A[i] != x; ++i);
-            reverse(A, i + 1);
-            res.add(i + 1);
-            reverse(A, x);
-            res.add(x);
+            reverse(A.begin(), A.begin() + i + 1);
+            res.push_back(i + 1);
+            reverse(A.begin(), A.begin() + x);
+            res.push_back(x);
         }
-        return res;
+        return res;  
     }
-
-    public void reverse(int[] A, int k) {
-        for (int i = 0, j = k - 1; i < j; i++, j--) {
-            int tmp = A[i];
-            A[i] = A[j];
-            A[j] = tmp;
-        }
-    }
-}
+};
