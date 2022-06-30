7eaae47930001696a5d89aca10b22dd110c0ec79
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-06-30 09:24:00 +0530

    Time: 5 ms (66.60%), Space: 46.3 MB (17.29%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,33 +1,7 @@
 class Solution {
-public:
-    int minMoves2(vector<int>& nums) {
-//         int n=nums.size();
-//         int count=0;
-//         vector<int> a(n);
-//         for(int i=0;i<n;++i)
-//         {
-//             for(int j=0;j<n;++j)
-//             {
-//                 if(i==j) continue;
-//                 else
-//                     count+=abs(nums[j]-nums[i]);
-//                  cout<<count<<" ";
-//             }
-//             a[i]=count;
-//         }
-//          for(int i=0;i<n;++i)
-//            cout<<a[i]<<" "<<endl;
-//             int mini=0;
-//         for(int i=0;i<n;++i)
-//         {
-//             if(a[i]<mini)
-//                 mini=a[i];
-            
-//         }
-//         return mini;
-        
-        sort(nums.begin(),nums.end());
-        int i = 0, j = nums.size()-1;
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
         int count = 0;
         while(i < j){
             count += nums[j]-nums[i];
@@ -36,4 +10,4 @@ public:
         }
         return count;
     }
-};
+}
