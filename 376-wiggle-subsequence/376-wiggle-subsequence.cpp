cfbaf82ceced004a5dd3930ba0978e7aa5e55a04
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-03 06:06:30 +0530

    Time: 3 ms (69.26%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,10 +4,11 @@ public:
         if(nums.size() == 0) return 0;
     int count = 1;
     int prevDiff = 0;
-    for (int i = 1; i < nums.size(); i++) {
+    for (int i = 1; i < nums.size(); i++)
+    {
         int diff = nums[i] - nums[i-1];
-        if( (diff > 0 && prevDiff <= 0) ||
-             (diff < 0 && prevDiff >=0) ) {
+        if( (diff > 0 && prevDiff <= 0) || (diff < 0 && prevDiff >=0) ) 
+        {
             count++;
             prevDiff = diff;
         }
