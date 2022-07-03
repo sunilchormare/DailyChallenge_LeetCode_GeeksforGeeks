8c8f1553879456058a81cad1eb111eeecbd51942
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-03 06:39:19 +0530

    Time: 49 ms (42.50%), Space: 80.8 MB (74.38%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
-    vector<int> getStrongest(vector<int>& arr, int k) {
-        sort(begin(arr), end(arr));
-    int i = 0, j = arr.size() - 1;
-    int med = arr[(arr.size() - 1) / 2];
-    while (--k >= 0)
-        if (med - arr[i] > arr[j] - med)
-            ++i;  
+    public int[] getStrongest(int[] arr, int k) {
+         Arrays.sort(arr);
+    int i = 0, j = arr.length - 1, p = 0;
+    int median = arr[(arr.length - 1) / 2];
+    int[] res = new int[k];
+    while (p < k)
+        if (median - arr[i] > arr[j] - median)
+            res[p++] = arr[i++];  
         else
-            --j;
-    arr.erase(begin(arr) + i, begin(arr) + j + 1);
-    return arr;
+            res[p++] = arr[j--];      
+    return res;
     }
-};
+}
