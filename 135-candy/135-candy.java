a8bace68a632c7aed06810bb56d0dae914109862
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-04 11:18:53 +0530

    Time: 5 ms (45.91%), Space: 52.1 MB (28.32%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,47 +1,22 @@
 class Solution {
-public:
-    int candy(vector<int>& ratings) {
-//       int n=ratings.size();
-//         vector<int> left(n);
-//         vector<int> right(n);
-//          for(int i=0;i<n;++i) left[i]=1;
-//          for(int i=0;i<n;++i) right[i]=1;
-        
-//         for(int i=1;i<n;++i)
-//         {
-//           if(ratings[i]>ratings[i-1])
-//               left[i]=left[i-1]+1;
-              
-//         }
-//           for(int i=n-1;i>=0;--i)
-//         {
-//           if(ratings[i-1]>ratings[i])
-//               right[i-1]=right[i]+1;
-              
-//         }
-//         int res=0;
-//         for(int i=0;i<n;++i)
-//             res+=max(left[i],right[i]);
-        
-//         return res;        
-        
-        int size=ratings.size();
+    public int candy(int[] ratings) {
+          int size=ratings.length;
 	 if(size<=1)
 		 return size;
-	 vector<int> num(size,1);
+	 int num[]=new int[size];
+        for(int i=0;i<size;++i) num[i]=1;
 	 for (int i = 1; i < size; i++)
 		 if(ratings[i]>ratings[i-1])
 			 num[i]=num[i-1]+1;
 	 
 	 for (int i= size-1; i>0 ; i--)
 		 if(ratings[i-1]>ratings[i])
-			 num[i-1]=max(num[i]+1,num[i-1]);
+			 num[i-1]=Math.max(num[i]+1,num[i-1]);
 	 
 	 int result=0;
 	 for (int i = 0; i < size; i++)
 		 result+=num[i];
 		// cout<<num[i]<<" ";
 	 return result;
-        
     }
-};
+}
