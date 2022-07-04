42c4e5374e7d219fbcfffe6499ece801e14c764c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-04 11:15:34 +0530

    Time: 47 ms (21.19%), Space: 17.6 MB (88.22%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -30,21 +30,17 @@ public:
 		 return size;
 	 vector<int> num(size,1);
 	 for (int i = 1; i < size; i++)
-	 {
 		 if(ratings[i]>ratings[i-1])
 			 num[i]=num[i-1]+1;
-	 }
+	 
 	 for (int i= size-1; i>0 ; i--)
-	 {
 		 if(ratings[i-1]>ratings[i])
 			 num[i-1]=max(num[i]+1,num[i-1]);
-	 }
+	 
 	 int result=0;
 	 for (int i = 0; i < size; i++)
-	 {
 		 result+=num[i];
 		// cout<<num[i]<<" ";
-	 }
 	 return result;
         
     }
