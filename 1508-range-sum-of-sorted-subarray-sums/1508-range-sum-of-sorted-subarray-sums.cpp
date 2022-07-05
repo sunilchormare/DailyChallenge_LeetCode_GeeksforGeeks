bb33332d902e75aca01868c35ad22b44afb2dc83
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-05 10:39:48 +0530

    Time: 105 ms (68.70%), Space: 54.6 MB (53.69%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,40 +1,22 @@
 class Solution {
-public:
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-//BRUTE FORCE
-        //        vector<int> res;
-//         // int n=nums.size();
-//         for(int i=0;i<n;++i)
-//         {
-//             int sum=nums[i];
-//             res.push_back(sum);
-//             for(int j=i+1;j<n;++j)
-//             {
-//               sum+=nums[j];
-//               res.push_back(sum);
-//             }
-            
-//         }
-//         int ans=0;
-//         sort(res.begin(),res.end());
-//         for(int i=left-1;i<right;++i)
-//           ans+=res[i];
-            
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int index = 0;
+        int[] sums = new int[n*(n+1)/2];
         
-//         // for(int i=0;i<res.size();++i)
-//         //         cout<<res[i]<<" ";
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            for(int j = i; j < n; j++) {
+                sum += nums[j];
+                sums[index++] = sum;
+            }
+        }
         
-//       return ans%1000000007;  
-        vector<int> sums { };
-	for (int i = 0; i < n; i++) {
-		int prefixSum = 0;
-		for (int j = i; j < n; j++) {
-			prefixSum += nums[j];
-			sums.push_back(prefixSum);
-		}
-	}
-	sort(begin(sums), end(sums));
-	int res = accumulate(sums.begin() + left - 1, sums.begin() + right, 0LL) % 1000000007;
-	return res;
+        int res = 0, mod = 1000000007;
+        Arrays.sort(sums);
+        for(int i = left-1; i < right; i++) {
+            res = (res+sums[i])%mod;
+        }
+        
+        return res;
     }
-};
+}
