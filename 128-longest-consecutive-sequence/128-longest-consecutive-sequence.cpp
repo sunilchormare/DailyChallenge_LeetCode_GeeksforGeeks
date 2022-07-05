9b02fc1be14314de01dd6f05c4b1246d6061f800
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-05 10:04:11 +0530

    Time: 23 ms (87.86%), Space: 63.8 MB (78.53%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,28 +1,26 @@
 class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-      if (nums.size() == 0) {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
             return 0;
         }
-  sort(nums.begin(),nums.end());
-        
-        int longestStreak=1;
-        int currentStreak=1;
-        for(int i=1;i<nums.size();++i)
-        {
-            if(nums[i]!=nums[i-1])
-            {
-                if(nums[i]==nums[i-1]+1)
-                    currentStreak+=1;
-                else
-                {
-                   longestStreak=max(longestStreak,currentStreak);
-                   currentStreak=1;
-                 }
+
+        Arrays.sort(nums);
+
+        int longestStreak = 1;
+        int currentStreak = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                if (nums[i] == nums[i-1]+1) {
+                    currentStreak += 1;
+                }
+                else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
             }
         }
-        
-        
-        return max(longestStreak,currentStreak);
+
+        return Math.max(longestStreak, currentStreak);
     }
-};
+}
