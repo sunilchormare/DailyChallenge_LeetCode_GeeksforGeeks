08804952a66091459bd7576ed821e6cbfa140b45
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-08 10:57:36 +0530

    Time: 65 ms (45.44%), Space: 17.1 MB (52.40%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -3,7 +3,8 @@ public:
    string pushDominoes(string d) {
         d = 'L' + d + 'R';
         string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j) {
+        for (int i = 0, j = 1; j < d.length(); ++j)
+        {
             if (d[j] == '.') continue;
             int middle = j - i - 1;
             if (i > 0)
@@ -12,8 +13,8 @@ public:
                 res += string(middle, d[i]);
             else if (d[i] == 'L' && d[j] == 'R')
                 res += string(middle, '.');
-    else
-res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
+           else
+               res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
             i = j;
         }
         return res;
