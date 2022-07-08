9318f64426a9960a7720c723b91230c0369d1da7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-08 10:58:47 +0530

    Time: 28 ms (70.87%), Space: 54.3 MB (67.98%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,22 +1,33 @@
+// Complexity:
+// Time O(N)
+// Space O(N)
 class Solution {
-public:
-   string pushDominoes(string d) {
+    public String pushDominoes(String d)
+    {
         d = 'L' + d + 'R';
-        string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j)
+        StringBuilder res = new StringBuilder();
+        for (int i = 0, j = 1; j < d.length(); ++j) 
         {
-            if (d[j] == '.') continue;
+            if (d.charAt(j) == '.') continue;
             int middle = j - i - 1;
             if (i > 0)
-                res += d[i];
-            if (d[i] == d[j])
-                res += string(middle, d[i]);
-            else if (d[i] == 'L' && d[j] == 'R')
-                res += string(middle, '.');
-           else
-               res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
-            i = j;
-        }
-        return res;
-    }
-};
+                res.append(d.charAt(i));
+            if (d.charAt(i) == d.charAt(j))
+                for (int k = 0; k < middle; k++)
+                    res.append(d.charAt(i));
+            else if (d.charAt(i) == 'L' && d.charAt(j) == 'R')
+                for (int k = 0; k < middle; k++)
+                    res.append('.');
+            else {
+                for (int k = 0; k < middle / 2; k++)
+                    res.append('R');
+                if (middle % 2 == 1)
+                    res.append('.');
+                for (int k = 0; k < middle / 2; k++)
+                    res.append('L');
+                }
+              i = j;
+          }
+        return res.toString();
+     }
+}
