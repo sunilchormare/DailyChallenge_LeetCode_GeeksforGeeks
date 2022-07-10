f2d647d45211e0f2668869c11aecfddd877bff1f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-10 11:09:15 +0530

    Time: 23 ms (82.69%), Space: 43.7 MB (85.25%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
@@ -1,17 +1,15 @@
 class Solution {
-public:
-    string findLongestWord(string s, vector<string>& d) {
-      string longest = "";
-    for (string dictWord : d) {
+    public String findLongestWord(String s, List<String> d) {
+         String longest = "";
+    for (String dictWord : d) {
         int i = 0;
-        for (char c : s) 
-            if (i < dictWord.size() && c == dictWord[i]) i++;
+        for (char c : s.toCharArray()) 
+            if (i < dictWord.length() && c == dictWord.charAt(i)) i++;
 
-        if (i == dictWord.size() && dictWord.size() >= longest.size()) 
-            if (dictWord.size() > longest.size() || dictWord.compare(longest) < 0)
+        if (i == dictWord.length() && dictWord.length() >= longest.length()) 
+            if (dictWord.length() > longest.length() || dictWord.compareTo(longest) < 0)
                 longest = dictWord;
     }
     return longest;
-        
     }
-};
+}
