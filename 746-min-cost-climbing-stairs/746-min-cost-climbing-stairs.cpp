08a17ce15f33405c907110746e516748c4f71372
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-10 09:12:07 +0530

    Time: 3 ms (97.24%), Space: 13.6 MB (79.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,7 +4,7 @@ public:
         int n = cost.size();
 	int first = cost[0];
 	int second = cost[1];
-	if (n<=2) return min(first, second);
+	//if (n<=2) return min(first, second);
 	
     for (int i=2; i<n; i++) 
     {
