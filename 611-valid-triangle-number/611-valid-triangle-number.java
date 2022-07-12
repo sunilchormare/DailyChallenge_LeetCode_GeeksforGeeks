305c73a821985f293874b038ff39526bdf1c3a29
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-12 10:41:13 +0530

    Time: 40 ms (72.60%), Space: 44.1 MB (37.53%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -21,5 +21,22 @@ class Solution {
         }
         
         return count;
+        
+        //BRUTE FORCE
+ 
+        // int count = 0;
+        // for (int i = 0; i < nums.length - 2; i++) {
+        //     for (int j = i + 1; j < nums.length - 1; j++) {
+        //         for (int k = j + 1; k < nums.length; k++) {
+        //             if (nums[i] + nums[j] > nums[k] && 
+        //                 nums[i] + nums[k] > nums[j] && 
+        //                 nums[j] + nums[k] > nums[i]) {
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
     }
 }
