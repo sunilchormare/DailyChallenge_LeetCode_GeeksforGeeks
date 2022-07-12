7cad858c9ec2a82fb3f8ad859f1502d965fd408f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-12 08:31:56 +0530

    Time: 28 ms (86.34%), Space: 10 MB (66.64%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -25,7 +25,7 @@ public:
             sum += val;
         }
         if (sum % 4 != 0) return false;
-        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
+       sort(nums.begin(), nums.end(), greater<int>());
         vector<int> sidesLength(4, 0);
         return dfs(sidesLength, nums, 0, sum / 4);
     }
