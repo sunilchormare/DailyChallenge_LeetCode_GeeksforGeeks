8c818158f0d14e3bdb83c0f5a230f0a565495667
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-14 13:13:27 +0530

    Time: 1 ms (44.75%), Space: 45.6 MB (70.62%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -1,15 +1,18 @@
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-       if(head==NULL) return false;
-        ListNode *fast=head,*slow=head;
-        while(fast->next!=NULL&&fast->next->next!=NULL) {
-         fast=fast->next->next;
-         slow=slow->next;
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+         if(head==null) return false;
+        ListNode fast=head,slow=head;
+        while(fast.next!=null&&fast.next.next!=null) {
+         fast=fast.next.next;
+         slow=slow.next;
             if(slow==fast)
                 return true;
         }   
         return false;
     }
-};
-
+}
