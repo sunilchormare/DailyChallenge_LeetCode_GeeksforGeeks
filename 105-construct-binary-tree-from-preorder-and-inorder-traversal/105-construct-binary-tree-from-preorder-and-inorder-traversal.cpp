174bd6e613d50d07ba41d439b10f58a6be373260
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-14 08:18:20 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (64.49%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,40 +1,41 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-     int preIndex;
+    int preIndex;
     int inIndex;
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
-    {
-    if (preorder.size()==0|| inorder.size()==0 || preorder.size() != inorder.size()) { return NULL; }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null || preorder.length != inorder.length) { return null; }
         preIndex = 0;
         inIndex = 0;
         return build(preorder, inorder, -3001);
-        
     }
-     TreeNode* build(vector<int> &preorder, vector<int> inorder, int rootValue) {
-        if (preIndex >= preorder.size()) { return NULL; }
+    public TreeNode build(int[] preorder, int[] inorder, int rootValue) {
+        if (preIndex >= preorder.length) { return null; }
         
         if (inorder[inIndex] == rootValue) {
             inIndex++;
-            return NULL;
+            return null;
         }
         
-        TreeNode* current = new TreeNode(preorder[preIndex]);
+        TreeNode current = new TreeNode(preorder[preIndex]);
         preIndex++;
         
-        current->left = build(preorder, inorder, current->val);
-        current->right = build(preorder, inorder, rootValue);
+        current.left = build(preorder, inorder, current.val);
+        current.right = build(preorder, inorder, rootValue);
         
         return current;
     }
-};
+}
