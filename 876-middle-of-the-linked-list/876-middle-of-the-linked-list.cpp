c5518ae83231ced87ef97609d5b4f37c8364e3fe
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-17 12:08:32 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (62.37%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -1,22 +1,13 @@
-// class Solution {
-// public:
-//     ListNode* middleNode(ListNode* head) {
-//         vector<ListNode*> A = {head};
-//         while (A.back()->next != NULL)
-//             A.push_back(A.back()->next);
-//         return A[A.size() / 2];
-//     }
-// };
 
 class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            
         }
         return slow;
     }
-};
+}
