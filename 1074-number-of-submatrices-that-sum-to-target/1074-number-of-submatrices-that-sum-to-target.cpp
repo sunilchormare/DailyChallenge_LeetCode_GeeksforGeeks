72113b5fdd950f1506fe8015c66b57f078e94c89
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-18 09:23:02 +0530

    Time: 1645 ms (18.31%), Space: 274.3 MB (18.17%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,10 +1,7 @@
-// Complexity
-// Time O(mnn)
-// Space O(m)
-
 class Solution {
-  public int numSubmatrixSumTarget(int[][] A, int target) {
-        int res = 0, m = A.length, n = A[0].length;
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& A, int target) {
+         int res = 0, m = A.size(), n = A[0].size();
         for (int i = 0; i < m; i++)
             for (int j = 1; j < n; j++)
                 A[i][j] += A[i][j - 1];
@@ -14,17 +11,17 @@ class Solution {
         {
             for (int j = i; j < n; j++) 
             {
-              Map<Integer, Integer> counter = new HashMap<>();
-                counter.put(0, 1);
+              map<int,int> counter;
+                counter[0]=1;
                 int cur = 0;
                 for (int k = 0; k < m; k++) 
                 {
                     cur += A[k][j] - (i > 0 ? A[k][i - 1] : 0);
-                    res += counter.getOrDefault(cur - target, 0);
-                    counter.put(cur, counter.getOrDefault(cur, 0) + 1);
+                    res += counter[cur - target];
+                    counter[cur]=counter[cur]+ 1;
                 }
             }
         }
         return res;
     }
-}
+};
