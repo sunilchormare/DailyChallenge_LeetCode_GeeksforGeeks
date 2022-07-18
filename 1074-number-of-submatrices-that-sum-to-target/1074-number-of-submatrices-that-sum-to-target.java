3142d34ebd5a7106edbd66d528e3ac1177b44c1f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-18 09:11:53 +0530

    Time: 180 ms (84.86%), Space: 118.3 MB (5.21%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -9,12 +9,12 @@ class Solution {
             for (int j = 1; j < n; j++)
                 A[i][j] += A[i][j - 1];
       
-       
+        Map<Integer, Integer> counter = new HashMap<>();
         for (int i = 0; i < n; i++) 
         {
             for (int j = i; j < n; j++) 
             {
-             Map<Integer, Integer> counter = new HashMap<>();
+                counter.clear();
                 counter.put(0, 1);
                 int cur = 0;
                 for (int k = 0; k < m; k++) 
