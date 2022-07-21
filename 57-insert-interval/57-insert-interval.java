48ad28086f65bf6f22b3ded728963a7ba225c3ca
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-21 15:25:56 +0530

    Time: 2 ms (81.25%), Space: 47.8 MB (38.35%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,28 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res;
-        size_t i = 0;
+    
+    public int[][] insert(int[][] intervals, int[] newInterval) {
         
-        // Left part (no intersection with newInterval)
-        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-            res.push_back(intervals[i]);
-            ++i;
-        }
+         List<int[]> result = new ArrayList<>();
         
-        // newInterval part (with or without merge)
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            ++i;
-        }
-        res.push_back(newInterval);
+         for(int[] i : intervals){
+             if(newInterval == null || i[1] < newInterval[0]){
+                 result.add(i);
+             }else if(i[0] > newInterval[1]){
+                // be carefult the sequence here
+                 result.add(newInterval);
+                 result.add(i);
+                 newInterval = null;
+             }else{
+                 
+                 newInterval[0] = Math.min(newInterval[0], i[0]);//get min
+                 newInterval[1] = Math.max(newInterval[1], i[1]);//get max
+             }
+         }
         
-        // Right part (no intersection with newInterval)
-        while (i < intervals.size() && intervals[i][0] > newInterval[1]) {
-            res.push_back(intervals[i]);
-            ++i;
-        }
-        return res;
+        if(newInterval != null)
+            result.add(newInterval);
+        
+        return result.toArray(new int[result.size()][]);
     }
-};
+}
