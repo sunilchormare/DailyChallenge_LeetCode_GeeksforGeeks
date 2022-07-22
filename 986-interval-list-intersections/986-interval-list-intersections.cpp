fc6c8c4cab7c93d6c1c0dacef488b96afc8587e2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-22 14:00:34 +0530

    Time: 51 ms (50.37%), Space: 18.7 MB (43.43%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,15 +1,24 @@
-class Solution {
+class Solution 
+{
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-      vector<vector<int>> v;
-        int i=0,j=0;
-        while(i<A.size() && j<B.size()){
-            int l=max(A[i][0], B[j][0]);
-            int u=min(A[i][1], B[j][1]);
-            if(l<=u) v.push_back({l,u});
-            if(A[i][1] < B[j][1])   i++;
-            else j++;
+vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+      vector<vector<int>> res;
+     vector<int> tem(2);
+     int Aptr=0,Bptr=0;
+    while(Aptr<A.size()&&Bptr<B.size())
+    {
+        if(B[Bptr][0]<=A[Aptr][1]&&A[Aptr][0]<=B[Bptr][1])
+        {
+            tem[0]=max(A[Aptr][0],B[Bptr][0]);
+            tem[1]=min(A[Aptr][1],B[Bptr][1]);
+                res.push_back(tem);
         }
-        return v;   
+        if(A[Aptr][1]>B[Bptr][1])
+          Bptr++;
+        else
+            Aptr++;
     }
+    
+        return res;
+}
 };
