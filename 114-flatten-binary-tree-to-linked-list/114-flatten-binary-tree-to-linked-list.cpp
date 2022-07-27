122497915dc8c1ad09a6476dccee9e017cdc065a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-27 09:14:53 +0530

    Time: 7 ms (81.17%), Space: 12.8 MB (18.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -4,10 +4,12 @@ public:
     void flatten(TreeNode* root) 
     {
      if(root==NULL) return;
-     flatten(root->right);
+    flatten(root->right);
     flatten(root->left);
+    
     root->right=prev;
     root->left=NULL;
     prev=root;
+   
     }
 };
