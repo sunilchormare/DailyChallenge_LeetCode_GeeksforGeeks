1faf96433320fc5857aae39405bdcaaaa8da1b95
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-28 10:07:57 +0530

    Time: 528 ms (5.04%), Space: 54.5 MB (5.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i=0;i<s.length();++i)
         {
             hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+            System.out.println("s.charAt(i) "+ s.charAt(i));
             hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)-1);
+            System.out.println("t.charAt(i) "+ t.charAt(i));
         }
         for(char c:hm.keySet()){
             if(hm.get(c)!=0) 
