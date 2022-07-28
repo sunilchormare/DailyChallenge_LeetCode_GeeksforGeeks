4f20081468535633e8406bb144429af21b33f6af
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-28 15:04:17 +0530

    Time: 1 ms (73.27%), Space: 51.5 MB (21.55%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,8 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-         int sum = nums.length;
-    for (int i = 0; i < nums.length; i++)
-        sum += i - nums[i];
-    return sum;
+    //      int sum = nums.length;
+    // for (int i = 0; i < nums.length; i++)
+    //     sum += i - nums[i];
+    // return sum;
+        
+         int xor = 0, i = 0;
+	for (i = 0; i < nums.length; i++) {
+		xor = xor ^ i ^ nums[i];
+	}
+
+	return xor ^ i;
     }
 }
