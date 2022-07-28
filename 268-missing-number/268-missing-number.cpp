020f602c057b41fa1875cfeedb9db3fd46564310
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-28 15:01:55 +0530

    Time: 42 ms (17.75%), Space: 18 MB (63.93%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         int sum = nums.size();
-    for (int i = 0; i < nums.size(); i++)
-        sum += i - nums[i];
-    return sum;
+    //      int sum = nums.size();
+    // for (int i = 0; i < nums.size(); i++)
+    //     sum += i - nums[i];
+    // return sum;
+         int xors = 0,i = 0;
+	for (i = 0; i < nums.size(); i++) {
+		xors = xors ^ i ^ nums[i];
+	}
+
+	return xors ^ i;
     }
 };
