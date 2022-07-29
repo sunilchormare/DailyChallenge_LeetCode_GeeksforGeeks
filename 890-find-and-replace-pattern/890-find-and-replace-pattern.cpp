7bada5427c4dcab81191978124347c6560b36692
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-29 11:07:57 +0530

    Time: 4 ms (76.53%), Space: 8.1 MB (88.90%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
      bool matches(string word,string pattern)
    {
-       // vector<char> patternToWord(26,-1);
-       // vector<char> wordToPattern(26,-1);
-         map<char,int> patternToWord,wordToPattern;
+        vector<char> patternToWord(26,0);
+        vector<char> wordToPattern(26,0);
+         //map<char,int> patternToWord,wordToPattern;
        for(int index=0;index<word.size();++index)
        {
            char wordChar=word[index];
            char patternChar=pattern[index];
-           if(patternToWord[patternChar-'0']==0)
-               patternToWord[patternChar-'0']=wordChar;
-           if(wordToPattern[wordChar-'0']==0)
-               wordToPattern[wordChar-'0']=patternChar;
-           if(patternToWord[patternChar-'0']!=wordChar||wordToPattern[wordChar-'0']!=patternChar)
+           if(patternToWord[patternChar-'a']==0)
+               patternToWord[patternChar-'a']=wordChar;
+           if(wordToPattern[wordChar-'a']==0)
+               wordToPattern[wordChar-'a']=patternChar;
+           if(patternToWord[patternChar-'a']!=wordChar||wordToPattern[wordChar-'a']!=patternChar)
                return false;
        }
        return true;
