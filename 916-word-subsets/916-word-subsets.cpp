3053bd60ddf28776df547db4faa8b9f62b5acaa4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-07-30 11:14:05 +0530

    Time: 342 ms (46.07%), Space: 102.2 MB (77.08%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,27 +1,52 @@
 class Solution {
-    public List<String> wordSubsets(String[] A, String[] B) {
-        int[] count = new int[26], tmp;
+public:
+    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
+        // string pattern="";
+        // for(string s:words2)
+        //     pattern+=s;
+        // vector<string> ans;
+        // unordered_map<char,int> pm;
+        // for(char c: pattern) pm[c]++;
+        // for(int i=0;i<words1.size();++i)
+        // {
+        //     unordered_map<char,int> m1;
+        //     for(char c: words1[i])
+        //         m1[c]++;
+        //      int cc=0;
+        //       for(char i:words1[i])
+        //       {
+        //       for(char c :pattern)     
+        //          if(m1[i]==pm[c])
+        //               cc++;
+        //       }
+        //     if(cc==pattern.size())
+        //         ans.push_back(words1[i]);
+        // }
+        // return ans;
+        
+        
+          vector<int> count(26,0), tmp;
         int i;
-        for (String b : B) {
+        for (string b : B) {
             tmp = counter(b);
             for (i = 0; i < 26; ++i)
-                count[i] = Math.max(count[i], tmp[i]);
+                count[i] = max(count[i], tmp[i]);
         }
-        List<String> res = new ArrayList<>();
-        for (String a : A) {
+       vector<string> res;
+        for (string a : A) {
             tmp = counter(a);
             for (i = 0; i < 26; ++i)
                 if (tmp[i] < count[i])
                     break;
-            if (i == 26) res.add(a);
+            if (i == 26) res.push_back(a);
         }
         return res;
     }
    
-    int[] counter(String word) {
-        int[] count = new int[26];
-        for (char c : word.toCharArray()) count[c - 'a']++;
+    vector<int> counter(string word) {
+        vector<int> count(26,0);
+        for (char c : word) count[c - 'a']++;
         return count;
     }
     
-}
+};
