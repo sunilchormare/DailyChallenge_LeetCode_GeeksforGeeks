847888e451e4a923d23aa92c1f0e145fa2585f25
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-01 09:42:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N=n+m-2;
-        int r=m-1;
-        double res=1;
-        for(int i=1;i<=r;++i)
-            res=res*(N-r+i)/i;
+//         int N=n+m-2;
+//         int r=m-1;
+//         double res=1;
+//         for(int i=1;i<=r;++i)
+//             res=res*(N-r+i)/i;
         
-        return (int)res;
-        // return (dfs(0,0,m,n));
-    }
-//     int dfs()
-//     {
-        
+//         return (int)res;
+//         // return (dfs(0,0,m,n));
 //     }
+// //     int dfs()
+// //     {
+        
+// //     }
+        
+        
+        int dp[m][n];
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=1;
+                else
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];   
+    }
 };
