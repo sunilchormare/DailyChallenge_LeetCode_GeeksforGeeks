370b956eb7eb2ab9a01e6bc6b8a48eb95d167b55
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-01 15:59:16 +0530

    Time: 26 ms (98.87%), Space: 13.8 MB (98.10%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,16 +1,9 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int i=0;i<m;++i)
-        {
-            for(int j=0;j<n;++j)
-            {
-                if(i==0||j==0)
-                    dp[i][j]=1;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-    }
-}
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1]*n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j] + dp[j-1]
+        return dp[n-1]
+    
+ 
