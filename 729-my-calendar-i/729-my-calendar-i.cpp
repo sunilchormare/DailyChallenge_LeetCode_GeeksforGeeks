dcb2ad250c0373518ff17a5392e329663969f4f5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-03 09:24:12 +0530

    Time: 218 ms (29.41%), Space: 37.6 MB (87.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,16 +1,16 @@
 class MyCalendar {
-
-    private List<int[]> books = new ArrayList<>();
-    public boolean book(int start, int end) {
-        for (int[] b : books)
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[]{ start, end });
+public:
+     vector<pair<int, int>> books;
+public:    
+    bool book(int start, int end) 
+    {
+        for (pair<int, int> p : books)
+        {// cout<<max(p.first,start)<<endl;
+            if (max(p.first, start) < min(end, p.second)) 
+                return false;
+        }
+        books.push_back({start, end});
         return true;
     }
-}
+};
 
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
