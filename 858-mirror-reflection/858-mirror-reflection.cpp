71d21dc3b302a436aff69a49bf908f7fa5a1fe14
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-04 09:10:07 +0530

    Time: 5 ms (12.40%), Space: 5.9 MB (62.81%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -3,10 +3,7 @@ public:
     int mirrorReflection(int p, int q) {
       // int ext=q,reflection=p;
         while(q%2==0&&p%2==0)
-        {
-            q/=2;
-            p/=2;
-        }
+            q/=2,p/=2;
         if(q%2==0&&p%2!=0) return 0;
         if(q%2!=0&&p%2==0) return 2;
         if(q%2!=0&&p%2!=0) return 1;
