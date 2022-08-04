9eb1eb7b85e85e74dbc90021a45dd104df66b8a9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-04 09:13:35 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (98.61%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -2,8 +2,7 @@ class Solution {
     public int mirrorReflection(int p, int q) {
         while(q%2==0&&p%2==0)
         {
-            q/=2;
-            p/=2;
+            q/=2;p/=2;
         }
         if(q%2==0&&p%2!=0) return 0;
         if(q%2!=0&&p%2==0) return 2;
