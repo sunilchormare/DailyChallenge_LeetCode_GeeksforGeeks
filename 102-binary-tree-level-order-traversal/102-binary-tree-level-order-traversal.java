59fae8e61e3be4fe059e82829b73b8e6d9fc5ba0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-09 10:55:24 +0530

    Time: 1 ms (93.16%), Space: 43.2 MB (76.62%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -1,44 +1,7 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
-//         List<List<Integer>> ans=new LinkedList<List<Integer>>();
-//         Queue<TreeNode> q=new LinkedList<TreeNode>();
-//         q.offer(root);
-//         while(!q.isEmpty())
-//         {
-//             int levelNum = q.size();
-//             List<Integer> subList = new LinkedList<Integer>();
-            
-//          for(int i=0;i<levelNum;i++)
-//             {
-//                 if(q.peek().left != null) q.offer(q.peek().left);
-//                 if(q.peek().right != null) q.offer(q.peek().right);
-//                 subList.add(q.poll().val);
-//             }
-//             ans.add(subList);
-            
-//         }
-        
-        
-//         return ans;
-        
-        List<List<Integer>> res = new LinkedList<>();
-        Queue<TreeNode> q = new LinkedList<>();
+     List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> q =new LinkedList<>();
         if (root != null) q.add(root);
         
         while (!q.isEmpty()) {
