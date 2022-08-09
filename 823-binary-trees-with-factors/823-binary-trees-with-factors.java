b112a097072b273c821ec869a5fe3b94b01808aa
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-09 09:37:23 +0530

    Time: 44 ms (70.52%), Space: 42.5 MB (90.17%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -3,13 +3,15 @@ class Solution {
        long res = 0L, mod = (long)1e9 + 7;
         Arrays.sort(A);
         HashMap<Integer, Long> dp = new HashMap<>();
-        for (int i = 0; i < A.length; ++i) {
+        for (int i = 0; i < A.length; ++i) 
+        {
             dp.put(A[i], 1L);
-            for (int j = 0; j < i; ++j)
-                if (A[i] % A[j] == 0)
-                    dp.put(A[i], (dp.get(A[i]) + dp.get(A[j]) * dp.getOrDefault(A[i] / A[j], 0L)) % mod);
+    for (int j = 0; j < i; ++j)
+        if (A[i] % A[j] == 0)
+           dp.put(A[i], (dp.get(A[i]) + dp.get(A[j]) * dp.getOrDefault(A[i] / A[j], 0L)) % mod);
             res = (res + dp.get(A[i])) % mod;
         }
+        
         return (int) res; 
     }
 }
