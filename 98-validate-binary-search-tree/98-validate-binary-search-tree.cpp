183b8b563e97ac7895f32e28948bd284ea54ebac
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-11 07:11:38 +0530

    Time: 7 ms (98.53%), Space: 21.8 MB (15.21%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,14 +1,34 @@
 
 class Solution {
 public:
-    bool isValidBST(TreeNode *root,long minVal,long maxVal)
-    {
-        if(root==NULL) return true;
-        if(root->val>=maxVal || root->val<=minVal) return false;
-        return (isValidBST(root->left,minVal,root->val)&&isValidBST(root->right,root->val,maxVal));
+//     bool isValidBST(TreeNode *root,long minVal,long maxVal)
+//     {
+//         if(root==NULL) return true;
+//         if(root->val>=maxVal || root->val<=minVal) return false;
+//         return (isValidBST(root->left,minVal,root->val)&&isValidBST(root->right,root->val,maxVal));
         
-    }
+//     }
     bool isValidBST(TreeNode* root) {
-        return isValidBST(root,LONG_MIN,LONG_MAX);
+       // return isValidBST(root,LONG_MIN,LONG_MAX);
+        
+        if(root==NULL) return true;
+        stack<TreeNode*> st;
+        TreeNode *pre=NULL;
+        while(root!=NULL || !st.empty())
+        {
+            while(root!=NULL)
+            {
+                st.push(root);
+                root=root->left;
+            }
+            root=st.top();
+            st.pop();
+            if(pre!=NULL&&root->val<=pre->val) return false;
+            pre=root;
+            root=root->right;
+            
+        }
+            
+            return true;
     }
 };
