48a071611a57f836715c5f5fa4fefdb00fa7f53b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-13 07:19:56 +0530

    Time: 156 ms (67.36%), Space: 73 MB (44.45%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,27 +1,30 @@
 class Solution {
-public:
-
-vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> counts;
-        vector<int> indexes;
-        for(string word : words)
-           counts[word]++;
-        int n = s.length(), num = words.size(),len = words[0].length();;
-        if (n == 0 || num == 0) return indexes;
-        for (int i = 0; i < n - num * len + 1; i++) {
-            unordered_map<string, int> seen;
-            int j = 0;
-            for (; j < num; j++) {
-                string word = s.substr(i + j * len, len);
-                if (counts.find(word) != counts.end()) {
-                    seen[word]++;
-                    if (seen[word] > counts[word])
-                        break;
-                }
-                else break;
-            }
-            if (j == num) indexes.push_back(i);
+    public List<Integer> findSubstring(String s, String[] words) {
+     final Map<String, Integer> counts = new HashMap<>();
+        for (final String word : words) {
+            counts.put(word, counts.getOrDefault(word, 0) + 1);
         }
-        return indexes;
+        final List<Integer> indexes = new ArrayList<>();
+        final int n = s.length(), num = words.length, len = words[0].length();
+        for (int i = 0; i < n - num * len + 1; i++) {
+            final Map<String, Integer> seen = new HashMap<>();
+            int j = 0;
+            while (j < num) {
+                final String word = s.substring(i + j * len, i + (j + 1) * len);
+                if (counts.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+            if (j == num) {
+                indexes.add(i);
+            }
+        }
+        return indexes;   
+    }
 }
-};
