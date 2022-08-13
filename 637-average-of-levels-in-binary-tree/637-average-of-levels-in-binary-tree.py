37c208451a8726dfae48dddabb7db96abb891d71
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-13 07:37:55 +0530

    Time: 2 ms (98.52%), Space: 43.9 MB (88.87%) - LeetHub

---
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            double sum=0;
+            for(int i=0;i<size;++i){
+                TreeNode node=q.poll();
+                sum+=node.val;
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+            }
+            
+            res.add(sum/size);
+        }
+        return res;
+    }
+}
