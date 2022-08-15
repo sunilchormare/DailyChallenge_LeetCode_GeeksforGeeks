5ee025120876d183aebe06c445270d86198246e2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-15 07:23:37 +0530

    Time: 17 ms (17.92%), Space: 46.1 MB (50.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,31 +1,30 @@
 class Solution {
-public:
-    int romanToInt(string s) {
+    public int romanToInt(String s) {
           int currHighest = 0;
         int ans = 0;
         
-        unordered_map<char, int> mp;
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
+        Map<Character, Integer> mp=new HashMap<>();
+        mp.put('I',1);
+        mp.put('V',5);
+        mp.put('X',10);
+        mp.put('L',50);
+        mp.put('C',100);
+        mp.put('D',500);
+        mp.put('M',1000);
         
         int n = s.length();
         for(int i=n-1; i>=0; i--) 
         {
-            if(mp[s[i]] < currHighest) 
+            if(mp.get(s.charAt(i)) < currHighest) 
             {
-                ans -= mp[s[i]];
+                ans -= mp.get(s.charAt(i));
             } 
             else 
             {
-                currHighest = mp[s[i]];
+                currHighest = mp.get(s.charAt(i));
                 ans += currHighest;
             }
         }
         return ans;
     }
-};
+}
