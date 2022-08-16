6f734a81ee567982558f8a18322f2ab0e2f702ff
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-16 10:18:05 +0530

    Time: 181 ms (48.56%), Space: 14.1 MB (59.00%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,13 +1,11 @@
-class Solution {
-public:
-    int firstUniqChar(string s) {
-        if(s.length()==1) return 0;
-        unordered_map<char,int> mp;
-        for(auto i:s) mp[i]++;
-        for(int i=0;i<s.size();++i)
-            if(mp[s[i]]==1)
-                return i; 
-          return -1;  
-     
-    }
-};
+class Solution:
+    def firstUniqChar(self, s):
+        d = {}
+        seen = set()
+        for idx, c in enumerate(s):
+            if c not in seen:
+                d[c] = idx
+                seen.add(c)
+            elif c in d:
+                del d[c]
+        return min(d.values()) if d else -1
