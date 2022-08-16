4136497a839b094cd803882bae0d0fd647ebd569
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-16 10:45:17 +0530

    Time: 4 ms (67.85%), Space: 12 MB (66.60%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -1,52 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-         recursion(root,0,res);
-      return res;
-    
-        }
-    
-    public void recursion(TreeNode root,int level,List<Integer> res)
-    { 
-        if(root==null) return;
-        if(res.size()==level) res.add(root.val);
-        recursion(root.right,level+1,res);
-        recursion(root.left,level+1,res);
-        
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helper(root,0,res);
+        return res;
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    void helper(TreeNode *root,int level,vector<int> &res)
+    {
+        if(root==NULL) return;
+        if(res.size()==level) res.push_back(root->val);
+        helper(root->right,level+1,res);
+        helper(root->left,level+1,res);
+            
+    }
+};
