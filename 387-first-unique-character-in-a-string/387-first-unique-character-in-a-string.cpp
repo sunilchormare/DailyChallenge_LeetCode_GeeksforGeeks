5a047bcac7cc036ba78dc7095affffeaab59b32c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-16 09:58:01 +0530

    Time: 40 ms (71.73%), Space: 10.7 MB (43.91%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -5,11 +5,10 @@ public:
         unordered_map<char,int> mp;
         for(auto i:s) mp[i]++;
         for(int i=0;i<s.size();++i)
-        {
             if(mp[s[i]]==1)
-                return i;
-        }
-            
+                return i; 
           return -1;  
+        
+        
     }
 };
