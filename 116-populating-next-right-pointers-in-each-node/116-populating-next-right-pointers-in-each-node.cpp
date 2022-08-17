b93c547b9517b8676ffdc2d4238d9303f425778f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-17 10:13:24 +0530

    Time: 28 ms (63.97%), Space: 16.8 MB (70.41%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -8,7 +8,8 @@ public:
         root->left->next = root->right;
         if (root->next)
           root->right->next = root->next->left;
-        connect(root->left);
+        
+          connect(root->left);
         connect(root->right);
       }
     }
