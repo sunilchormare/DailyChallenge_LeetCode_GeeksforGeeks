f3fbdb230e3a61322f732bbf12c578475ccfb0b2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-18 09:31:20 +0530

    Time: 280 ms (72.33%), Space: 77.6 MB (87.22%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    public int minSetSize(int[] arr) 
-    {
-        int n = arr.length;
-        HashMap<Integer, Integer> cnt = new HashMap<>();
-        for (int x : arr) cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+public:
+    int minSetSize(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, int> cnt;
+        for (int x : arr) ++cnt[x];
 
-        int[] counting = new int[n + 1];
-        for (int freq : cnt.values()) ++counting[freq];
+        vector<int> counting(n + 1);
+        for (auto [_, freq] : cnt) 
+            ++counting[freq];
 
         int ans = 0, removed = 0, half = n / 2, freq = n;
         while (removed < half) {
             ans += 1;
-            while (counting[freq] == 0) --freq;
+            while (counting[freq] == 0) 
+                --freq;
             removed += freq;
             --counting[freq];
         }
         return ans;
     }
-}
+};
