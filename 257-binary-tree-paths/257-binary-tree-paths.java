4ae1c291defd8a74b02c7f6f0ca2e710eb4e2960
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-20 10:53:38 +0530

    Time: 17 ms (14.63%), Space: 43.5 MB (29.40%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -1,13 +1,39 @@
 
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-    List<String> answer = new ArrayList<String>();
-    if (root != null) searchBT(root, "", answer);
-    return answer;
+    // List<String> answer = new ArrayList<String>();
+    // if (root != null) searchBT(root, "", answer);
+    // return answer;
+        List<String> res=new ArrayList<>();
+        Stack<TreeNode> nodes=new Stack<>();
+        Stack<String> path=new Stack<>();
+        nodes.push(root);
+         if(root==null) return res;
+        path.push("");
+        while(!nodes.isEmpty())
+        {
+            TreeNode node=nodes.pop();
+            String curStr=path.pop();
+            if(node.left==null&&node.right==null) 
+                res.add(curStr+node.val);
+            if(node.left!=null) {
+                nodes.push(node.left);
+                path.push(curStr+node.val+"->");
+            }
+            if(node.right!=null)
+            {
+                nodes.push(node.right);
+                path.push(curStr+node.val+"->");
+            }           
+        }           
+        return res;
 }
-private void searchBT(TreeNode root, String path, List<String> answer) {
-    if (root.left == null && root.right == null) answer.add(path + root.val);
-    if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
-    if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
-}
+// private void searchBT(TreeNode root, String path, List<String> answer)
+// {
+//     if (root.left == null && root.right == null) answer.add(path + root.val);
+   
+//     if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
+    
+//     if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
+//  }
 }
