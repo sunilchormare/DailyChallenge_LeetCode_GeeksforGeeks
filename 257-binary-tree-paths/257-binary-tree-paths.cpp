d8300981264da05f850743c5c493d9d1351e62b2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-20 10:38:05 +0530

    Time: 4 ms (76.30%), Space: 12.1 MB (98.78%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
-    void paths(TreeNode* root,string path,vector<string> &res)
-    {
-    if (root->left == NULL && root->right == NULL) res.push_back(path + to_string(root->val));
-    if (root->left != NULL) paths(root->left, path + to_string(root->val) + "->", res);
-    if (root->right != NULL) paths(root->right, path + to_string(root->val) + "->", res);
+//     void paths(TreeNode* root,string path,vector<string> &res)
+//     {
+//     if (root->left == NULL && root->right == NULL) res.push_back(path + to_string(root->val));
+//     if (root->left != NULL) paths(root->left, path + to_string(root->val) + "->", res);
+//     if (root->right != NULL) paths(root->right, path + to_string(root->val) + "->", res);
         
-    }
+//     }
     
     vector<string> binaryTreePaths(TreeNode* root) {
-      vector<string> res;
-      if (root != NULL) paths(root,"",res);
-      return res;
+      // vector<string> res;
+      // if (root != NULL) paths(root,"",res);
+      // return res;
+        
+         vector<string> list;
+    stack<TreeNode*> sNode;
+    stack<string> sStr;
+    
+    if(root==NULL) return list;
+    sNode.push(root);
+    sStr.push("");
+    while(!sNode.empty()) {
+        TreeNode* curNode=sNode.top(); sNode.pop();
+        string curStr=sStr.top();sStr.pop();
+        
+        if(curNode->left==NULL && curNode->right==NULL) list.push_back(curStr+to_string(curNode->val));
+        if(curNode->left!=NULL) {
+            sNode.push(curNode->left);
+            sStr.push(curStr+to_string(curNode->val)+"->");
+        }
+        if(curNode->right!=NULL) {
+            sNode.push(curNode->right);
+            sStr.push(curStr+to_string(curNode->val)+"->");
+        }
+    }
+    return list;
     }
 };
