7284d9651ba824d97fdb776f0e50470ffb82e9e0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-20 09:24:37 +0530

    Time: 3 ms (99.16%), Space: 49.2 MB (33.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,17 +1,20 @@
 class Solution {
-public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
         if (startFuel >= target) return 0;
-        priority_queue<int> queue;
-        int i = 0, n = stations.size(), stops = 0, maxDistance = startFuel;
-        while (maxDistance < target) 
+        Queue<Integer> queue = new PriorityQueue<>((a,b) -> b-a);
+        int i = 0, n = stations.length, stops = 0, maxDistance = startFuel;
+        while (maxDistance < target)
         {
-            while (i < n && stations[i][0] <= maxDistance)
-                queue.push(stations[i++][1]);
-            if (queue.empty()) return -1;
-            maxDistance += queue.top(); queue.pop();
+            while (i < n && stations[i][0] <= maxDistance) {
+                queue.offer(stations[i++][1]);
+            }
+            
+            if (queue.isEmpty()) return -1;
+            
+            maxDistance += queue.poll();
+            
             stops++;
         }
         return stops;
     }
-};
+}
