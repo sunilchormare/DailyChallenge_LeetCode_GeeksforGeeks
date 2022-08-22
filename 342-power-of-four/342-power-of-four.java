703dc5813281df2faaaf92e1b4d1476e2aa35d4b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-22 15:05:55 +0530

    Time: 2 ms (65.34%), Space: 41.2 MB (58.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,8 +1,10 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-          if(n>1) 
-            while(n%4==0) 
-                n /= 4;
-      return n==1;  
+    public boolean isPowerOfFour(int num) {
+      //     if(n>1) 
+      //       while(n%4==0) 
+      //           n /= 4;
+      // return n==1;  
+        
+        return (num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0);
     }
 }
