cc05d1d4de0678a2ce7c051a5c7b402644ca5de8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-25 07:21:55 +0530

    Time: 15 ms (84.13%), Space: 8.7 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> a(26,0);
+          cout<<a[magazine[0]-'a'];
         for(int i=0;i<magazine.size();++i)
             a[magazine[i]-'a']++;
         for(int i=0;i<ransomNote.size();++i)
              if(--a[ransomNote[i]-'a']<0)
                  return false;
         return true;
+      
     }
 };
