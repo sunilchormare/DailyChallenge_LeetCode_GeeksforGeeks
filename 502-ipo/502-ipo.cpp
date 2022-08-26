f5b068341e3dd50de83387e310c9e1ee33f7e71e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-26 09:19:09 +0530

    Time: 335 ms (62.50%), Space: 84.3 MB (28.96%) - LeetHub

--- 502-ipo/502-ipo.cpp
+++ 502-ipo/502-ipo.cpp
@@ -13,13 +13,17 @@ public:
     int findMaximizedCapital(int k, int W, vector<int>& Profits, vector<int>& Capital) {
         priority_queue<pair<int, int>, vector<pair<int, int>>, CapCmp> cap;
         priority_queue<pair<int, int>, vector<pair<int, int>>, ProCmp> pro;
-        for (int i = 0; i < Profits.size(); ++i) cap.push({ Capital[i], Profits[i] });
+        for (int i = 0; i < Profits.size(); ++i) 
+            cap.push({ Capital[i], Profits[i] });
+        
         while (k--) {
-            while (cap.size() && cap.top().first <= W) {
+            while (cap.size() && cap.top().first <= W) 
+            {
                 pro.push(cap.top());
                 cap.pop();
             }
-            if (pro.empty()) return W;
+            if (pro.empty()) 
+                return W;
             W += pro.top().second;
             pro.pop();
         }
