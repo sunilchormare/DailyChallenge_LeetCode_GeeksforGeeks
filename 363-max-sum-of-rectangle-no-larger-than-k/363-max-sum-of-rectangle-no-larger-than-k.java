4ff33c1c92bb33dfaee53ebb173f57cdb02653de
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-27 09:33:55 +0530

    Time: 768 ms (35.71%), Space: 117.3 MB (44.64%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
@@ -3,19 +3,22 @@ class Solution {
         if(matrix.length == 0) return 0;
     int m = matrix.length, n = matrix[0].length;
     int result = Integer.MIN_VALUE;
-    for(int left = 0; left < n; left++){
+    for(int left = 0; left < n; left++)
+    {
         int[] sums = new int[m];
-        for(int right = left; right < n; right++){
-            for(int i = 0; i < m; i++){
+        for(int right = left; right < n; right++)
+        {
+            for(int i = 0; i < m; i++) {
                 sums[i] += matrix[i][right];
             }
             TreeSet<Integer> set = new TreeSet<Integer>();
             set.add(0);
             int currSum = 0;
-            for(int sum : sums){
+            for(int sum : sums) {
                 currSum += sum;
                 Integer num = set.ceiling(currSum - k);
-                if(num != null) result = Math.max( result, currSum - num );
+                if(num != null)
+                    result = Math.max( result, currSum - num );
                 set.add(currSum);
             }
         }
