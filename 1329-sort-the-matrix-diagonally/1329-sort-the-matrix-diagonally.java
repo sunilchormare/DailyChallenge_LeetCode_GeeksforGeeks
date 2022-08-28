035fdc4e9cacc9bd44ae5eceadc18fe50e0ad5a5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-28 08:02:38 +0530

    Time: 15 ms (69.45%), Space: 9.5 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int[][] diagonalSort(int[][] mat) {
-         int m=mat.length;
-        int n=mat[0].length;
-        HashMap<Integer,PriorityQueue<Integer>> map=new HashMap<>();
-        for(int i=0;i<m;++i){
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        unordered_map<int,priority_queue<int,vector<int>, greater<int>>> map;
+        for(int i=0;i<m;++i)
             for(int j=0;j<n;++j)
-            {
-                map.putIfAbsent(i-j,new PriorityQueue<>());
-                map.get(i - j).add(mat[i][j]);
-            }
-        }
+                map[i-j].push(mat[i][j]);
+            
 
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j)
-            { 
-               mat[i][j] = map.get(i - j).poll();
+            {
+                mat[i][j]=map[i-j].top(); 
+                map[i-j].pop();
             }
         }
         return mat;
     }
-}
+};
