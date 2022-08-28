0f8d414789b49b992cbdaeb8b6c48c9721229c91
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-28 07:35:05 +0530

    Time: 17 ms (57.89%), Space: 9.5 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -5,18 +5,13 @@ public:
         int n=mat[0].size();
         unordered_map<int,priority_queue<int,vector<int>, greater<int>>> map;
         for(int i=0;i<m;++i)
-        {
             for(int j=0;j<n;++j)
-            {
                 map[i-j].push(mat[i][j]);
-            }
-        }
-        for(int i=0;i<m;++i)
-        {
+            
+
+        for(int i=0;i<m;++i){
             for(int j=0;j<n;++j)
-            {
-                mat[i][j]=map[i-j].top(); map[i-j].pop();
-            }
+            {  mat[i][j]=map[i-j].top(); map[i-j].pop();}
         }
         return mat;
     }
