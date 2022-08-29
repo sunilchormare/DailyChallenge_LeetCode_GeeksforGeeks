a6a8ce531887a7d7e3f2226d3d6adbcf9d4c9b36
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-29 08:37:19 +0530

    Time: 3 ms (69.68%), Space: 41.6 MB (54.33%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -1,15 +1,15 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        String ans="";
         int fact=1;
         List<Integer> num=new ArrayList<>();
         for(int i=1;i<n;++i)
         {
             fact=fact*i;
-            num.add(i);
+          num.add(i);    
         }
         num.add(n);
-        k=k-1;
+        String ans="";
+      k=k-1;
         while(true)
         {
             ans=ans+num.get(k/fact);
@@ -20,6 +20,7 @@ class Solution {
             fact=fact/num.size();
             
         }
+            
         
         return ans;
     }
