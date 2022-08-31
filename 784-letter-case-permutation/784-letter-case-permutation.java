def802d2c039f61e8d27f7e3c525838adcc623bc
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-31 09:27:22 +0530

    Time: 1 ms (99.85%), Space: 42.9 MB (95.14%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        if (s == null) {
+            return new LinkedList<>();
+        }
+        
+        List<String> res = new LinkedList<>();
+        helper(s.toCharArray(), res, 0);
+        return res;
+    } 
+    public void helper(char[] chs, List<String> res, int pos) {
+        if (pos == chs.length) {
+            res.add(new String(chs));
+            return;
+        }
+        if (chs[pos] >= '0' && chs[pos] <= '9') {
+            helper(chs, res, pos + 1);
+            return;
+        }
+        
+        chs[pos] = Character.toLowerCase(chs[pos]);
+        helper(chs, res, pos + 1);
+        
+        chs[pos] = Character.toUpperCase(chs[pos]);
+        helper(chs, res, pos + 1);
+    }
+}
