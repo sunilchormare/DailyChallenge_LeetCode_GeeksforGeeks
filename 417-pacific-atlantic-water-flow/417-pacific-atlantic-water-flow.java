45ef6843d27ff36e080438f28abaccb0a379cb59
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-08-31 08:37:29 +0530

    Time: 1032 ms (5.02%), Space: 144.8 MB (5.60%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -4,26 +4,23 @@ class Solution {
     HashSet<Long> visited = new HashSet<>();
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-
         this.grid = heights;
         idk = Math.max(heights.length, heights[0].length);
         List<List<Integer>> res = new ArrayList<>();
-
         for(int i=0;i<heights.length;i++)
             for(int j=0;j<heights[0].length;j++){
-
-                check(i, j, Integer.MAX_VALUE);
+                dfs(i, j, Integer.MAX_VALUE);
                 if(ocean[0] && ocean[1])
                     res.add(new ArrayList<>(Arrays.asList(i,j)));
 
-                ocean[0]=false; ocean[1]=false; // reset global variables.
+                ocean[0]=false; ocean[1]=false; 
                 visited = new HashSet<>();
             }
 
         return res;
     }
 
-    void check(int i, int j, int prev){
+    void dfs(int i, int j, int prev){
         if(i==-1||j==-1){  // pacific
             ocean[0] = true; return;
         }
@@ -36,7 +33,7 @@ class Solution {
 
         visited.add(i*idk+j);
         prev = grid[i][j];
-        check(i-1,j,prev); check(i+1,j,prev);
-        check(i,j-1,prev); check(i, j+1,prev);
+        dfs(i-1,j,prev); dfs(i+1,j,prev);
+        dfs(i,j-1,prev); dfs(i, j+1,prev);
     }
 }
