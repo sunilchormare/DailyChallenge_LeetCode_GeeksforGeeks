f15989e8c11174a88fe20ad947f397470dfd785f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-01 08:29:41 +0530

    Time: 2 ms (100.00%), Space: 50.3 MB (98.43%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -1,14 +1,14 @@
+
 class Solution {
-public:
-    int goodNodes(TreeNode* root) { 
-        return dfs(root,-10000);;
+    public int goodNodes(TreeNode root) {
+        return dfs(root,-10000);
     }
-    int dfs(TreeNode* root,int maxVal)
+    public int dfs(TreeNode root,int maxVal)
     {
-       if (root == NULL) return 0;
-        int res = root->val >= maxVal ? 1 : 0;
-        res += dfs(root->left,max(maxVal,root->val));
-        res += dfs(root->right,max(maxVal, root->val));
+        if(root==null) return 0;
+        int res=(root.val>=maxVal)?1:0;
+        res+=dfs(root.left,Math.max(maxVal,root.val));
+        res+=dfs(root.right,Math.max(maxVal,root.val));
         return res;
     }
-};
+}
