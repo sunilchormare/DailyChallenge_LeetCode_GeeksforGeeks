6ee02c3f3937acbc1e0da720ed403cafaddbebbb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-03 09:47:15 +0530

    Time: 4 ms (65.31%), Space: 43.2 MB (41.56%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,23 +1,21 @@
 class Solution {
-public:
-    void dfs(int num,int n,int k,vector<int>&res)
+    void dfs(int num,int n,int k,List<Integer> res)
     {
         if(n==0)
         {
-            res.push_back(num);
+            res.add(num);
             return;
-            
         }
         int last_digit=num%10;
         if(last_digit>=k) dfs(num*10+last_digit-k,n-1,k,res);
-        if(k>0&&last_digit+k<10) dfs(num*10+last_digit+k,n-1,k,res);
-        
+        if(k>0&&last_digit+k<10) dfs(num*10+last_digit+k,n-1,k,res);  
     }
-    vector<int> numsSameConsecDiff(int n, int k) {
-       vector<int> res;
-        
-        for(int d=1;d<10;++d)
-            dfs(d,n-1,k,res);
-        return res;
-      }
-};
+    public int[] numsSameConsecDiff(int n, int k) {
+    List<Integer> res=new ArrayList<>();
+    for(int d=1;d<10;++d)
+    {
+      dfs(d,n-1,k,res);
+    }
+    return res.stream().mapToInt(i->i).toArray();
+    }
+}
