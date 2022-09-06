552e85cb8bd63147066112c287a8f83c27e7a49b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-06 06:51:51 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (97.00%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,14 +1,19 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        low = 0;
-        high = len(nums) - 1
-        while(low <= high):
-            mid = low + (high - low) // 2
-            if(nums[mid] == target):
-                return mid
-            
-            elif(nums[mid] < target):
-                low = mid + 1
-            else:
-                high = mid - 1
-        return -1
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
