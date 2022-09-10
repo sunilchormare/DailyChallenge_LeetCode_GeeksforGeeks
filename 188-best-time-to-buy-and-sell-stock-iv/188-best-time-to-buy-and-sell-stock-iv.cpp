7091702f516049d00fe557dabeb8b5a29de3cff8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-10 08:44:33 +0530

    Time: 11 ms (29.12%), Space: 42.6 MB (51.53%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,26 @@
 class Solution {
-public:
-    vector<vector<vector<int>>> mem;
-    int recursion(vector<int> &prices,int n,int k,int pos,bool brought)
-    {
-        if(k==0||pos>=n) return 0;
-        if(mem[brought][k][pos]!=-1)
-            return mem[brought][k][pos];
-        int sum=recursion(prices,n,k,pos+1,brought);
-        if(brought)
-            sum=max(sum,recursion(prices,n,k-1,pos+1,false)+prices[pos]);
-        else
-            sum=max(sum,recursion(prices,n,k,pos+1,true)-prices[pos]);
-    mem[brought][k][pos]=sum;
-    return sum;
-        
+   public int maxProfit(int k, int[] prices) {
+	int n = prices.length;
+	if (n <= 1)
+		return 0;
+
+	if (k >=  n/2) {
+		int maxPro = 0;
+		for (int i = 1; i < n; i++) {
+			if (prices[i] > prices[i-1])
+				maxPro += prices[i] - prices[i-1];
+		}
+		return maxPro;
+	}
+	
+    int[][] dp = new int[k+1][n];
+    for (int i = 1; i <= k; i++) {
+    	int localMax = dp[i-1][0] - prices[0];
+    	for (int j = 1; j < n; j++) {
+    		dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
+    		localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
+    	}
     }
-    int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        mem.resize(2,vector<vector<int>>(k+1,vector<int>(prices.size(),-1)));
-        return recursion(prices,n,k,0,false);
-    }
-};
+    return dp[k][n-1];
+}
+}
