de163da703201185e568a0cb87adeed39aa7388f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-10 09:01:43 +0530

    Time: 315 ms (5.09%), Space: 59.6 MB (66.18%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,7 +1,8 @@
 class Solution {
-    public int peakIndexInMountainArray(int[] A) {
-      int i = 0;
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int i = 0;
         while (A[i] < A[i+1]) i++;
         return i;
     }
-}
+};
