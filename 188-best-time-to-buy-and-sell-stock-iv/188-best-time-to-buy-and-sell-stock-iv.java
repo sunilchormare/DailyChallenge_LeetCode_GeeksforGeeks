376024f1817c7c3fd42e6fef203a7b0319f98751
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-10 08:43:56 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (88.06%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,8 +3,7 @@ class Solution {
 	int n = prices.length;
 	if (n <= 1)
 		return 0;
-	
-	//if k >= n/2, then you can make maximum number of transactions.
+
 	if (k >=  n/2) {
 		int maxPro = 0;
 		for (int i = 1; i < n; i++) {
