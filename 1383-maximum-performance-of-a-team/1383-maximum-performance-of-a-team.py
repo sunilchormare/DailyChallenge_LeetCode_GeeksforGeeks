4e262793f415f82eb21bd24cb3b9db65a0eea722
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-11 07:40:18 +0530

    Time: 865 ms (16.04%), Space: 29.6 MB (83.54%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -1,21 +1,11 @@
-class Solution {
-public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-         vector<pair<int, int>> ess(n);
-        for (int i = 0; i < n; ++i)
-            ess[i] = {efficiency[i], speed[i]};
-        sort(rbegin(ess), rend(ess));
-        long sumS = 0, res = 0;
-        priority_queue <int, vector<int>, greater<int>> pq; //min heap
-        for(auto& [e, s]: ess){
-            pq.emplace(s);
-            sumS += s;
-            if (pq.size() > k) {
-                sumS -= pq.top();
-                pq.pop();
-            }
-            res = max(res, sumS * e);
-        }
-        return res % (int)(1e9+7);
-    }
-};
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        h = []
+        res = sSum = 0
+        for e, s in sorted(zip(efficiency, speed), reverse=1):
+            heapq.heappush(h, s)
+            sSum += s
+            if len(h) > k:
+                sSum -= heapq.heappop(h)
+            res = max(res, sSum * e)
+        return res % (10**9 + 7)
