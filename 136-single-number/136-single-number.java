72087337edc3b6541d11cce6a33ceb112735cbe2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-13 08:36:41 +0530

    Time: 19 ms (92.19%), Space: 17 MB (53.68%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,17 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-         int ans=0;
-        for(int a: nums)
-            ans=ans^a;
-        return ans;
+public:
+    int singleNumber(vector<int>& nums) {
+        //Time: O(n) Space:O(n)
+        // unordered_map<int,int> c;
+        // for(auto i:nums) c[i]++;
+        // for(int i=0;i<nums.size();++i)
+        //     if(c[nums[i]]==1)
+        //         return nums[i];
+        // return 0;    
+        
+        int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;  
     }
-}
+};
