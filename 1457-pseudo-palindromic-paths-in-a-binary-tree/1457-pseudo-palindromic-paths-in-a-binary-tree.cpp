623ee100a7a20ed2c83017a28ec2b5450dd57565
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-14 09:32:28 +0530

    Time: 705 ms (33.37%), Space: 180.3 MB (71.43%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+    return dfs(root,0);
+    }
+      int dfs(TreeNode *root, int count) {
+        if (root ==  NULL) return 0;
+        count ^= 1 << (root->val - 1);
+        int res = dfs(root->left, count) + dfs(root->right, count);
+        if (root->left == root->right && (count & (count - 1)) == 0) res++;
+        return res;
+    }
+};
