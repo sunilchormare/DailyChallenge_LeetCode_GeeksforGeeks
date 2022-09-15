6d9c4e4aebd2ed2f50fbc044d3d9edec015caf66
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-15 07:57:46 +0530

    Time: 429 ms (90.87%), Space: 146.5 MB (35.65%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,19 +1,34 @@
 class Solution {
-    public int[] findOriginalArray(int[] A) {
-         int n = A.length, i = 0;
-        if (n % 2 == 1) return new int[0];
-        int[] res = new int[n / 2];
-        Map<Integer, Integer> count = new TreeMap<>();
-        for (int a : A)
-            count.put(a, count.getOrDefault(a, 0) + 1);
-        for (int x : count.keySet()) {
-            if (count.get(x) > count.getOrDefault(x + x, 0))
-                return new int[0];
-            for (int j = 0; j < count.get(x); ++j) {
-                res[i++] = x;
-                count.put(x + x, count.get(x + x) - 1);
-            }
+public:
+    vector<int> findOriginalArray(vector<int>& A) {
+//         set<int> s;
+//         vector<int> res;
+//         int n=A.size();
+//         for(int i=0;i<n;++i)
+//         {
+//             for(int j=0;j<n;++j)
+//             {
+//                 if(i!=j&&nums[i]*2==nums[j])
+//                     s.insert(nums[i]);break;
+//             }
+//         }
+//         for(auto i:s) cout<<i<<" ";
+//         //cout<<s.size();
+        
+//         return res;
+         if (A.size() % 2) return {};
+        unordered_map<int, int> c;
+        for (int a : A) c[a]++;
+        vector<int> keys;
+        for (auto it : c)
+            keys.push_back(it.first);
+        sort(keys.begin(), keys.end(), [](int i, int j) {return abs(i) < abs(j);});
+        vector<int> res;
+        for (int x : keys) {
+            if (c[x] > c[2 * x]) return {};
+            for (int i = 0; i < c[x]; ++i, c[2 * x]--)
+                res.push_back(x);
         }
         return res;
     }
-}
+};
