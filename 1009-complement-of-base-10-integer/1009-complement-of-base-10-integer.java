44bf3b1416a6a92d3caa09c75aa2efe18e203933
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-15 08:38:34 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (88.76%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,8 +1,7 @@
 class Solution {
-public:
-    int bitwiseComplement(int N) {
-         int X = 1;
+    public int bitwiseComplement(int N) {
+            int X = 1;
         while (N > X) X = X * 2 + 1;
         return X - N;
     }
-};
+}
