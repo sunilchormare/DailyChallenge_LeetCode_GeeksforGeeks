c035f69f1ea545a14540c88d02574788f489d9cb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-17 10:52:27 +0530

    Time: 625 ms (24.63%), Space: 70.1 MB (50.46%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,20 +1,39 @@
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         vector<vector<int>> res(k);
+//         vector<pair<int,pair<int,int>>> v;
+//         int dist;
+//         for(auto it:points)
+//         {
+//             dist=it[0]*it[0]+it[1]*it[1];
+//             v.push_back({dist,{it[0],it[1]}});
+//         }
+//         sort(v.begin(),v.end());
+//         for(int i=0;i<k;++i)
+//         {
+//             res[i].push_back(v[i].second.first);
+//             res[i].push_back(v[i].second.second);
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> res(k);
-        vector<pair<int,pair<int,int>>> v;
-        int dist;
-        for(auto it:points)
-        {
-            dist=it[0]*it[0]+it[1]*it[1];
-            v.push_back({dist,{it[0],it[1]}});
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<vector<int>, vector<vector<int>>, compare> pq(points.begin(), points.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < K; i++) {
+            ans.push_back(pq.top());
+            pq.pop();
         }
-        sort(v.begin(),v.end());
-        for(int i=0;i<k;++i)
-        {
-            res[i].push_back(v[i].second.first);
-            res[i].push_back(v[i].second.second);
-        }
-        return res;
+        return ans;
     }
+private:
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] > q[0] * q[0] + q[1] * q[1];
+        }
+    };
 };
