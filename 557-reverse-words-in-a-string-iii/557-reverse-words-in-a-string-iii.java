0ae4cf3884a7af9be0c0db65376a6e3c9a9dfa73
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-22 06:07:39 +0530

    Time: 5 ms (94.47%), Space: 48.8 MB (81.12%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -4,7 +4,8 @@ public class Solution {
         for (int i = 0; i < ca.length; i++) {
             if (ca[i] != ' ') {   // when i is a non-space
                 int j = i;
-                while (j + 1 < ca.length && ca[j + 1] != ' ') { j++; } // move j to the end of the word
+                while (j + 1 < ca.length && ca[j + 1] != ' ') 
+                { j++; } 
                 reverse(ca, i, j);
                 i = j;
             }
