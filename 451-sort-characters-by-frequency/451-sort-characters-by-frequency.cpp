1079071f440069824e28e400fff26615d3dbdbe7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-22 07:37:11 +0530

    Time: 21 ms (45.55%), Space: 8.5 MB (39.33%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     string frequencySort(string s) {
-       string res="";
-       vector<pair<int,char>> hash('z'+1,{0,0});
-       for(char c:s)
-           hash[c]={hash[c].first+1,c};
-        sort(hash.begin(),hash.end());
-        for(auto i:hash)
+       // string res="";
+       // vector<pair<int,char>> hash('z'+1,{0,0});
+       // for(char c:s)
+       //     hash[c]={hash[c].first+1,c};
+       //  sort(hash.begin(),hash.end());
+       //  for(auto i:hash)
+       //  {
+       //      res=string(i.first,i.second)+res;
+       //  }
+       //  return res;
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+            mp[s[i]]++;
+        for(auto i:mp)
+            pq.push(make_pair(i.second,i.first));
+        string ans="";
+        while(!pq.empty())
         {
-            res=string(i.first,i.second)+res;
+            int a=pq.top().first;
+            char b= pq.top().second;
+            for(int i=0;i<a;i++)
+            {
+                ans+=b;
+            }
+            pq.pop();
         }
-        return res;
+        return ans;
     }
 };
