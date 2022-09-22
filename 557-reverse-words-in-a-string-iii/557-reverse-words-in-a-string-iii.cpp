6b8b49be9663d0f660be698ed3f56f98e3b9c7a6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-22 05:59:29 +0530

    Time: 22 ms (58.88%), Space: 9.6 MB (75.20%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) { 
+        
+        for (int i = 0; i < s.length(); i++) {
+                if (s[i] != ' ') {   
+                    int j = i;
+                    for (; j < s.length() && s[j] != ' '; j++) { } 
+                    reverse(s.begin() + i, s.begin() + j);
+                    i = j - 1;
+                }
+            }
+
+            return s;
+     
+    }
+};
