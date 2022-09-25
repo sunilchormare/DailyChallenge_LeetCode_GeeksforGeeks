ceaa9c4c4f2f8f85d4a816a98c41337d98a4bda9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-25 09:16:53 +0530

    Time: 12 ms (8.17%), Space: 48 MB (66.54%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -1,34 +1,34 @@
 class MyCircularQueue {
-public:
-    MyCircularQueue(int k) {
-        data.resize(k);
+    int maxSize, head = 0, tail = -1;
+    int[] data;
+    public MyCircularQueue(int k) {
+        data = new int[k];
         maxSize = k;
     }
-    bool enQueue(int val) {
+    public boolean enQueue(int val) {
         if (isFull()) return false;
         tail = (tail + 1) % maxSize;
         data[tail] = val;
         return true;
     }
-    bool deQueue() {
+    public boolean deQueue() {
         if (isEmpty()) return false;
-        if (head == tail) head = 0, tail = -1;
-        else head = (head + 1) % maxSize;
+        if (head == tail) {
+            head = 0;
+            tail = -1;
+        } else head = (head + 1) % maxSize;
         return true;
     }
-    int Front() {
+    public int Front() {
         return isEmpty() ? -1 : data[head];
     }
-    int Rear() {
+    public int Rear() {
         return isEmpty() ? -1 : data[tail];
     }
-    bool isEmpty() {
+    public boolean isEmpty() {
         return tail == -1;
     }
-    bool isFull() {
+    public boolean isFull() {
         return !isEmpty() && (tail + 1) % maxSize == head;
     }
-private:
-    int maxSize, head = 0, tail = -1;
-    vector<int> data;
-};
+}
