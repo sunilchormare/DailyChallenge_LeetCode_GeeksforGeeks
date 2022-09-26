5207d7dcabf712956229ced0ef942dba16281e55
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-26 06:51:24 +0530

    Time: 2 ms (69.35%), Space: 42.2 MB (73.50%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,19 +1,18 @@
 class Solution {
-public:
-  int uf[26];
-    bool equationsPossible(vector<string>& equations) {
+      int[] uf = new int[26];
+    public boolean equationsPossible(String[] equations) {
         for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (string e : equations)
-            if (e[1] == '=')
-                uf[find(e[0] - 'a')] = find(e[3] - 'a');
-        for (string e : equations)
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a'))
+        for (String e : equations)
+            if (e.charAt(1) == '=')
+                uf[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
+        for (String e : equations)
+            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a'))
                 return false;
         return true;
     }
 
-    int find(int x) {
+    public int find(int x) {
         if (x != uf[x]) uf[x] = find(uf[x]);
         return uf[x];
     }
-};
+}
