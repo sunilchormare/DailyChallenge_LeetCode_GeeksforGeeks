792ab55f9fe9038a368d84a397733f6779dd22d1
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-09-28 08:48:57 +0530

    Time: 7 ms (23.43%), Space: 6.4 MB (35.59%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,36 +1,44 @@
 class Solution {
-    public String reorganizeString(String S) {
-        int[] hash = new int[26];
-        for (int i = 0; i < S.length(); i++) {
-            hash[S.charAt(i) - 'a']++;
-        } 
-        int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > max) {
-                max = hash[i];
-                letter = i;
-            }
+public:
+
+    string reorganizeString(string S) {
+         string res="";
+        unordered_map<char,int> mp;
+        priority_queue<pair<int,char>>pq;
+        
+        for(auto s: S)
+            mp[s]+=1;
+        
+        for(auto m: mp)
+            pq.push(make_pair(m.second,m.first));
+        
+        while(pq.size()>1){
+            auto top1= pq.top();
+            pq.pop();
+            auto top2 = pq.top();
+            pq.pop();
+            
+            res+=top1.second;
+            res+=top2.second;
+            
+            top1.first -=1;
+            top2.first -= 1;
+            
+            if(top1.first > 0)
+                pq.push(top1);
+            
+            if(top2.first > 0)
+                pq.push(top2);
         }
-        if (max > (S.length() + 1) / 2) {
-            return ""; 
+        
+        if(!pq.empty()){
+            if(pq.top().first > 1)
+                return "";
+            
+            else
+                res+=pq.top().second;
         }
-        char[] res = new char[S.length()];
-        int idx = 0;
-        while (hash[letter] > 0) {
-            res[idx] = (char) (letter + 'a');
-            idx += 2;
-            hash[letter]--;
-        }
-        for (int i = 0; i < hash.length; i++) {
-            while (hash[i] > 0) {
-                if (idx >= res.length) {
-                    idx = 1;
-                }
-                res[idx] = (char) (i + 'a');
-                idx += 2;
-                hash[i]--;
-            }
-        }
-        return String.valueOf(res);
+        
+        return res;
     }
-}
+};
