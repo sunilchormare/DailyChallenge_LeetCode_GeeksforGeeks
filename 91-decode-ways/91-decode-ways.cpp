7b65f894248e99704a4e23e7e59c78799ae716ee
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-01 08:20:08 +0530

    Time: 43 ms (76.76%), Space: 13.9 MB (80.35%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,13 +1,14 @@
-class Solution {
-public:
-   int numDecodings(string s) {
-        int p = 1, pp, n = s.size();
-        for(int i=n-1;i>=0;i--) {
-            int cur = s[i]=='0' ? 0 : p;
-            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
-            pp = p;
-            p = cur;
-        }
-        return s.empty()? 0 : p;   
-    }
-};
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s)+1)
+        dp[0] = 1 
+        dp[1] = 0 if s[0] == "0" else 1
+         
+        for i in range(2, len(s) + 1): 
+            # One step jump
+            if int(s[i-1]) != 0 :   
+                dp[i] += dp[i - 1]
+            # Two step jump
+            if  10 <= int(s[i-2:i]) <= 26: 
+                dp[i] += dp[i - 2]
+        return dp[-1]
