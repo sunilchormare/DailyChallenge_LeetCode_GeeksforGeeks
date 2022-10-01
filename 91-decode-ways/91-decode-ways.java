1773ed94f1dc8708c67df6a95c5eef697d868d5e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-01 08:04:11 +0530

    Time: 1 ms (98.72%), Space: 42.1 MB (67.65%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -3,7 +3,7 @@ class Solution {
          int dp1=1, dp2=0, n=s.length();
         for(int i=n-1;i>=0;i--) {
             int dp=s.charAt(i)=='0'?0:dp1;
-            if(i<n-1&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7'))
+            if(i<n-1&&(s.charAt(i)=='1'|| s.charAt(i)=='2'&&s.charAt(i+1)<'7'))
                 dp+=dp2;
             dp2=dp1;
             dp1=dp;
