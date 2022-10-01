0bf647cfc34313ab6f711e67eaf32a5f8eb1e38f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-01 08:19:02 +0530

    Time: 65 ms (27.00%), Space: 13.8 MB (80.35%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s)+1)
+
+        # base case initialization
+        dp[0] = 1 
+        dp[1] = 0 if s[0] == "0" else 1
+         
+        for i in range(2, len(s) + 1): 
+            # One step jump
+            if int(s[i-1]) != 0 :   
+                dp[i] += dp[i - 1]
+            # Two step jump
+            if  10 <= int(s[i-2:i]) <= 26: 
+                dp[i] += dp[i - 2]
+        return dp[-1]
