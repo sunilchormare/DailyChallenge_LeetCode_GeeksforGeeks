ea9d88a4b668a4c9a9d6ef2f1fb8019df5d98011
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-02 13:58:46 +0530

    Time: 298 ms (10.85%), Space: 47 MB (72.68%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,22 +1,19 @@
-
 class Solution {
-public:
-    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-        ListNode* head = l1->val <= l2->val? l1 : l2;
-        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+     ListNode merge2Lists(ListNode l1, ListNode l2) {
+        if (l1==null) return l2;
+        if (l2==null) return l1;
+        ListNode head = l1.val <= l2.val? l1 : l2;
+        head.next = l1.val <= l2.val ? merge2Lists(l1.next, l2) : merge2Lists(l1, l2.next);
         return head;
     }
-    
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.size() == 0) return NULL;
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
         
-        ListNode* head = lists[0];
+        ListNode head = lists[0];
         
-        for (int i = 1; i < lists.size(); i++)
+        for (int i = 1; i < lists.length; i++)
             head = merge2Lists(head, lists[i]);
         
         return head;
     }
-};
+}
