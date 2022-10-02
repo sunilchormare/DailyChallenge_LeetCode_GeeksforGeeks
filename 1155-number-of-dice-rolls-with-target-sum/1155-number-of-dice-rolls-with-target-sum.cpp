5b70eeeb72adaaef3bc6bbf4bf91e2db8f73ee9b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-02 13:34:18 +0530

    Time: 73 ms (30.72%), Space: 8.3 MB (63.30%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
-    public int numRollsToTarget(int d, int f, int target) {
-        int []dp=new int[target + 1];
+public:
+    int numRollsToTarget(int d, int f, int target) {
+        vector<int> dp(target + 1);
   dp[0] = 1;
   for (int i = 1; i <= d; ++i) {
-    int []dp1=new int[target + 1];
+    vector<int> dp1(target + 1);
     for (int j = 1; j <= f; ++j)
-      for (int k = j; k <= target; ++k)
+      for (auto k = j; k <= target; ++k)
         dp1[k] = (dp1[k] + dp[k - j]) % 1000000007;
-     int[] temp = dp1;
-            dp = dp1;
-            dp1 = temp;
-            dp1[0] = 0;
+    swap(dp, dp1);
   }
   return dp[target];
     }
-}
+};
