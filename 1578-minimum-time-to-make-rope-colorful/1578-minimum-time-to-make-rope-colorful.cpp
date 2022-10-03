b1bd549297f1230040258b41cd0a9650127a82f4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-03 08:29:42 +0530

    Time: 27 ms (21.25%), Space: 89.5 MB (63.68%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,27 +1,13 @@
 class Solution {
-public:
-    int minCost(string colors, vector<int>& neededTime) {
-     
-//         int cost=0;
-//         for(int i=1;i<neededTime.size();++i)
-//         {
-//             int temp=0;
-//             if(colors[i]==colors[i-1])
-//                 temp=min(neededTime[i],neededTime[i-1]);
-//             cost+=temp;
-//         }
-//         return cost;
-        
-        int totalTime = 0, currMaxTime = 0;
-        for (int i = 0; i < colors.size(); ++i) {
-            if (i > 0 && colors[i] != colors[i - 1]) {
-                currMaxTime = 0;
-            }
-            totalTime += min(currMaxTime, neededTime[i]);
-            
-            currMaxTime = max(currMaxTime, neededTime[i]);
-           // cout<<"i : "<<i<<" totalTime :  "<<totalTime<<" currMaxTime : "<<currMaxTime<<endl;
+    public int minCost(String colors, int[] neededTime) {
+        int cost=0,curMax=0;
+        for(int i=0;i<colors.length();++i)
+        {
+            if(i>0&&colors.charAt(i)!=colors.charAt(i-1))
+                curMax=0;
+            cost+=Math.min(curMax,neededTime[i]);
+            curMax=Math.max(curMax,neededTime[i]);
         }
-        return totalTime;
+        return cost;
     }
-};
+}
