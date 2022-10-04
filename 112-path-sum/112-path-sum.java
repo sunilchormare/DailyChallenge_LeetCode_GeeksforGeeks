a29b7ea7bbf9459d4b49f14d2ef58d0934d5d10c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-04 06:25:09 +0530

    Time: 1 ms (69.13%), Space: 43.2 MB (77.00%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -1,10 +1,9 @@
-
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null) return false;
-        targetSum-=root.val;
+       if(root==null) return false;
+       targetSum-=root.val;
         if(root.left==null&&root.right==null)
-            return (targetSum==0)?true:false;
-        return(hasPathSum(root.left,targetSum)||hasPathSum(root.right,targetSum));
+            return (targetSum==0) ?true:false;
+        return hasPathSum(root.left,targetSum) ||hasPathSum(root.right,targetSum);
     }
 }
