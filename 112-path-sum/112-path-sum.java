5d93202ba5c4f729f9fa5dbb4906a85b3316e084
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-04 06:44:20 +0530

    Time: 6 ms (6.06%), Space: 44.1 MB (16.57%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -1,9 +1,33 @@
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-       if(root==null) return false;
-       targetSum-=root.val;
-        if(root.left==null&&root.right==null)
-            return (targetSum==0) ?true:false;
-        return hasPathSum(root.left,targetSum) ||hasPathSum(root.right,targetSum);
+    public boolean hasPathSum(TreeNode root, int sum) {
+       //Recursive
+        // if(root==null) return false;
+       // targetSum-=root.val;
+       //  if(root.left==null&&root.right==null)
+       //      return (targetSum==0) ?true:false;
+       //  return hasPathSum(root.left,targetSum) ||hasPathSum(root.right,targetSum);
+        
+        //Iterative
+         if (root == null) {return false;}
+    Stack<TreeNode> path = new Stack<>();
+    Stack<Integer> sub = new Stack<>();
+    path.push(root);
+    sub.push(root.val);
+    while (!path.isEmpty()) {
+        TreeNode temp = path.pop();
+        int tempVal = sub.pop();
+        if (temp.left == null && temp.right == null) {if (tempVal == sum) return true;}
+        else {
+            if (temp.left != null) {
+                path.push(temp.left);
+                sub.push(temp.left.val + tempVal);
+            }
+            if (temp.right != null) {
+                path.push(temp.right);
+                sub.push(temp.right.val + tempVal);
+            }
+        }
+    }
+    return false;
     }
 }
