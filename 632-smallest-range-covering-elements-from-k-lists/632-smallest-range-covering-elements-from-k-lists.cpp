54eac1f2219d50d13da516129fabc558ab499adf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-04 07:52:55 +0530

    Time: 111 ms (50.27%), Space: 14.6 MB (73.44%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   struct mycompare {
+        bool operator () (pair<int, int>& a, pair<int, int>& b) {return a.first > b.first;}  
+    };
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int n = nums.size(), large = INT_MIN, maxlen = INT_MAX;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, mycompare> pq;
+        for (int i = 0; i < nums.size(); i++) {
+            large = max(large, nums[i][0]);
+            pq.push({nums[i][0], i});
+        }
+        vector<int> ans(2, 0), idx(n, 0);
+        while (pq.size() == n) {
+            int cur = pq.top().first, row = pq.top().second;
+            pq.pop();
+            if (large-cur < maxlen) {
+                maxlen = large-cur;
+                ans[0] = cur;
+                ans[1] = large;
+            }
+            if (++idx[row] < nums[row].size()) {
+                pq.push({nums[row][idx[row]], row});
+                large = max(large, nums[row][idx[row]]);
+            }
+        }
+        return ans;
+    }
+};
