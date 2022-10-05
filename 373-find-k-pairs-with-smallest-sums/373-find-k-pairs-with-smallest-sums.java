8186a14852b2ce61352044a533c3001a31fdabb3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-05 12:01:34 +0530

    Time: 581 ms (44.11%), Space: 119.3 MB (29.30%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
@@ -1,42 +1,20 @@
 class Solution {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-//         PriorityQueue<List<Integer>> pq=new PriorityQueue<>((a,b)->(a.get(0)+a.get(1))-
-//         (b.get(0)+b.get(1))                                                 
-//         );
-//         for(int i=0;i<nums1.length;++i){
-//             for(int j=0;j<nums2.length;++j)
-//                     { pq.add(Arrays.asList(nums1[i],nums2[j]));
-                     
-//                     }
-//             }
-        
-//         List<List<Integer>> res=new ArrayList<>();
-//         while(k>0&&!pq.isEmpty())
-//         {
-//             res.add(pq.poll());
-//             k--;
-//         }
-//         return res;
-        
-          List<List<Integer>> pairs = new ArrayList<> ();
-        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
-            return pairs;
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        int n = nums1.size(), m = nums2.size();
+        vector<vector<int>> ans;
+        for(int i = 0; i < n; i++)
+            pq.push({nums1[i]+nums2[0],{i,0}});
+        while(!pq.empty() && k--)
+        {
+            pair<int,pair<int,int>> tp = pq.top();
+            int x = tp.second.first, y = tp.second.second;
+            pq.pop();
+            ans.push_back({nums1[x], nums2[y]});
+            if(y != m-1)
+                pq.push({nums1[x]+nums2[y+1],{x, y+1}});
         }
-        
-        PriorityQueue<int[]> minHeap = new PriorityQueue<> ((arr1, arr2) -> arr1[0] + arr1[1] - arr2[0] - arr2[1]);
-        
-        for (int i = 0; i < nums1.length && i < k; i++) {
-            minHeap.offer (new int[] {nums1[i], nums2[0], 0});
-        }
-        
-        while (k-- != 0 && !minHeap.isEmpty ()) {
-            int[] curr = minHeap.poll ();
-            pairs.add (List.of (curr[0], curr[1]));
-            if (curr[2] + 1 < nums2.length) {
-                minHeap.offer (new int[] {curr[0], nums2[curr[2] + 1], curr[2] + 1});
-            }
-        }
-        
-        return pairs;
+        return ans;
     }
-}
+};
