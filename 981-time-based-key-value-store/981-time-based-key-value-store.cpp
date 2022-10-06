122b826a6066de929d87574122090df8170b2ad3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-06 16:29:27 +0530

    Time: 768 ms (19.12%), Space: 130.8 MB (19.24%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -0,0 +1,12 @@
+class TimeMap {
+public:
+   unordered_map<string, map<int, string>> m;
+void set(string key, string value, int timestamp) {
+  m[key].insert({ timestamp, value });
+}
+string get(string key, int timestamp) {
+  auto it = m[key].upper_bound(timestamp);
+  return it == m[key].begin() ? "" : prev(it)->second;
+}
+};
+
