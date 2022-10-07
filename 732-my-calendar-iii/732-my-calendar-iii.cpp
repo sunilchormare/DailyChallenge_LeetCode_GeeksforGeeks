dd081ac220f65596fb56883895257278f5bef295
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-07 07:51:29 +0530

    Time: 169 ms (70.02%), Space: 26.4 MB (37.06%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,13 @@
+class MyCalendarThree {
+    map<int, int> timeline;
+public:
+    int book(int s, int e) {
+        timeline[s]++; // 1 new event will be starting at [s]
+        timeline[e]--; // 1 new event will be ending at [e];
+        int ongoing = 0, k = 0;
+        for (pair<int, int> t : timeline)
+            k = max(k, ongoing += t.second);
+        return k;
+    }
+};
+
