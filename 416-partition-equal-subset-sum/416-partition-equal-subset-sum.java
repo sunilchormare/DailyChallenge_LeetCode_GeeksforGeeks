9f78b7c46ebdc168993c62bc6e62ec064398a084
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-07 09:22:25 +0530

    Time: 133 ms (90.69%), Space: 9.7 MB (87.84%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,29 +1,45 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-       int sum = 0;
-    
-    for (int num : nums) {
+public:
+//     vector<vector<int>> m;
+//     bool subsetSum(vector<int> &nums,int n,int pos,int sum)
+//     {
+//         if(sum==0) return true;
+//         else if(pos>=n||sum<0) return false;
+//         if(m[pos][sum]!=-1)
+//             return m[pos][sum];
+//         return m[pos][sum]=subsetSum(nums,n,pos+1,sum-nums[pos])||subsetSum(nums,n,pos+1,sum);
+        
+//     }
+    bool canPartition(vector<int>& nums) {
+       //  int sum=0,n=nums.size();
+       //  for(int i=0;i<n;++i)
+       //      sum+=nums[i];
+       //  if(sum&1) return false;
+       //  m.clear();
+       //  m.resize(n+1,vector<int>(sum/2+1,-1));
+       // return subsetSum(nums,n,0,sum/2);                                      
+        
+    int sum = 0;    
+    for (int num : nums) 
         sum += num;
-    }
-    
-    if ((sum & 1) == 1) {
+
+    if ((sum & 1) == 1) 
         return false;
-    }
     sum /= 2;
-    
-    int n = nums.length;
-    boolean[] dp = new boolean[sum+1];
-    Arrays.fill(dp, false);
+    int n = nums.size();
+     vector<bool> dp(sum+1,false);
     dp[0] = true;
-    
     for (int num : nums) {
+        //cout<<"num: "<<num;
         for (int i = sum; i > 0; i--) {
-            if (i >= num) {
+            if (i >= num) 
+            {
                 dp[i] = dp[i] || dp[i-num];
+                //cout<<" i= "<<i<<" dp[i] : "<<dp[i]<<" dp[i-num] : "<<dp[i-num]<<endl;
             }
         }
+       // cout<<endl;
     }
-    
-    return dp[sum];   
+    return dp[sum];  
     }
-}
+};
