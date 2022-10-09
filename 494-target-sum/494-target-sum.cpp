524cdca7c3326bfa5c70975a1d40d8548606d35c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-09 06:40:48 +0530

    Time: 14 ms (75.48%), Space: 51.4 MB (31.14%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,13 +1,12 @@
 class Solution {
-public:
-    int findTargetSumWays(vector<int>& nums, int s) {
-        int sum = 0; 
+    public int findTargetSumWays(int[] nums, int s) {
+     int sum = 0; 
         for(int i: nums) sum+=i;
         if(s>sum || s<-sum) return 0;
-        vector<int> dp(2*sum+1);
+        int[] dp = new int[2*sum+1];
         dp[0+sum] = 1;
-        for(int i = 0; i<nums.size(); i++){
-            vector<int> next(2*sum+1);
+        for(int i = 0; i<nums.length; i++){
+            int[] next = new int[2*sum+1];
             for(int k = 0; k<2*sum+1; k++){
                 if(dp[k]!=0){
                     next[k + nums[i]] += dp[k];
@@ -18,4 +17,4 @@ public:
         }
         return dp[sum+s];
     }
-};
+}
