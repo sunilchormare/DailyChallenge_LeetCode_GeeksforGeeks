68b8d2dd9a76114c486bc45ccb02a8f8a6f7b161
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-09 06:42:00 +0530

    Time: 40 ms (53.76%), Space: 21.7 MB (30.39%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int s) {
-     int sum = 0; 
+public:
+    int findTargetSumWays(vector<int>& nums, int s) {
+        int sum = 0; 
         for(int i: nums) sum+=i;
         if(s>sum || s<-sum) return 0;
-        int[] dp = new int[2*sum+1];
+        vector<int> dp(2*sum+1);
         dp[0+sum] = 1;
-        for(int i = 0; i<nums.length; i++){
-            int[] next = new int[2*sum+1];
+        for(int i = 0; i<nums.size(); i++){
+            vector<int> next(2*sum+1);
             for(int k = 0; k<2*sum+1; k++){
                 if(dp[k]!=0){
                     next[k + nums[i]] += dp[k];
@@ -17,4 +18,4 @@ class Solution {
         }
         return dp[sum+s];
     }
-}
+};
