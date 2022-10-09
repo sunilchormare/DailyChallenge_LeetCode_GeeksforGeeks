875f335186031df155bb2c852b813c236953a5c0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-09 06:07:31 +0530

    Time: 83 ms (42.61%), Space: 38.8 MB (29.89%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,14 +1,16 @@
 
 class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set = new HashSet<>();
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> set;
         return dfs(root, set, k);
     }
     
-    public boolean dfs(TreeNode root, HashSet<Integer> set, int k){
-        if(root == null)return false;
-        if(set.contains(k - root.val))return true;
-        set.add(root.val);
-        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    bool dfs(TreeNode* root, unordered_set<int>& set, int k){
+        if(root == NULL)return false;
+        if(set.count(k - root->val))
+            return true;
+        set.insert(root->val);
+        return dfs(root->left, set, k) || dfs(root->right, set, k);
     }
-}
+};
