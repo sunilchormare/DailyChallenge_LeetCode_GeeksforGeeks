bbe6f0fbdaf909d3fccac4c60d32d3a597e110f8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-10 16:36:00 +0530

    Time: 52 ms (31.58%), Space: 68.6 MB (78.71%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,9 +1,9 @@
 class Solution {
-public:
-    int numSubseq(vector<int>& A, int target) {
-        sort(A.begin(), A.end());
-        int res = 0, n = A.size(), l = 0, r = n - 1, mod = 1e9 + 7;
-        vector<int> pows(n, 1);
+    public int numSubseq(int[] A, int target) {
+        Arrays.sort(A);
+        int res = 0, n = A.length, l = 0, r = n - 1, mod = (int)1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
         for (int i = 1 ; i < n ; ++i)
             pows[i] = pows[i - 1] * 2 % mod;
         while (l <= r) {
@@ -15,4 +15,4 @@ public:
         }
         return res;
     }
-};
+}
