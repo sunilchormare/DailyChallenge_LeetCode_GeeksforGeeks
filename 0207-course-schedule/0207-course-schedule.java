f0bac10fa9f4fb1bbf20f0701b9a798084f787a6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-11 06:55:20 +0530

    Time: 46 ms (7.86%), Space: 75.2 MB (5.00%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,61 +1,31 @@
-// class Solution {
-//     bool isCyclic(vector<vector<int>>& adj,vector<int>& visited,int curr)
-//     {
-//         if(visited[curr]==2)
-//             return true;
-        
-//         visited[curr] = 2;
-//         for(int i=0;i<adj[curr].size();++i)
-//             if(visited[adj[curr][i]]!=1)
-//                 if(isCyclic(adj,visited,adj[curr][i]))
-//                     return true;
-        
-//         visited[curr] = 1;
-//         return false;
-//     }
-// public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         ios_base::sync_with_stdio(false);
-//         cin.tie(NULL);
-//         cout.tie(NULL);
-        
-//         vector<vector<int>> adj(numCourses);
-//         //Make adjacency matrix (Directed graph)
-//         for(int i=0;i<prerequisites.size();++i)
-//             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        
-//         vector<int> visited(numCourses,0);
-//         for(int i=0;i<numCourses;++i)
-//             if(visited[i]==0)
-//                 if(isCyclic(adj,visited,i))
-//                     return false;
-        
-//         return true;
-//     }
-// };
-
 class Solution {
-public:
-    bool acyclic(vector<vector<int>>& graph, int node, vector<bool>& visit, vector<bool>& done) {
-        if (visit[node]) return false;
-        if (done[node]) return true;
-        visit[node]= true;
-        done[node]= true;
-        for (int adj: graph[node]) {
-            if (acyclic(graph, adj, visit, done)== false) 
-                return false;
-        }
-        visit[node]= false;
-        return true;
+   public boolean canFinish(int numCourses, int[][] prerequisites) {
+    int[][] matrix = new int[numCourses][numCourses]; // i -> j
+    int[] indegree = new int[numCourses];
+    
+    for (int i=0; i<prerequisites.length; i++) {
+        int ready = prerequisites[i][0];
+        int pre = prerequisites[i][1];
+        if (matrix[pre][ready] == 0)
+            indegree[ready]++; //duplicate case
+        matrix[pre][ready] = 1;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n= numCourses;
-        vector<vector<int>> graph(n);
-        for (auto x: prerequisites) graph[x[1]].push_back(x[0]);
-        vector<bool> visit(n, false), done(n, false);
-        for (int i= 0; i<n; i++) {
-            if (done[i]== false and acyclic(graph, i, visit, done)== false) return false;
-        }
-        return true;
+    
+    int count = 0;
+    Queue<Integer> queue = new LinkedList();
+    for (int i=0; i<indegree.length; i++) {
+        if (indegree[i] == 0) queue.offer(i);
     }
-};
+    while (!queue.isEmpty()) {
+        int course = queue.poll();
+        count++;
+        for (int i=0; i<numCourses; i++) {
+            if (matrix[course][i] != 0) {
+                if (--indegree[i] == 0)
+                    queue.offer(i);
+            }
+        }
+    }
+    return count == numCourses;
+}
+}
