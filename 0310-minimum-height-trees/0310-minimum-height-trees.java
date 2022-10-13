94218ce206927d472b85167f2ebaa02324d60a18
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-13 07:26:59 +0530

    Time: 45 ms (74.15%), Space: 72.5 MB (73.33%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,43 +1,47 @@
 class Solution {
-public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-       if(n==0)
-            return {};
-        if(n==1)
-            return {0};
-        vector<int>res;
-        vector<int>degrees(n,0);
-        vector<vector<int>>adj(n);
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][0]].push_back(edges[i][1]);//creating adjacent list
-            adj[edges[i][1]].push_back(edges[i][0]);
-            degrees[edges[i][1]]++;//updating how many edges each node has
-            degrees[edges[i][0]]++;
-        }
-        queue<int>queue;
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+         
+        if (n == 1) return Collections.singletonList(0);
+        
+        List<Integer> res = new ArrayList<>();
+        
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i=0;i<n;i++)
-        {
-            if(degrees[i]==1)//adding all the leave nodes
-                queue.push(i);
+            graph.add(new ArrayList<>());
+        
+        int[]degree = new int[n];
+        
+        for(int[]e:edges){
+            int from = e[1];
+            int to = e[0];
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+            
+            degree[from]++;
+            degree[to]++;
         }
-        while(!queue.empty())
-        {
-            res.clear();// clear vector before we start traversing level by level.
-            int size=queue.size();
-            for(int i=0;i<size;i++)
-            {
-                int cur=queue.front();
-                queue.pop();
-                res.push_back(cur);//adding nodes to vector.Goal is to get a vector of  just 1 or 2 nodes available.
-                for(auto &neighbor:adj[cur])
-                {
-                    degrees[neighbor]--;//removing current leave nodes
-                    if(degrees[neighbor]==1)//adding current leave nodes
-                        queue.push(neighbor);
-                }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i=0;i<degree.length;i++)
+            if(degree[i]==1)
+                queue.offer(i);
+        
+        while(!queue.isEmpty()){
+        
+            res.clear();
+            int size = queue.size();
+            
+            while(size-->0){
+                
+                int node = queue.poll();
+                res.add(node);
+                
+                for(int nei:graph.get(node))
+                    if(--degree[nei]==1)
+                        queue.offer(nei);
             }
         }
-        return res;  
+        return res;
     }
-};
+}
