9de2e47d422887c818ac70d925ce24db1eb1b0cb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-14 08:10:27 +0530

    Time: 15 ms (89.01%), Space: 5.9 MB (71.14%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
@@ -1,21 +1,20 @@
 class Solution {
-    public int count(int m, int n, int x) {
+public:
+    int count(int m, int n, int x) {
         int ans = 0;
         for(int i = 1; i <= m; i++) 
-            ans += Math.min(x / i, n);
+            ans += min(x / i, n);
         return ans;
     }
-    public int findKthNumber(int m, int n, int k) {
-         int L = 1, R = m*n, mid, ans=0;
+    int findKthNumber(int m, int n, int k) {
+        int L = 1, R = m*n, mid, ans;
         while(L <= R) {
             mid = (L + R) >> 1;
             if(count(m, n, mid) < k) 
                 L = mid + 1;
-            else{
-                R = mid - 1;
-                ans = mid;
-            }
+            else
+                R = mid - 1, ans = mid;
         }
         return ans;
     }
-}
+};
