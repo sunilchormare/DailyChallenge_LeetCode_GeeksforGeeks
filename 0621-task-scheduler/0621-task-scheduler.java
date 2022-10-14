4c72ae7167a428655530dc21ae679421736e80d1
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-14 07:55:35 +0530

    Time: 35 ms (46.97%), Space: 43.1 MB (95.67%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,31 +1,28 @@
 class Solution {
-public:
-    int leastInterval(vector<char>& tasks, int n) {
-     unordered_map<char,int> counts;
-     priority_queue<int> pq;
+    public int leastInterval(char[] tasks, int n) {
+         Map<Character,Integer> counts=new HashMap<>();
+     
      int result=0;
       for(char c: tasks) 
-          counts[c]++;
-      for(auto t:counts) 
-          pq.push(t.second);
-        while(!pq.empty())
+          counts.put(c,counts.getOrDefault(c,0)+1);
+     PriorityQueue<Integer> pq=new                      PriorityQueue(counts.size(),Collections.reverseOrder());
+        pq.addAll(counts.values());
+        while(!pq.isEmpty())
         {
-             int time=0;
-             vector<int> tmp;
-             for(int i=0;i<n+1;++i)
-            {
-                if(!pq.empty()) {
-                    tmp.push_back(pq.top()-1);
-                    pq.pop();
-                    time++;
-                }
+         int time=0;
+         List<Integer> tmp=new ArrayList<>();
+        for(int i=0;i<n+1;++i)
+        {
+            if(!pq.isEmpty()) {
+                tmp.add(pq.remove()-1);
+                time++;
             }
-                for(auto t:tmp)
-                    if(t)
-                        pq.push(t);
-
-                result+=pq.empty()?time:n+1;
+        }
+            for(int t:tmp)
+                if(t>0) pq.add(t);
+                
+            result+=pq.isEmpty()?time:n+1;
         }
         return result;
     }
-};
+}
