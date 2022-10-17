8f67c4fdef8d5415733a46bac22028be21719bf4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-17 08:06:14 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (28.54%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
-    public int getMaximumGenerated(int n) {
-       if (n == 0 || n == 1) return n;
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0 || n == 1) return n;
         
-       int[] arr=new int[n+1];
+        vector<int> arr(n+1);
         arr[0] = 0;
         arr[1] = 1;
         int maxi = 1;
         
         for (int i = 2; i <= n; i++) {
             arr[i] = i % 2 == 0 ? arr[i/2] : arr[i / 2] + arr[i / 2 + 1];
-            maxi = Math.max(maxi, arr[i]);
+            maxi = max(maxi, arr[i]);
         }
         
-        return maxi; 
+        return maxi;
     }
-}
+};
