7b821d07032da4a996b6e45da8a3a6ee0a6a2f18
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-17 07:48:35 +0530

    Time: 10 ms (14.30%), Space: 7.2 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-      for (int i = 0; i < 26; ++i) {
-            char currChar = 'a' + i;
+//       for (int i = 0; i < 26; ++i) {
+//             char currChar = 'a' + i;
          
-            if (sentence.find(currChar) == string::npos)
-                return false;
-        }
+//             if (sentence.find(currChar) == string::npos)
+//                 return false;
+//         }
         
-        return true;   
+//         return true; 
+         unordered_set<char> seen(sentence.begin(), sentence.end());
+        
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
     }
 };
