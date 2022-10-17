f859f0eb3f7676e9dae4edc1fe7be3a6fcff0fe6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-17 07:49:51 +0530

    Time: 33 ms (45.91%), Space: 13.5 MB (29.55%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,20 +1,5 @@
-class Solution {
-    public boolean checkIfPangram(String sentence) {
-//         for (int i = 0; i < 26; ++i) {
-//             char currChar = (char)('a' + i);
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        return len(set(sentence))==26; 
 
-//             if (sentence.indexOf(currChar) == -1)
-//                 return false;
-//         }
-    
-//         return true;
-         Set<Character> seen = new HashSet<>();
         
-        // Iterate over 'sentence' add every letter to 'seen'.
-        for (char currChar : sentence.toCharArray())
-            seen.add(currChar);
-        
-        // If the size of 'seen' is 26, then 'sentence' is a pangram.
-        return seen.size() == 26;
-    }
-}
