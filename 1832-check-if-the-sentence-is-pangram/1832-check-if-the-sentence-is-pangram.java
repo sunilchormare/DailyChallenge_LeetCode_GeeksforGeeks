155941c57cfbeb6b6d5d3a65396acc4d734f98f3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-17 07:40:16 +0530

    Time: 3 ms (54.57%), Space: 42.9 MB (6.31%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,12 +1,20 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        for (int i = 0; i < 26; ++i) {
-            char currChar = (char)('a' + i);
+//         for (int i = 0; i < 26; ++i) {
+//             char currChar = (char)('a' + i);
 
-            if (sentence.indexOf(currChar) == -1)
-                return false;
-        }
+//             if (sentence.indexOf(currChar) == -1)
+//                 return false;
+//         }
     
-        return true;
+//         return true;
+         Set<Character> seen = new HashSet<>();
+        
+        // Iterate over 'sentence' add every letter to 'seen'.
+        for (char currChar : sentence.toCharArray())
+            seen.add(currChar);
+        
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
     }
 }
