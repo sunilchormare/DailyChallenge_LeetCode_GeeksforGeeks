471d82a5f68c8cf9691bf9c2fb459260903a5531
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-23 09:04:32 +0530

    Time: 63 ms (68.81%), Space: 21.3 MB (86.33%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> res(2,0);
-        int sumA=0;
-        int n=nums.size();
-        unordered_map<int,int> m;
-        for(int i=0;i<n;++i){
-            m[nums[i]]++;
-            sumA+=nums[i];
-        }
-        int sum=(n*(n+1))/2;
-        //cout<<sum;
-        for(int i=0;i<n;++i)
-        {
-            if(m[nums[i]]>1)
-                res[0]=nums[i],res[1]=sum-(sumA-nums[i]);
-        }
+//         vector<int> res(2,0);
+//         int sumA=0;
+//         int n=nums.size();
+//         unordered_map<int,int> m;
+//         for(int i=0;i<n;++i){
+//             m[nums[i]]++;
+//             sumA+=nums[i];
+//         }
+//         int sum=(n*(n+1))/2;
+//         //cout<<sum;
+//         for(int i=0;i<n;++i)
+//         {
+//             if(m[nums[i]]>1)
+//                 res[0]=nums[i],res[1]=sum-(sumA-nums[i]);
+//         }
          
-     return res;
+//      return res;
+         int correctSum = (nums.size()*(2 + nums.size()-1))/2;
+        int actualSum = 0;
+        int badNum = 0;        
+        for (auto n : nums) {
+            nums[abs(n)-1] *= -1;
+            if (nums[abs(n)-1] > 0) {
+                badNum = abs(n);
+            }
+            actualSum += abs(n);
+        }
+        return {badNum, correctSum - (actualSum - badNum)};
     }
 };
