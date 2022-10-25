611ed9662508eb8c734fdedee037e33ff6940cc9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-25 10:33:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (71.62%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,12 +1,28 @@
 class Solution {
-    public String sortSentence(String s) {
-       String[] words = s.split(" ");
-        String[] bucket = new String[words.length];
-        for (String word : words) {
-            int index = Character.getNumericValue(word.charAt(word.length() - 1)) - 1;
-            bucket[index] = word.substring(0, word.length() - 1);
-        }
-        
-        return String.join(" ", bucket);   
+public:
+    string sortSentence(string s) {
+       string word="" ;
+        vector<string> v(10);
+         for(int i=0;i<s.size();i++)  
+         {
+             if(s[i]>='0'&&s[i]<='9')
+             {
+                 v[s[i]-'0']=word +" ";
+                 word="";
+                 i++;
+             }
+           else
+               word+=s[i];
+         } 
+       
+      for(string x:v)
+      {
+          word+=x;
+       
+      }
+         word.pop_back();
+
+          return word;
+
     }
-}
+};
