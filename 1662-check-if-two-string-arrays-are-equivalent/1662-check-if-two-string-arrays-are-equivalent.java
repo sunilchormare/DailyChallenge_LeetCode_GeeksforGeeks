1f2972ca8c1d22ae36bd85f1dd3891ffa8772bbb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-25 09:42:10 +0530

    Time: 8 ms (59.56%), Space: 11.1 MB (99.96%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,20 +1,33 @@
 class Solution {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        int i = 0, j = 0; 
-        int m = 0, n = 0; 
-        while (i < word1.length && j < word2.length) {            
-            if (word1[i].charAt(m++) != word2[j].charAt(n++))
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    //     string s1="",s2="";
+    //  for(int i=0;i<word1.size();++i)
+    //  {
+    //      s1+=word1[i];
+    //  }
+    //  for(int i=0;i<word2.size();++i)
+    //      s2+=word2[i];
+    //  if(s1==s2)
+    //       return true;
+    // else
+    //      return false;
+    // //return false;
+        
+          int i = 0, j = 0; // word pointers
+        int m = 0, n = 0; // char pointers
+        
+        while (i < word1.size() and j < word2.size()) {            
+            if (word1[i][m++] != word2[j][n++])
                 return false;
             
-            if (m >= word1[i].length())
-            { 
-                i++;
-                m = 0;
-            }
-            if (n >= word2[j].length())
-            {j++; n = 0;}
+            if (m >= word1[i].size())
+                i++, m = 0;
+            
+            if (n >= word2[j].size())
+                j++, n = 0;
         }
         
-        return i == word1.length && j == word2.length;  
+        return i == word1.size() and j == word2.size();
     }
-}
+};
