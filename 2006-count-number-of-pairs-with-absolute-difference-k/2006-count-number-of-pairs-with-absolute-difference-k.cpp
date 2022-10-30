d8197c23bc460ba1dc8313e64ea6fc79f72d3d31
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-30 15:31:35 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (72.31%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -5,17 +5,24 @@ public:
         //BRUTE FORCE SOLUTION
         //TIME COMPLEXITY: O(N^2) 
         //SPACE COMPLEXITY: O(1)
-        int count=0;
-        for(int i=0;i<nums.size();++i)
-        {
-            for(int j=0;j<nums.size();++j)
-            {
-                if(i==j) continue;
-                if(i<j&&abs(nums[i]-nums[j])==k){
-                    count++;
-                }
-            }
-        }
-         return count;
+        // int count=0;
+        // for(int i=0;i<nums.size();++i)
+        // {
+        //     for(int j=0;j<nums.size();++j)
+        //     {
+        //         if(i==j) continue;
+        //         if(i<j&&abs(nums[i]-nums[j])==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        //  return count;
+        
+         int cnt[101] = {}, res = 0;
+    for (auto n : nums)
+        ++cnt[n];
+    for (int i = k + 1; i < 101; ++i)
+        res += cnt[i] * cnt[i - k];
+    return res;
     }
 };
