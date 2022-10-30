fb8e6226ab200431bd4d0119f9c84ee7bdf157da
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-30 15:05:07 +0530

    Time: 22 ms (79.98%), Space: 47 MB (67.78%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,48 +1,39 @@
 class Solution {
-public:
-    int solve(vector<vector<int>>& grid,int k){
-   vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
-        queue<vector<int>> q;
-		
-		// queue stores (x,y,current path length,number of obstacles we can still remove)
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto t=q.front();
-            int x=t[0],y=t[1];
-            q.pop();
-			
-			// Exit if current position is outside of the grid
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
-                continue;
-            }
-			
-			// Destination found
-            if(x==grid.size()-1 && y==grid[0].size()-1)
-                return t[2];
-
-            if(grid[x][y]==1){
-                if(t[3]>0) // If we encounter an obstacle and we can remove it
-                    t[3]--;
-                else
-                    continue;
-            }
-			
-			// The cell was previously visited by some path and we were able to remove more obstacles from the previous path.
-			// Then we don't need to continue on our current path
-            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
-                continue;
-            vis[x][y]=t[3];
-            
-            q.push({x+1,y,t[2]+1,t[3]});
-            q.push({x,y+1,t[2]+1,t[3]});
-            q.push({x-1,y,t[2]+1,t[3]});
-            q.push({x,y-1,t[2]+1,t[3]});
-            
+    public int shortestPath(int[][] grid, int k) {
+        int step = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] DIRS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] seen = new int[m][n]; // min obstacles elimination from (0,0) to (x, y)
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(seen[i], Integer.MAX_VALUE);
         }
-        return -1;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0, 0});
+        seen[0][0] = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int[] cur = q.poll();
+                if (cur[0] == m - 1 && cur[1] == n - 1) {
+                    return step;
+                }
+                for (int[] dir : DIRS) {
+                    int x = dir[0] + cur[0];
+                    int y = dir[1] + cur[1];
+                    if (x < 0 || x >= m || y < 0 || y >= n) {
+                        continue;
+                    }
+                    int o = grid[x][y] + cur[2];
+                    if (o >= seen[x][y] || o > k) {
+                        continue;
+                    }
+                    seen[x][y] = o;
+                    q.offer(new int[]{x, y, o});
+                }
+            }
+            ++step;
+        }
+        return -1;  
     }
-    
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        return solve(grid,k);
-    }
-};
+}
