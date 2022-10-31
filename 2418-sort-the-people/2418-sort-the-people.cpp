0399bdff0e170078c3d41e4aa14b2ddfd66cfa92
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-31 06:45:51 +0530

    Time: 72 ms (61.27%), Space: 23.3 MB (47.79%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
 
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<pair<int, string>> A;
-        int N = names.size();
-        for(int i = 0; i < N; i++) {
-            A.push_back({heights[i], names[i]});
+//         vector<pair<int, string>> A;
+//         int N = names.size();
+//         for(int i = 0; i < N; i++) {
+//             A.push_back({heights[i], names[i]});
+//         }
+
+//         sort(A.rbegin(), A.rend());
+
+//         vector<string> ans;
+//         for(int i = 0; i < N; i++) {
+//             ans.push_back(A[i].second);
+//         }
+//         return ans;
+         priority_queue<pair<int, string>> pq;
+        
+        for(int i = 0; i < heights.size(); i++){
+            pq.push({heights[i], names[i]});
         }
-
-        sort(A.rbegin(), A.rend());
-
+        
         vector<string> ans;
-        for(int i = 0; i < N; i++) {
-            ans.push_back(A[i].second);
+        
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }
