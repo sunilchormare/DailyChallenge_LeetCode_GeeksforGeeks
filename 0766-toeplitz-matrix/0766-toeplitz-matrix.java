102f0f4ef8bef5e8c84ee760a56106e8c57e0ed7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-10-31 06:12:44 +0530

    Time: 175 ms (60.11%), Space: 13.7 MB (98.45%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,10 +1,8 @@
-class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
-        for(int row=0;row<matrix.length;++row)
-            for(int col=0;col<matrix[row].length;++col)
-                if(row>0&&col>0&&matrix[row][col]!=matrix[row-1][col-1])
-                    return false;
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if row>0 and col>0 and matrix[row][col]!=matrix[row-1][col-1] :
+                    return False
         
-        return true;
-    }
-}
+        return True
