edbfe62689780d20c15205d9015104fdab03fd15
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-06 10:23:18 +0530

    Time: 15 ms (63.69%), Space: 43.6 MB (67.89%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -5,6 +5,7 @@ class Solution {
     char cur = 'a';
     for (char k:key.toCharArray())
         m[k] = m[k] != 0 ? m[k] : cur++;
-    return message.chars().mapToObj(i -> String.valueOf(m[i])).collect(Collectors.joining());
+
+return message.chars().mapToObj(i -> String.valueOf(m[i])).collect(Collectors.joining());
     }
 }
