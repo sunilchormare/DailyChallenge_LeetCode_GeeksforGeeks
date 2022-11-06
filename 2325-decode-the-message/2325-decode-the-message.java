e3ba02e849104a29058c5cf4d47bf3f938acc88e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-06 10:21:16 +0530

    Time: 15 ms (63.69%), Space: 43.9 MB (63.69%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,30 +1,10 @@
 class Solution {
-public:
-    // string decodeMessage(string key, string message) {
-    //    unordered_map<char,char> m;
-    //     int ks=key.size(),ms=message.size(),c='a';
-    //     for(int i=0;i<ks;++i)
-    //     {
-    //         if(key[i]==' ') m[' ']=' ';
-    //         if(m[key[i]]>0) continue;
-    //         m[key[i]]=c;
-    //         c++;
-    //     }
-    //     string res="";
-    //     for(int i=0;i<ms;++i)
-    //     {
-    //         //if(message[i]==' ') res+=' ';
-    //         res+=m[message[i]];
-    //     }
-    //     return res;
-    // }
-    string decodeMessage(string key, string mess) {
-    char m[128] = {}, cur = 'a';
-    for (char k : key)
-        if (isalpha(k) && m[k] == 0)
-            m[k] = cur++;
-    for (int i = 0; i < mess.size(); ++i)
-        mess[i] = m[mess[i]] ?: mess[i];
-    return mess;
+    public String decodeMessage(String key, String message) {
+        char[] m = new char[128];
+    m[' '] = ' ';
+    char cur = 'a';
+    for (char k:key.toCharArray())
+        m[k] = m[k] != 0 ? m[k] : cur++;
+    return message.chars().mapToObj(i -> String.valueOf(m[i])).collect(Collectors.joining());
+    }
 }
-};
