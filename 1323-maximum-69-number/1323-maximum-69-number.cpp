efebf1c261ed553fd89d42819b1c70b673734d46
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-07 06:29:26 +0530

    Time: 7 ms (22.51%), Space: 41.6 MB (17.40%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,22 +1,24 @@
 class Solution {
-public:
-    int maximum69Number (int num) {
-        string n=to_string(num);
-        int maxi=0,count=0;
-        for(int i=0;i<n.size();++i)
-        {
-            char c=n[i];
-            if(n[i]=='6') 
-            {
-                n[i]='9';
-                int temp=stoi(n);
-                maxi=max(maxi,temp);
-                n[i]=c;
-            }
-            else count++;
+    public int maximum69Number (int num) {
+//         String s=Integer.toString(num);
+//         char n[]=s.toCharArray();
+//         int maxi=0,count=0;
+//         for(int i=0;i<n.length;++i)
+//         {
+//             char c=n[i];
+//             if(n[i]=='6') 
+//             {
+//                 n[i]='9';
+//                 int temp=Integer.parseInt(new String(n));
+//                 maxi=Math.max(maxi,temp);
+//                 n[i]=c;
+//             }
+//             else count++;
             
             
-        }
-        return count==n.size()?num:maxi;
+//         }
+//         return count==n.length?num:maxi;
+        
+         return Integer.parseInt(("" + num).replaceFirst("6", "9"));
     }
-};
+}
