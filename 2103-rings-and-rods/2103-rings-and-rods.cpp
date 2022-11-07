14b3295ace1b63b362a465158589386e4e0be16a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-07 06:47:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (78.48%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int countPoints(string rings) {
         int rods[10] = {};
-    for (int i = 0; i < rings.size(); i += 2) {
+    for (int i = 0; i < rings.size(); i += 2) 
+    {
         int color = rings[i] == 'R' ? 1 : rings[i] == 'G' ? 2 : 4;
         rods[rings[i + 1] - '0'] |= color;
     }
