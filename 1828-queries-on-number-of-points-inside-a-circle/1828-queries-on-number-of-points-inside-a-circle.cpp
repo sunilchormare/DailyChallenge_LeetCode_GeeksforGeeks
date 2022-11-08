a774bc6da5a6a6a0fa864d9d82a2236cedc40ce3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-08 08:18:43 +0530

    Time: 148 ms (83.23%), Space: 16.3 MB (34.89%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    public int[] countPoints(int[][] points, int[][] queries) {
-         int res[]=new int[queries.length];
-        for(int i=0;i<queries.length;++i)
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> res;
+        for(int i=0;i<queries.size();++i)
         {
             int count=0;
-            for(int j=0;j<points.length;++j)
+            for(int j=0;j<points.size();++j)
             {
-                if(Math.pow((points[j][0]-queries[i][0]),2)+Math.pow((points[j][1]-queries[i][1]),2)<=Math.pow(queries[i][2],2))
+                if(pow((points[j][0]-queries[i][0]),2)+pow((points[j][1]-queries[i][1]),2)<=pow(queries[i][2],2))
                     count++;
             }
-            res[i]=count;
+            res.push_back(count);
         }
         return res;
     }
-}
+};
