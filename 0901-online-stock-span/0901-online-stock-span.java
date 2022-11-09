7d035d159d3ffc0c36337a6e4edee0dcaa777c1f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-09 07:21:13 +0530

    Time: 431 ms (92.72%), Space: 19.5 MB (39.40%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,20 +1,17 @@
-class StockSpanner {
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
 
-    Stack<int[]> stack = new Stack<>();
-    
-    public int next(int price) {
-        int ans = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price) {
-            ans += stack.pop()[1];
-        }
+    def next(self, price: int) -> int:
+        ans = 1
+        while self.stack and self.stack[-1][0] <= price:
+            ans += self.stack.pop()[1]
         
-        stack.push(new int[] {price, ans});
-        return ans;
-    }
-}
+        self.stack.append([price, ans])
+        return ans
+        
 
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner obj = new StockSpanner();
- * int param_1 = obj.next(price);
- */
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
