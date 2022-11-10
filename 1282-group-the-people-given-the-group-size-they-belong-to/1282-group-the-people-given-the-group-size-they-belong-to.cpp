6050ca4675cc407ce0bc8640ed4d94ccb9dcf13a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-10 06:56:31 +0530

    Time: 31 ms (32.46%), Space: 13.4 MB (38.23%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& gz) {
          vector<vector<int>> res, groups(gz.size() + 1);
-  for (auto i = 0; i < gz.size(); ++i) {
+  for (auto i = 0; i < gz.size(); ++i) 
+  {
     groups[gz[i]].push_back(i);
-    if (groups[gz[i]].size() == gz[i]) {
+    if (groups[gz[i]].size() == gz[i]) 
+    {
       res.push_back({});
       swap(res.back(), groups[gz[i]]);
     }
