3e79e323c3a2c2c4bb75d1b84174bc8e6821b827
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-10 06:55:28 +0530

    Time: 4 ms (98.50%), Space: 42.5 MB (99.30%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,14 +1,16 @@
 class Solution {
-public:
-    vector<vector<int>> groupThePeople(vector<int>& gz) {
-         vector<vector<int>> res, groups(gz.size() + 1);
-  for (auto i = 0; i < gz.size(); ++i) {
-    groups[gz[i]].push_back(i);
-    if (groups[gz[i]].size() == gz[i]) {
-      res.push_back({});
-      swap(res.back(), groups[gz[i]]);
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+      Map<Integer, List<Integer>> map=new HashMap<>();
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<groupSizes.length;i++) {
+            if(!map.containsKey(groupSizes[i])) map.put(groupSizes[i], new ArrayList<>());
+            List<Integer> cur=map.get(groupSizes[i]);
+            cur.add(i);
+            if(cur.size()==groupSizes[i]) {
+                res.add(cur);
+                map.remove(groupSizes[i]);
+            }
+        }
+        return res;  
     }
-  }
-  return res;
-    }
-};
+}
