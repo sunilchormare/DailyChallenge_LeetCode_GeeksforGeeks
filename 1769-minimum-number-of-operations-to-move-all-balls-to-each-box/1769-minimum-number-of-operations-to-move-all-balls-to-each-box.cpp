8652e9dd583e2ec02d478c36602ee7193230df9d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-10 13:17:57 +0530

    Time: 17 ms (65.62%), Space: 47.8 MB (30.28%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,17 +1,16 @@
 class Solution {
-public:
-    vector<int> minOperations(string boxes) {
-         vector<int> res(boxes.length()); 
+    public int[] minOperations(String boxes) {
+        int[] res = new int[boxes.length()];
     for (int i = 0, ops = 0, cnt = 0; i < boxes.length(); ++i) {
        res[i] += ops;
-       cnt += boxes[i] == '1' ? 1 : 0;
+       cnt += boxes.charAt(i) == '1' ? 1 : 0;
        ops += cnt;
-    }
+    }    
     for (int i = boxes.length() - 1, ops = 0, cnt = 0; i >= 0; --i) {
         res[i] += ops;
-        cnt += boxes[i] == '1' ? 1 : 0;
+        cnt += boxes.charAt(i) == '1' ? 1 : 0;
         ops += cnt;
     }
     return res;
     }
-};
+}
