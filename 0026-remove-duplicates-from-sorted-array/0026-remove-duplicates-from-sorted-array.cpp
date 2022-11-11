7d2ae2529509d717a328e05c289b60e8c69fb6bf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-11 05:48:08 +0530

    Time: 28 ms (42.53%), Space: 18.4 MB (74.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-         if(nums.length==0) return 0;
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        if(nums.size()==0) return 0;
         
         int i=0;
-        for(int j=1;j<nums.length;++j)
+        for(int j=1;j<nums.size();++j)
         {
             if(nums[j]!=nums[i])
             
@@ -15,4 +17,4 @@ class Solution {
         
         return i+1;
     }
-}
+};
