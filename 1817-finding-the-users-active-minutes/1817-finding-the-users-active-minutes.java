2b763d20510d72bd3174651e4d5bdfb2df95f639
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-11 06:02:07 +0530

    Time: 67 ms (16.93%), Space: 137.8 MB (19.19%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -1,12 +1,13 @@
 class Solution {
-public:
-    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-        unordered_map<int, unordered_set<int>> m;
-    vector<int> res(k);
-    for (auto &l : logs)
-        m[l[0]].insert(l[1]);
-    for (auto &p : m)
-        ++res[p.second.size() - 1];
-    return res;
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for(int i=0;i<logs.length;i++) {
+            map.putIfAbsent(logs[i][0], new HashSet<>());
+            map.get(logs[i][0]).add(logs[i][1]);
+        }
+        int[] res=new int[k];
+        for(int key : map.keySet()) 
+            res[map.get(key).size()-1]++;
+        return res;
     }
-};
+}
