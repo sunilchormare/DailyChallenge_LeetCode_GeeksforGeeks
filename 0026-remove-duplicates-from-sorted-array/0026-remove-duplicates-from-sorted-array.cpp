30fed32e6c5c641f61238ac6b20e21e57544f127
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-11 05:50:54 +0530

    Time: 216 ms (34.83%), Space: 15.7 MB (20.60%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,10 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums)
-    {
-        if(nums.size()==0) return 0;
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+         # if(nums.size()==0) return 0;
         
-        int i=0;
-        for(int j=1;j<nums.size();++j)
-        {
-            if(nums[j]!=nums[i])
-            
-            {
-                i++;
-                nums[i]=nums[j];
-            }
-        }
-        
-        return i+1;
-    }
-};
+        i=0
+        for j in range(1,len(nums)):
+            if(nums[j]!=nums[i]):
+                i=i+1
+                nums[i]=nums[j]
+        return i+1
