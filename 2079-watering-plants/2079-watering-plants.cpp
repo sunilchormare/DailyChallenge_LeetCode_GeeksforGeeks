c51a0edb3eccb66a548b2afb428ecc2195e7b8c9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-12 10:21:41 +0530

    Time: 9 ms (55.33%), Space: 8.5 MB (47.80%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int wateringPlants(vector<int>& plants, int capacity) {
         int n=plants.size();
-        if(plants[0]<=capacity&&n==1) return 1;
+        //if(plants[0]<=capacity&&n==1) return 1;
         int ans=0;
         int tem=capacity;
         for(int i=0;i<n;++i)
