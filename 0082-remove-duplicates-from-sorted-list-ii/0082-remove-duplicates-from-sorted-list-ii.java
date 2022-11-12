2e5e9c6175907aab71462518bb1d5e4f9716eee8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-12 08:57:35 +0530

    Time: 1 ms (85.02%), Space: 44.2 MB (12.14%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,24 +1,22 @@
-
 class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL) return NULL;
-        ListNode *FakeHead=new ListNode(0);
-        FakeHead->next=head;
-        ListNode *pre=FakeHead;
-        ListNode *cur=head;
-        while(cur!=NULL){
-            while(cur->next!=NULL&&cur->val==cur->next->val)
-                cur=cur->next;
-            
-            if(pre->next==cur)
-                pre=pre->next;
-            
-            else
-                pre->next=cur->next;
-            
-            cur=cur->next;
+    public ListNode deleteDuplicates(ListNode head) {
+   if(head==null) return null;
+        ListNode FakeHead=new ListNode(0);
+        FakeHead.next=head;
+        ListNode pre=FakeHead;
+        ListNode cur=head;
+        while(cur!=null){
+            while(cur.next!=null&&cur.val==cur.next.val){
+                cur=cur.next;
+            }
+            if(pre.next==cur){
+                pre=pre.next;
+            }
+            else{
+                pre.next=cur.next;
+            }
+            cur=cur.next;
         }
-        return FakeHead->next;
+        return FakeHead.next;
     }
-};
+}
