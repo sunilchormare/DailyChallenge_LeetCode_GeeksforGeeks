fe1988ff3c2d1a95f7358a1220db6bffa15413ea
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-12 10:24:27 +0530

    Time: 7 ms (72.43%), Space: 8.4 MB (84.85%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.cpp
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int wateringPlants(vector<int>& plants, int capacity) {
-        int n=plants.size();
-        //if(plants[0]<=capacity&&n==1) return 1;
-        int ans=0;
-        int tem=capacity;
-        for(int i=0;i<n;++i)
-        {
-            if(plants[i]<=capacity)
-            {
-                ans++;
-                capacity-=plants[i];
-            }
-            else
-            {
+    int wateringPlants(vector<int>& p, int capacity) {
+//         int n=plants.size();
+//         //if(plants[0]<=capacity&&n==1) return 1;
+//         int ans=0;
+//         int tem=capacity;
+//         for(int i=0;i<n;++i)
+//         {
+//             if(plants[i]<=capacity)
+//             {
+//                 ans++;
+//                 capacity-=plants[i];
+//             }
+//             else
+//             {
                 
-                capacity=tem;
-                capacity-=plants[i];
-                ans+=(2*i)+1;
-            }
+//                 capacity=tem;
+//                 capacity-=plants[i];
+//                 ans+=(2*i)+1;
+//             }
+//         }
+//         return ans;
+        
+        int res = 0, can = capacity;
+    for (int i = 0; i < p.size(); ++i) {
+        if (can < p[i]) {
+            res += i * 2;
+            can = capacity;
         }
-        return ans;
+        can -= p[i];
+    }
+    return res + p.size();    
     }
 };
