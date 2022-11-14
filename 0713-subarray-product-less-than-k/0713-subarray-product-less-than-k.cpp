cefee1c9ca8cf7c380b99c0d7a08f06d50d3421a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-14 07:32:09 +0530

    Time: 224 ms (28.54%), Space: 61.1 MB (93.82%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,9 +1,11 @@
 class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-         if (k == 0) return 0;
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        
+      if (k == 0) return 0;
         int cnt = 0;
         int pro = 1;
-        for (int i = 0, j = 0; j < nums.length; j++) {
+        for (int i = 0, j = 0; j < nums.size(); j++) {
             pro *= nums[j];
             while (i <= j && pro >= k) {
                 pro /= nums[i++];
@@ -12,4 +14,4 @@ class Solution {
         }
         return cnt;
     }
-}
+};
