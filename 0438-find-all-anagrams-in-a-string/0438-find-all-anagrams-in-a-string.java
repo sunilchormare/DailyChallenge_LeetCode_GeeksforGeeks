9871ccd97583f5be205078502d7b1b66e8474590
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-14 07:29:44 +0530

    Time: 15 ms (80.69%), Space: 47.5 MB (70.99%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,30 +1,29 @@
 class Solution {
-public:
-    vector<int> findAnagrams(string s, string p) {
-        if(p.size() > s.size())
-        return {};
-    
-    vector<int> res;
-    vector<int> freq(26), curr(26);
-    
-    for(char ch : p)
-        freq[ch-'a']++;
-    
-    int k = p.size();
-    //sliding window
-    for(int i=0; i<k;i++)
-        curr[s[i]-'a']++;
-    
-    if(curr == freq)
-        res.push_back(0);
-    
-    for(int i=k;i<s.size();i++){
-        curr[s[i-k]-'a']--;//removed
-        curr[s[i]-'a']++; //added
-          if(curr == freq)
-            res.push_back(i-k+1);
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+    int[] hash = new int[256]; //character hash
+    //record each character in p to hash
+    for (char c : p.toCharArray()) {
+        hash[c]++;
     }
+    //two points, initialize count to p's length
+    int left = 0, right = 0, count = p.length();
+    while (right < s.length()) {
+        //move right everytime, if the character exists in p's hash, decrease the count
+        //current hash value >= 1 means the character is existing in p
+        if (hash[s.charAt(right++)]-- >= 1) count--; 
+        
+        //when the count is down to 0, means we found the right anagram
+        //then add window's left to result list
+        if (count == 0) list.add(left);
     
-    return res;
+        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
+        //++ to reset the hash because we kicked out the left
+        //only increase the count if the character is in p
+        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
+        if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
     }
-};
+    return list; 
+    }
+}
