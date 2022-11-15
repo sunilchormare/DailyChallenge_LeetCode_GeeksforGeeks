c3d0132ef762c0af68e6c5fabb9a232c519c779a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-15 08:07:46 +0530

    Time: 165 ms (48.90%), Space: 25.5 MB (54.95%) - LeetHub

--- 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.cpp
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
-    public int minOperations(int[] A) {
-          int res = 0, maxLen = 1;
+public:
+    int minOperations(vector<int>& A) {
+        int res = 0, maxLen = 1;
         for (int a : A) {
             int bits = 0;
             while (a > 0) {
@@ -8,8 +9,8 @@ class Solution {
                 bits++;
                 a >>= 1;
             }
-            maxLen = Math.max(maxLen, bits);
+            maxLen = max(maxLen, bits);
         }
         return res + maxLen - 1;
     }
-}
+};
