61c25d6642179ca90b4d160be1fba3cab71b9fdf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-17 06:22:58 +0530

    Time: 4 ms (80.14%), Space: 55.5 MB (16.67%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,24 @@
+class Solution {
+   public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        backtracking(res, graph, 0, path);
+        return res;
+    }
+    
+    private void backtracking(List<List<Integer>> res, int[][] graph, int start, List<Integer> cur) {
+        if(graph[start].length==0 || start==graph.length-1) {
+            if(start==graph.length-1) {
+                res.add(new ArrayList<>(cur));
+            }
+            return;
+        }
+        
+        for(int next:graph[start]) {
+            cur.add(next);
+            backtracking(res, graph, next, cur);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
