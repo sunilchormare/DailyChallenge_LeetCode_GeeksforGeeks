fbdf67077a8cf37a069ffd82ff2fbe3e5c62671a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-17 05:44:23 +0530

    Time: 5 ms (51.55%), Space: 43.2 MB (44.66%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,8 +1,7 @@
 class Solution {
-public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-    int left = max(A,E), right = max(min(C,G), left);
-    int bottom = max(B,F), top = max(min(D,H), bottom);
+   int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int left = Math.max(A,E), right = Math.max(Math.min(C,G), left);
+    int bottom = Math.max(B,F), top = Math.max(Math.min(D,H), bottom);
     return (C-A)*(D-B) - (right-left)*(top-bottom) + (G-E)*(H-F);
 }
-};
+}
