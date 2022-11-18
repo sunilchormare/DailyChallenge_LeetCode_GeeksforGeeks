531dc57cba6608bb215dbf93e63de3bc2a4fea01
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-18 07:13:56 +0530

    Time: 2 ms (48.66%), Space: 43.9 MB (5.66%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,23 @@
 class Solution {
-public:
-    void helper(int i,vector<int> &nums,vector<int> ds,vector<vector<int>> &res){
-        if(i==nums.size()){
-            res.push_back(ds);
-            return;
-        }
-        ds.push_back(nums[i]);
-        helper(i+1,nums,ds,res);
-        ds.pop_back();
-        helper(i+1,nums,ds,res);
+    
+    
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> res=new ArrayList<>();
+    //Arrays.sort(nums);
+    helper(0,nums,new ArrayList<>(),res);
+    
+    return res;
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> ds;
-        helper(0,nums,ds,res);
-        return res;
+    private void helper(int in,int []nums,List<Integer> ds,List<List<Integer>> res)
+    {  
+        res.add(new ArrayList<>(ds));
+    for(int i = in; i < nums.length; i++)
+    {
+        ds.add(nums[i]);
+        helper(i+1,nums, ds,res);
+        ds.remove(ds.size() - 1);
+        
     }
-};
+    }
+    
+}
