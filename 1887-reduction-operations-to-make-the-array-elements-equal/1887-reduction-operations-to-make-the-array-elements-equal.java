d4717ec77d57cb53ca53305f07df98a6cf9f29d4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-18 07:24:46 +0530

    Time: 69 ms (36.43%), Space: 86.9 MB (42.14%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,11 +1,10 @@
 class Solution {
-public:
-    int reductionOperations(vector<int>& n) {
-       int res = 0, sz = n.size();
-    sort(begin(n), end(n));
+    public int reductionOperations(int[] n) {
+        int res = 0, sz = n.length;
+    Arrays.sort(n);
     for (int j = sz - 1; j > 0; --j)
         if (n[j - 1] != n[j])
             res += sz - j;
-    return res;  
+    return res;
     }
-};
+}
