03ca06b5ea904915685d064abf609db252e3fb53
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-19 06:35:33 +0530

    Time: 3 ms (96.61%), Space: 13.4 MB (21.70%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
-    public void findCombinations(int in,int[] A,int target,List<List<Integer>> res,List<Integer>ds)
+public:
+    void findCombinations(int in,vector<int> A,int target,vector<vector<int>> &res,vector<int> &ds)
     {
-            if(target==0){
-            res.add(new ArrayList<>(ds));
+        if(target==0){
+            res.push_back(ds);
             return;
-            }
-        for(int i=in;i<A.length;++i){  
+        }
+        for(int i=in;i<A.size();++i)
+        {
             if(i>in&&A[i]==A[i-1]) continue;
             if(A[i]>target) break;
-            ds.add(A[i]);
+            ds.push_back(A[i]);
             findCombinations(i+1,A,target-A[i],res,ds);
-            ds.remove(ds.size()-1);
+            ds.pop_back();
         }
-
     }
-    public List<List<Integer>> combinationSum2(int[] A, int target) {
-         List<List<Integer>> res=new ArrayList<>();
-        Arrays.sort(A);
-        findCombinations(0,A,target,res,new ArrayList<>());
+    
+    vector<vector<int>> combinationSum2(vector<int>& A, int target) {
+        vector<vector<int>> res;
+        vector<int> ds;
+        sort(A.begin(),A.end());
+        findCombinations(0,A,target,res,ds);
         return res;
     }
-      
-}
+};
