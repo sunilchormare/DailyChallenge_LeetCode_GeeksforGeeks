0fac23d09d2aa60ac1ee3e32ef62e7bbdd727473
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-20 06:02:48 +0530

    Time: 30 ms (36.34%), Space: 44.3 MB (50.63%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,47 +1,37 @@
 class Solution {
-public:
-    int calculate(string s) {
-         stack<pair<int,int>> st; // pair(prev_calc_value , sign before next bracket () )
-       
-       long long int sum = 0;
-       int sign = +1;
-       
-       for(int i = 0 ; i < s.size() ; ++i)
-       {
-           char ch = s[i];
-           
-           if(isdigit(ch))
-           {
-               long long int num = 0;
-               while(i < s.size() and isdigit(s[i]))
-               {
-                   num = (num * 10) + s[i] - '0';
-                   i++;
-               }
-               i--; // as for loop also increase i , so if we don't decrease i here a sign will be skipped
-               sum += (num * sign);
-               sign = +1; // reseting sign
-           }
-           else if(ch == '(')
-           {
-               // Saving current state of (sum , sign) in stack
-               st.push(make_pair(sum , sign));
-               
-               // Reseting sum and sign for inner bracket calculation
-               sum = 0; 
-               sign = +1;
-           }
-           else if(ch == ')')
-           {
-               sum = st.top().first + (st.top().second * sum);
-               st.pop();
-           }
-           else if(ch == '-')
-           {
-               // toggle sign
-               sign = (-1 * sign);
-           }
-       }
-       return sum;
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+    int result = 0;
+    int number = 0;
+    int sign = 1;
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        if(Character.isDigit(c)){
+            number = 10 * number + (int)(c - '0');
+        }else if(c == '+'){
+            result += sign * number;
+            number = 0;
+            sign = 1;
+        }else if(c == '-'){
+            result += sign * number;
+            number = 0;
+            sign = -1;
+        }else if(c == '('){
+            //we push the result first, then sign;
+            stack.push(result);
+            stack.push(sign);
+            //reset the sign and result for the value in the parenthesis
+            sign = 1;   
+            result = 0;
+        }else if(c == ')'){
+            result += sign * number;  
+            number = 0;
+            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+            
+        }
     }
-};
+    if(number != 0) result += sign * number;
+    return result;
+    }
+}
