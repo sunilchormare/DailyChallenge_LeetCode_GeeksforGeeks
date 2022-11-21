30b9d85be20cc9bfddaad4e52b6465dd12a2d528
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-21 05:59:21 +0530

    Time: 385 ms (90.98%), Space: 17.1 MB (5.38%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,9 +1,8 @@
-class Solution {
-   public int[] fairCandySwap(int[] A, int[] B) {
-        int dif = (IntStream.of(A).sum() - IntStream.of(B).sum()) / 2;
-        HashSet<Integer> S = new HashSet<>();
-        for (int a : A) S.add(a);
-        for (int b : B) if (S.contains(b + dif)) return new int[] {b + dif, b};
-        return new int[0];
-    }
-}
+class Solution:
+    def fairCandySwap(self, A, B):
+        dif = (sum(A) - sum(B)) / 2
+        A = set(A)
+        for b in set(B):
+            if dif + b in A:
+                return [dif + b, b]
+        
