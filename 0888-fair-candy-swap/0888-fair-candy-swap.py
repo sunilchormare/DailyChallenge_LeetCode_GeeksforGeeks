a8a10050e3e84ef2c92ef70084612a83453ca2bc
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-21 05:59:11 +0530

    Time: 850 ms (61.98%), Space: 16.3 MB (79.31%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fairCandySwap(self, A, B):
+        dif = (sum(A) - sum(B)) / 2
+        A = set(A)
+        for b in set(B):
+            if dif + b in A:
+                return [dif + b, b]
+        
