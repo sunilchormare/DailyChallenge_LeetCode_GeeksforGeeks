715fe57cad7de5e275b748574b9c14590a2d36ed
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-22 18:08:37 +0530

    Time: 37 ms (82.64%), Space: 43.9 MB (32.70%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,20 +1,17 @@
 class Solution {
-public:
-    int numSquares(int n) {
-        vector<int> dp(n+1,INT_MAX);
-    
-        dp[0]=0;
-        int count = 1;
-        while(count*count <= n) 
-        {
-        int sq = count*count;
-        for(int i = sq; i < n+1; i++) 
-        {
-            dp[i] = min(dp[i-sq] + 1,dp[i]);
-        }
-       
-        count++;
+    public int numSquares(int n) {
+       int[] dp = new int[n + 1];
+	Arrays.fill(dp, Integer.MAX_VALUE);
+	dp[0] = 0;
+	for(int i = 1; i <= n; ++i) {
+		int min = Integer.MAX_VALUE;
+		int j = 1;
+		while(i - j*j >= 0) {
+			min = Math.min(min, dp[i - j*j] + 1);
+			++j;
+		}
+		dp[i] = min;
+	}		
+	return dp[n]; 
     }
-    return dp[n]; 
-    }
-};
+}
