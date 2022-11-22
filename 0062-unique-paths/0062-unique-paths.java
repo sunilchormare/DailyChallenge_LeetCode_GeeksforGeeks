d8eaa3ed4ca3f82d2c9b247f7f4c2eea092f2212
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-22 18:20:12 +0530

    Time: 65 ms (28.60%), Space: 13.9 MB (32.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,32 +1,9 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-//         int N=n+m-2;
-//         int r=m-1;
-//         double res=1;
-//         for(int i=1;i<=r;++i)
-//             res=res*(N-r+i)/i;
-        
-//         return (int)res;
-//         // return (dfs(0,0,m,n));
-//     }
-// //     int dfs()
-// //     {
-        
-// //     }
-        
-        
-        int dp[m][n];
-        for(int i=0;i<m;++i)
-        {
-            for(int j=0;j<n;++j)
-            {
-                if(i==0 || j==0)
-                    dp[i][j]=1;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];   
-    }
-};
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1]*n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j] + dp[j-1]
+        return dp[n-1]
+    
+ 
