e68964ced31323be5ad89acbf5ae67dcfe06e56e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-23 07:06:13 +0530

    Time: 53 ms (38.01%), Space: 20.3 MB (27.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,19 +1,24 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+         vector<set<int>> rows(9), cols(9), blocks(9);
         
-        Set seen=new HashSet();
-        for(int i=0;i<9;++i)
-        {
-            for(int j=0;j<9;++j)
-            {
-                char number=board[i][j];
-                if(number!='.')
-                   if (!seen.add(number + " in row " + i) ||
-                    !seen.add(number + " in column " + j) ||
-                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+                if (board[i][j] == '.') continue;
+                
+                int curr = board[i][j] - '0';
+                if (rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
                     return false;
+                
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3)*3+j/3].insert(curr);
             }
         }
+        
         return true;
     }
-}
+};
