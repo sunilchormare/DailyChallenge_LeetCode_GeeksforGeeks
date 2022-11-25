c92d4cfa5f5e0d07d3f762fcba71f2b268123264
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-25 06:48:48 +0530

    Time: 182 ms (84.31%), Space: 13.3 MB (53.51%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -7,7 +7,7 @@ public:
             for(int j = 0; j <i ; j++){
                 if(nums[i] > nums[j]){
                     if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
-                    if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
+                    else if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
                 }
             }
             if(max_len == dp[i].first)res += dp[i].second;
