c2f0d7324e603774e752ae3e33c45a4396fb42fa
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-26 08:45:32 +0530

    Time: 1375 ms (5.03%), Space: 44.1 MB (79.11%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -4,10 +4,19 @@ class Solution {
     for(int i = 0; i <= word1.length(); i++) {
         for(int j = 0; j <= word2.length(); j++) {
             if(i == 0 || j == 0) dp[i][j] = 0;
-            else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
+            else 
+                dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
                     : Math.max(dp[i-1][j], dp[i][j-1]);
         }
     }
+         for(int i = 1; i <= word1.length(); i++){
+        System.out.print(word1.charAt(i-1));
+            for(int j = 1; j <= word2.length(); j++){
+                System.out.print(dp[i][j]);
+            }
+        System.out.println();
+        }
+        
     int val =  dp[word1.length()][word2.length()];
     return word1.length() - val + word2.length() - val;
     }
