1c589eccc0efbb5ed7b7b0f5d25b0dc169e48996
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-26 07:46:54 +0530

    Time: 48 ms (79.95%), Space: 51.6 MB (88.91%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,35 +1,18 @@
 class Solution {
-public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)    {
-//      int profit=0;
-//         int n=s.size();
-//      for(int i=0;i<n;++i)
-//      {
-//          int cur=0;
-//          for(int j=i+1;j<n;++j)
-//          {
-//              if(s[j]<e[i]&&e[j]>e[i]) continue;
-//               else
-//               {
-//                   cur+=p[j];
-//                   profit=max(profit,cur);
-//               }
-//          }
-//      }
-        
-//         return profit;
-         int n = startTime.size();
-        vector<vector<int>> jobs;
-        for (int i = 0; i < n; ++i) {
-            jobs.push_back({endTime[i], startTime[i], profit[i]});
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
         }
-        sort(jobs.begin(), jobs.end());
-        map<int, int> dp = {{0, 0}};
-        for (auto& job : jobs) {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
-            if (cur > dp.rbegin()->second)
-                dp[job[0]] = cur;
+        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+        for (int[] job : jobs) {
+            int cur = dp.floorEntry(job[0]).getValue() + job[2];
+            if (cur > dp.lastEntry().getValue())
+                dp.put(job[1], cur);
         }
-        return dp.rbegin()->second;
+        return dp.lastEntry().getValue();
     }
-};
+}
