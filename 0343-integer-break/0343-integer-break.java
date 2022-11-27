97f8a19800ebe5fbbf142b69a4d562ddccda715d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-27 10:10:30 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (5.20%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,7 +1,6 @@
 class Solution {
-public:
-    int integerBreak(int n) {
-        if(n==2) return 1;
+    public int integerBreak(int n) {
+       if(n==2) return 1;
         if(n==3) return 2;
         int product = 1;
         while(n>4){
@@ -10,6 +9,6 @@ public:
         }
         product*=n;
         
-        return product;
+        return product; 
     }
-};
+}
