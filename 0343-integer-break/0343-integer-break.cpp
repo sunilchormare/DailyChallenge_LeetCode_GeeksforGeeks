6858e32e197c60c2168e3999eaf6fe2874822142
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-27 10:10:01 +0530

    Time: 2 ms (53.84%), Space: 5.9 MB (74.70%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,7 @@
 class Solution {
-    public int integerBreak(int n) {
-       if(n==2) return 1;
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
         if(n==3) return 2;
         int product = 1;
         while(n>4){
@@ -9,6 +10,6 @@ class Solution {
         }
         product*=n;
         
-        return product; 
+        return product;
     }
-}
+};
