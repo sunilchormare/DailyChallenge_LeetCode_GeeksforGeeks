790e60e55652bdc9f6bbcfeb6961433f7c9b38e3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-27 10:02:27 +0530

    Time: 152 ms (59.17%), Space: 14.5 MB (71.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,15 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] change = new int[amount+1];
-    Arrays.fill(change,-1);
-    change[0]=0;
-    
-    for(int i=1;i<=amount;i++){
-        boolean flag = false;
-        int min = Integer.MAX_VALUE;
-        for(int coin:coins){
-            if(i>=coin && change[i-coin] != -1){
-                flag=true;
-                min = Math.min(min,change[i-coin]);
-            }
-        }
-        if(flag){
-            change[i]= 1+min;
+public:
+    int coinChange(vector<int>& coins, int amount) {
+     vector<int> need(amount+1, amount+1);
+    need[0] = 0;
+    for (int c : coins){
+         //cout<<" c = "<<c<<endl; 
+        for (int a=c; a<=amount; a++)
+        {  need[a] = min(need[a], need[a-c] + 1);
+         //cout<<" need[a] = "<<need[a]<<endl; 
         }
     }
-    return change[amount];
+    return need.back() > amount ? -1 : need.back();   
     }
-}
+};
