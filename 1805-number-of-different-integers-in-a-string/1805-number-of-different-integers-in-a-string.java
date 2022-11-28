4cecb138e3aa55d6ab25e9aa556657c8727a1b1b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-28 07:04:10 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (20.55%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.java
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.java
@@ -1,16 +1,15 @@
 class Solution {
-    public int numDifferentIntegers(String w) {
-       var s = new HashSet<String>(Arrays.asList(""));
-    int i = 0;
-    for (int j = 0; j < w.length(); ++j) {
-        if (Character.isDigit(w.charAt(j)))
-            i += i < j && w.charAt(i) == '0' ? 1 : 0;
+public:
+    int numDifferentIntegers(string w) {
+       unordered_set<string> s{""};    
+    for (int i = 0, j = 0; j <= w.size(); ++j) {
+        if (j < w.size() && isdigit(w[j]))
+            i += i < j && w[i] == '0';
         else {
-            s.add(w.substring(i, j));
+            s.insert(w.substr(i, j - i));
             i = j + 1;
         }
     }
-    s.add(w.substring(i));        
-    return s.size() - 1;   
+    return s.size() - 1; 
     }
-}
+};
