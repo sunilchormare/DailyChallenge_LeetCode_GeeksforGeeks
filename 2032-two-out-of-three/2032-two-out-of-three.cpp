97df7c1c174998c5dd2bae9c2469364d8831824d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-28 06:58:54 +0530

    Time: 16 ms (95.76%), Space: 25.2 MB (92.61%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+    bool cnt[3][101] = {};
+        vector<int> res;
+        for (auto n : nums1)
+            cnt[0][n] = true;
+        for (auto n : nums2)
+            cnt[1][n] = true;
+        for (auto n : nums3)
+            cnt[2][n] = true;
+        for (int i = 1; i <= 100; ++i)
+            if (cnt[0][i] + cnt[1][i] + cnt[2][i] > 1)
+                res.push_back(i);
+        return res;   
+    }
+};
