8a2e91f0f950ed4721f3a771043e7f049ba1ebd9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-28 06:59:41 +0530

    Time: 4 ms (86.74%), Space: 47 MB (53.72%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,17 +1,18 @@
 class Solution {
-public:
-    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
-    bool cnt[3][101] = {};
-        vector<int> res;
-        for (auto n : nums1)
-            cnt[0][n] = true;
-        for (auto n : nums2)
-            cnt[1][n] = true;
-        for (auto n : nums3)
-            cnt[2][n] = true;
-        for (int i = 1; i <= 100; ++i)
-            if (cnt[0][i] + cnt[1][i] + cnt[2][i] > 1)
-                res.push_back(i);
-        return res;   
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        List<Integer> list = new ArrayList<>();
+        int count[][] = new int[3][101];
+        for(int n : nums1)
+            count[0][n] = 1;
+        for(int n : nums2) 
+            count[1][n] = 1;
+        for(int n : nums3)
+            count[2][n] = 1;
+        
+        for(int i=1; i<=100; i++) {
+            if(count[0][i] + count[1][i] + count[2][i] > 1)
+                list.add(i);
+        }
+        return list;
     }
-};
+}
