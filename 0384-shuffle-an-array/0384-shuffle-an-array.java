5cf2bda98e3921d70d7504c1269efc2b7d5945c4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-29 06:03:29 +0530

    Time: 54 ms (84.17%), Space: 48.3 MB (88.22%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.java
+++ 0384-shuffle-an-array/0384-shuffle-an-array.java
@@ -1,22 +1,41 @@
-class Solution {
-    vector<int> nums;
-public:
-    Solution(vector<int> nums) {
-        this->nums = nums;
-    }
-    
-    /** Resets the array to its original configuration and return it. */
-    vector<int> reset() {
-        return nums;
-    }
-    
-    /** Returns a random shuffling of the array. */
-    vector<int> shuffle() {
-        vector<int> result(nums);
-        for (int i = 0;i < result.size();i++) {
-            int pos = rand()%(result.size()-i);
-            swap(result[i+pos], result[i]);
+class Solution 
+{
+    private int[] array;
+    private int[] original;
+    private Random rand = new Random();
+    private List<Integer> getArrayCopy() 
+    {
+        List<Integer> asList = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) 
+        {
+            asList.add(array[i]);
         }
-        return result;
+        return asList;
     }
-};
+
+    public Solution(int[] nums) 
+    {
+        array = nums;
+        original = nums.clone();
+    }
+    
+    public int[] reset() 
+    {
+        array = original;
+        original = original.clone();
+        return array;
+    }
+    
+    public int[] shuffle()
+    {
+        List<Integer> aux = getArrayCopy();
+        for (int i = 0; i < array.length; i++) 
+        {
+            int removeIdx = rand.nextInt(aux.size());
+            array[i] = aux.get(removeIdx);
+            aux.remove(removeIdx);
+        }
+        return array;
+    }
+}
+
