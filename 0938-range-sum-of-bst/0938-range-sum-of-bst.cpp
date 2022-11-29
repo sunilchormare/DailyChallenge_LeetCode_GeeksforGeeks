eb59a5f60294b3ca86d09a46641cf9f1809684c6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-11-29 13:41:28 +0530

    Time: 386 ms (5.14%), Space: 64.6 MB (60.97%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-   int dfs(TreeNode* root,int low,int high,int sum)
-   {
-       stack<TreeNode*> st;
-       //int sum=0;
+   // int dfs(TreeNode* root,int low,int high,int sum)
+   // {
+   //     stack<TreeNode*> st;
+   //     //int sum=0;
+   //     st.push(root);
+   //     while(!st.empty())
+   //     {
+   //         TreeNode* node=st.top();
+   //         st.pop();
+   //         if(node->val>=low && node->val<=high)
+   //             sum+=node->val;
+   //         if(node->left!=NULL) st.push(node->left);
+   //         if(node->right!=NULL) st.push(node->right);
+   //         //if(node->left==NULL&&node->right==NULL) st.push_back(node->left);
+   //     }
+   //     return sum;
+   // }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        //return dfs(root,low,high,0);
+        stack<TreeNode*> st;
+       int sum=0;
        st.push(root);
        while(!st.empty())
        {
@@ -16,8 +33,5 @@ public:
            //if(node->left==NULL&&node->right==NULL) st.push_back(node->left);
        }
        return sum;
-   }
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        return dfs(root,low,high,0);
     }
 };
