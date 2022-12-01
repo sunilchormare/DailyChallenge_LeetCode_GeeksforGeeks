6684d3170d3a03af52bc136b52d52a9489b2862c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-01 06:51:39 +0530

    Time: 6 ms (58.35%), Space: 6.7 MB (31.29%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,11 +1,23 @@
-class Solution:
-    def halvesAreAlike(self, s: str) -> bool:
-        vowels = set('aeiouAEIOU')
-        a = b = 0
-        i, j = 0, len(s) - 1
-        while i < j:
-            a += s[i] in vowels
-            b += s[j] in vowels
-            i += 1
-            j -= 1
-        return a == b  
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+      int mid=(0+s.size()-1)/2;
+    int count1=0, count2=0;
+    
+    for(int i=0;i<=mid;i++)
+    {
+        if(s[i]=='a'|| s[i]=='e'|| s[i]== 'i'|| s[i]== 'o'|| s[i]=='u'|| s[i]=='A'|| s[i]=='E'|| s[i]=='I'|| s[i]=='O'|| s[i]=='U')
+            count1++;
+    }
+    
+    for(int i=mid+1;i<s.size();i++)
+    {
+        if(s[i]=='a'|| s[i]=='e'||s[i]== 'i'||s[i]== 'o'||s[i]== 'u'||s[i]== 'A'||s[i]== 'E'||s[i]== 'I'||s[i]== 'O'|| s[i]=='U')
+            count2++;
+    }
+    
+    if(count1==count2)
+        return true;
+    else return false;
+    }
+};
