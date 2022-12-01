51c55b1a1cfe1a750a40ecc8eab701c8206ca435
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-01 07:38:45 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (77.35%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -26,7 +26,8 @@ class Solution {
     // }
     // }
     public boolean evaluateTree(TreeNode root) {
-if(root.left == null && root.right == null) return root.val == 0 ? false : true;
+if(root.left == null && root.right == null) 
+    return root.val == 0 ? false : true;
 return root.val == 2 ? evaluateTree(root.left) || evaluateTree(root.right) : evaluateTree(root.left) && evaluateTree(root.right);
 }
 }
