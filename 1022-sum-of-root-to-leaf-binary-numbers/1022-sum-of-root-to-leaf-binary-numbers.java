f67a4db59960c01fa6c269258f700e48b7fd41e3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-02 08:11:18 +0530

    Time: 17 ms (31.22%), Space: 16.6 MB (76.40%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -1,26 +1,22 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-     public int sumRootToLeaf(TreeNode root) {
-        return dfs(root, 0);
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root,0);
     }
-
-    public int dfs(TreeNode root, int val) {
-        if (root == null) return 0;
-        val = val * 2 + root.val;
-        return root.left == root.right ? val : dfs(root.left, val) + dfs(root.right, val);
+   int dfs(TreeNode* root, int val) {
+        if (root == NULL) return 0;
+        val = val * 2 + root->val;
+        return root->left == root->right ? val : dfs(root->left, val) + dfs(root->right, val);
     }
-}
+};
