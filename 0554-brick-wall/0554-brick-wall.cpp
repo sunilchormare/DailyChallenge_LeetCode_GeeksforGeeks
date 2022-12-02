032260809d271cf82dc318857347882915521497
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-02 07:50:37 +0530

    Time: 37 ms (99.54%), Space: 21.7 MB (43.76%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         
-     unordered_map<int, int> edge_frequency;     //HashMap to store the number of common edges among the rows
-
-        int max_frequency = 0;         //Variable to store the frequency of most occuring edge
+     unordered_map<int, int> edge_frequency;    
+        int max_frequency = 0;         
         
-        for(int row=0; row<wall.size(); row++)        //Iterating through each row
+        for(int row=0; row<wall.size(); row++)        
         {
-            int edge_postion = 0;       //Variable to store different edge postion
+            int edge_postion = 0;      
             
-            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    //Iterating through each brick inside a row
+            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    
             { 
-                int current_brick_length = wall[row][brick_no];  //Length of the current brick
+                int current_brick_length = wall[row][brick_no];  
                 
-                edge_postion = edge_postion + current_brick_length ;  //Next Edge Position = Previous Edge Position + Current Brick's Length
+                edge_postion = edge_postion + current_brick_length ;  
                 
-                edge_frequency[edge_postion]++;  //Incrementing the Frequency of just calculated Edge Postion
+                edge_frequency[edge_postion]++;  
                 
-                max_frequency = max(edge_frequency[edge_postion],max_frequency);  //Comparing the "Frequency of just calculated Edge Postion" with "Max Frequency seen till now" & storing whichever is greater.
+                max_frequency = max(edge_frequency[edge_postion],max_frequency);  
             }
         }
-        return wall.size() - max_frequency; // returning (Number of Bricks Crossed by Line) i.e. (Number of Rows in Wall - Frequency of Most Occuring Edge) 
+        return wall.size() - max_frequency; 
     }
 };
