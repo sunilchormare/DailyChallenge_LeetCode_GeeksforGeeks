daf12b97179c3bca6c2750873827571811f5a494
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-02 08:24:41 +0530

    Time: 6 ms (21.76%), Space: 8.5 MB (65.82%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,18 +11,39 @@
  */
 class Solution {
 public:
-     vector<int> postorderTraversal(TreeNode* root) {
+//      vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> nodes;
+//         postorder(root, nodes);
+//         return nodes;
+//     }
+// private:
+//     void postorder(TreeNode* root, vector<int>& nodes) {
+//         if (!root) {
+//             return;
+//         }
+//         postorder(root -> left, nodes);
+//         postorder(root -> right, nodes);
+//         nodes.push_back(root -> val);
+//     }
+    vector<int> postorderTraversal(TreeNode* root) {
         vector<int> nodes;
-        postorder(root, nodes);
-        return nodes;
-    }
-private:
-    void postorder(TreeNode* root, vector<int>& nodes) {
-        if (!root) {
-            return;
+        stack<TreeNode*> todo;
+        TreeNode* last = NULL;
+        while (root || !todo.empty()) {
+            if (root) {
+                todo.push(root);
+                root = root -> left;
+            } else {
+                TreeNode* node = todo.top();
+                if (node -> right && last != node -> right) {
+                    root = node -> right;
+                } else {
+                    nodes.push_back(node -> val);
+                    last = node;
+                    todo.pop();
+                }
+            }
         }
-        postorder(root -> left, nodes);
-        postorder(root -> right, nodes);
-        nodes.push_back(root -> val);
+        return nodes;
     }
 };
