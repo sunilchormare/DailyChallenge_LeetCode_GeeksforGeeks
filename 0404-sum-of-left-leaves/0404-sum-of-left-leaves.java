d600ced3821f782ea58aa54c18664027c25c1208
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-03 09:18:03 +0530

    Time: 1 ms (24.60%), Space: 39.9 MB (95.09%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -1,9 +1,30 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-       if(root==null) return 0;
-        else if(root.left!=null &&root.left.left==null&&root.left.right==null)
-            return root.left.val+sumOfLeftLeaves(root.right);
-        else
-            return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);        
+       // if(root==null) return 0;
+       //  else if(root.left!=null &&root.left.left==null&&root.left.right==null)
+       //      return root.left.val+sumOfLeftLeaves(root.right);
+       //  else
+       //      return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);        
+        
+        //ITERATIVE
+        if(root == null) return 0;
+    int ans = 0;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root);
+    
+    while(!stack.empty()) {
+        TreeNode node = stack.pop();
+        if(node.left != null) {
+            if (node.left.left == null && node.left.right == null)
+                ans += node.left.val;
+            else
+                stack.push(node.left);
+        }
+        if(node.right != null) {
+            if (node.right.left != null || node.right.right != null)
+                stack.push(node.right);
+        }
+    }
+    return ans;
     }
 }
