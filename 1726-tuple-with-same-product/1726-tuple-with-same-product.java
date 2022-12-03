47fca7c79d509ced68768f72514159bfdbb9fb6a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-03 08:31:37 +0530

    Time: 413 ms (75.29%), Space: 148.8 MB (24.71%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -1,36 +1,15 @@
 class Solution {
-public:
-    int tupleSameProduct(vector<int>& nums) {
-        // int res=0;
-        // int n=nums.size();
-        // for(int i=0;i<n;++i)
-        // {
-        //     for(int j=0;j<n;++j)
-        //     {
-        //         for(int k=0;k<n;++k)
-        //         {
-        //             for(int l=0;l<n;++l)
-        //             {
-        //                 if(nums[i]!=nums[j]&&nums[j]!=nums[k]&&nums[k]!=nums[l]&&nums[l]!=nums[i]
-        //                   && nums[l]!=nums[j]&&nums[i]*nums[j]==nums[k]*nums[l])
-        //                     res++;
-        //             }
-        //         }
-        //     }
-        // }
-        // return res;
-        
-        int n = nums.size();
-        unordered_map<int,int> umap;
+    public int tupleSameProduct(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> umap=new HashMap<>();
         int res = 0;
         for(int i = 0 ; i < n ; ++i){
             for(int j = 0 ; j < i ; ++j){
                 int prod = nums[i] * nums[j];
-				/*Every tuple has 8 permutations*/
-                res += 8 * umap[prod];
-                ++umap[prod];
+                res += 8 * umap.getOrDefault(prod,0);
+                umap.put(prod,umap.getOrDefault(prod,0)+1);
             }
         }
         return res;
     }
-};
+}
