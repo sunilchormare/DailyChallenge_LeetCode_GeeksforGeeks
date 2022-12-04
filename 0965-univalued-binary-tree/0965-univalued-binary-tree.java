9006886e31d3e13f0daf8f68f63e341fa32b2c30
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-04 06:13:19 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (99.73%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.java
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -1,23 +1,20 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
- int val = -1;
-    public boolean isUnivalTree(TreeNode root) {
-        if (root == null) return true;
-        if (val < 0) val = root.val;
-        return root.val == val && isUnivalTree(root.left)  && isUnivalTree(root.right);
-}
-}
+public:
+    int val = -1;
+    bool isUnivalTree(TreeNode* root) {
+        if (!root) return true;
+        if (val < 0) val = root->val;
+        return root->val == val && isUnivalTree(root->left) && isUnivalTree(root->right);
+    }
+};
