982d4c6fcf06cd200952f2b3dc50f460c55fdf43
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-04 05:46:43 +0530

    Time: 344 ms (27.02%), Space: 78.4 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,13 +1,14 @@
 class Solution {
-    public int minimumAverageDifference(int[] nums) {
-        int len = nums.length, res = 0;
-	long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        int len = nums.size(), res = 0;
+	long min = LONG_MAX, sum = 0, leftSum = 0, rightSum = 0;
 	for (int num : nums)
 		sum += num;
 	for (int i = 0; i < len; i++) {
 		leftSum += nums[i];
 		rightSum = sum - leftSum;
-		long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
+		long diff = abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
 		if (diff < min) {
 			min = diff;
 			res = i;
@@ -15,4 +16,4 @@ class Solution {
 	}
 	return res;
     }
-}
+};
