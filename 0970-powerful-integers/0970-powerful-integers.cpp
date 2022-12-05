5ffee5fd4f86a3960c00fb58b1671ce29b7012d5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-05 10:02:48 +0530

    Time: 3 ms (51.35%), Space: 6.5 MB (85.59%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.cpp
+++ 0970-powerful-integers/0970-powerful-integers.cpp
@@ -1,13 +1,15 @@
 class Solution {
-    public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        Set<Integer> s = new HashSet<>();
-        for (int i = 1; i < bound; i *= x) {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        unordered_set<int> s;
+        for (int i = 1; i <= bound; i *= x) {
             for (int j = 1; i + j <= bound; j *= y) {
-                s.add(i + j);
+                s.insert(i + j);
                 if (y == 1) break;
             }
             if (x == 1) break;
+
         }
-        return new ArrayList<>(s);
+        return vector<int>(s.begin(), s.end());
     }
-}
+};
