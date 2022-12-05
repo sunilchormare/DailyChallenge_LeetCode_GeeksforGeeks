7ec3da7ee12d4208ef1eb368e008678154ffb281
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-05 08:59:37 +0530

    Time: 66 ms (15.60%), Space: 14 MB (11.63%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,22 +1,12 @@
-// class Solution {
-// public:
-//     ListNode* middleNode(ListNode* head) {
-//         vector<ListNode*> A = {head};
-//         while (A.back()->next != NULL)
-//             A.push_back(A.back()->next);
-//         return A[A.size() / 2];
-//     }
-// };
-
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-};
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow,fast=head,head
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+        return slow
