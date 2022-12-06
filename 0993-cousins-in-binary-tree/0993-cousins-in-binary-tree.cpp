468108946db55d3a9dda22d8a9383e8a6ea7b30a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-06 07:59:56 +0530

    Time: 8 ms (35.80%), Space: 11.1 MB (25.14%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+            int n=q.size();
+             bool f1=false,f2=false;
+            for(int i=0;i<n;i++){
+                TreeNode*tmp=q.front();
+                q.pop();
+                //if values found
+                if(tmp->val==x) f1=true;
+                if(tmp->val==y) f2=true;
+                
+                //checking if parent is same
+                if(tmp->left && tmp->right){
+                if((tmp->left->val==x && tmp->right->val==y)|| (tmp->left->val==y && tmp->right->val==x))
+                    return false;
+                }
+                if(tmp->left)
+                    q.push(tmp->left);
+                if(tmp->right)
+                    q.push(tmp->right);
+            }
+          
+            if(f1&&f2)
+                return true;
+        }
+        
+        return false;
+    }
+};
