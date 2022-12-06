9e1e06191dca7e44c1adf66ca276f37609bc9c37
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-06 07:14:38 +0530

    Time: 5 ms (95.34%), Space: 54.6 MB (73.10%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+     public int subarraysDivByK(int[] A, int K) {
+        int[] map = new int[K];
+		map[0] = 1;
+        int count = 0, sum = 0;
+        for(int a : A) {
+            sum = (sum + a) % K;
+            if(sum < 0) sum += K;  // Because -1 % 5 = -1, but we need the positive mod 4
+            count += map[sum];
+            map[sum]++;
+        }
+        return count;
+    }
+}
