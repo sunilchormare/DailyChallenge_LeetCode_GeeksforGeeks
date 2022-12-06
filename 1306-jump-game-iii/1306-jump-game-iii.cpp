f153ad05cc87218b994d5b96f3d668dfea60e84a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-06 08:04:14 +0530

    Time: 149 ms (16.09%), Space: 30.8 MB (98.14%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    public boolean canReach(int[] A, int i) {
-        return 0 <= i && i < A.length && A[i] >= 0 && ((A[i] = -A[i]) == 0 || canReach(A, i + A[i]) || canReach(A, i - A[i]));
+public:
+     bool canReach(vector<int>& A, int i) {
+        return 0 <= i && i < A.size() && A[i] >= 0 && (!(A[i] = -A[i]) || canReach(A, i + A[i]) || canReach(A, i - A[i]));
     }
-}
+};
