9d385f6abaafbb230f0569be595c17102b1c8e93
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-06 07:55:29 +0530

    Time: 17 ms (18.36%), Space: 13.9 MB (83.77%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+   int res = 0;
+    int distributeCoins(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        int left = dfs(root->left), right = dfs(root->right);
+        res += abs(left) + abs(right);
+        return root->val + left + right - 1;
+    }
+};
