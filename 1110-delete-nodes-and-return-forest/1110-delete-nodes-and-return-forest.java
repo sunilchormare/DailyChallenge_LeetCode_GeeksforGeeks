a5f894a851ac4e6d223145f9a9b869c931ef3e79
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-07 07:06:07 +0530

    Time: 38 ms (69.21%), Space: 25.5 MB (46.06%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -1,22 +1,21 @@
 
 class Solution {
-    Set<Integer> to_delete_set;
-    List<TreeNode> res;
-    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        to_delete_set = new HashSet<>();
-        res = new ArrayList<>();
+public:
+   vector<TreeNode*> result;
+    set<int> to_delete_set;
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         for (int i : to_delete)
-            to_delete_set.add(i);
-        helper(root, true);
-        return res;
+            to_delete_set.insert(i);
+        helper(root, result, to_delete_set, true);
+        return result;
     }
 
-    private TreeNode helper(TreeNode node, boolean is_root) {
-        if (node == null) return null;
-        boolean deleted = to_delete_set.contains(node.val);
-        if (is_root && !deleted) res.add(node);
-        node.left = helper(node.left, deleted);
-        node.right =  helper(node.right, deleted);
-        return deleted ? null : node;
+    TreeNode* helper(TreeNode* node, vector<TreeNode*>& result, set<int>& to_delete_set, bool is_root) {
+        if (node == NULL) return NULL;
+        bool deleted = to_delete_set.find(node->val) != to_delete_set.end();
+        if (is_root && !deleted) result.push_back(node);
+        node->left = helper(node->left, result, to_delete_set, deleted);
+        node->right =  helper(node->right, result, to_delete_set, deleted);
+        return deleted ? NULL : node;
     }
-}
+};
