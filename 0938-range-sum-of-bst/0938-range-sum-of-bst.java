e402139363ad43e206d5eac04748c2629a801193
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-07 06:55:53 +0530

    Time: 41 ms (5.08%), Space: 67.9 MB (14.16%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -1,37 +1,19 @@
+
 class Solution {
-public:
-   // int dfs(TreeNode* root,int low,int high,int sum)
-   // {
-   //     stack<TreeNode*> st;
-   //     //int sum=0;
-   //     st.push(root);
-   //     while(!st.empty())
-   //     {
-   //         TreeNode* node=st.top();
-   //         st.pop();
-   //         if(node->val>=low && node->val<=high)
-   //             sum+=node->val;
-   //         if(node->left!=NULL) st.push(node->left);
-   //         if(node->right!=NULL) st.push(node->right);
-   //         //if(node->left==NULL&&node->right==NULL) st.push_back(node->left);
-   //     }
-   //     return sum;
-   // }
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        //return dfs(root,low,high,0);
-        stack<TreeNode*> st;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+         Stack<TreeNode> st=new Stack<>();
        int sum=0;
-       st.push(root);
+       st.add(root);
        while(!st.empty())
        {
-           TreeNode* node=st.top();
-           st.pop();
-           if(node->val>=low && node->val<=high)
-               sum+=node->val;
-           if(node->left!=NULL) st.push(node->left);
-           if(node->right!=NULL) st.push(node->right);
+           TreeNode node=st.pop();
+           
+           if(node.val>=low && node.val<=high)
+               sum+=node.val;
+           if(node.left!=null) st.add(node.left);
+           if(node.right!=null) st.push(node.right);
            //if(node->left==NULL&&node->right==NULL) st.push_back(node->left);
        }
        return sum;
     }
-};
+}
