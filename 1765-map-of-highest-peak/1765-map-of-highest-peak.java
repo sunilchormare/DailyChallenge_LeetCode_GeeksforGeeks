f4a4f62f38b35631ff4083d176f3160af0d0bf94
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-07 07:23:44 +0530

    Time: 402 ms (89.67%), Space: 108.1 MB (90.00%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,29 +1,37 @@
 class Solution {
-    public int[][] highestPeak(int[][] isWater) {
-        int m = isWater.length, n = isWater[0].length;
-        Queue<int[]> bfs = new LinkedList<>();
-        int[][] height = new int[m][n];
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (isWater[r][c] == 1) {
-                    height[r][c] = 0;
-                    bfs.offer(new int[]{r, c});
-                } else {
-                    height[r][c] = -1;
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int r = isWater.size();
+        int c = isWater[0].size();
+        vector<vector<int>> ans(r,vector<int>(c,INT_MAX));
+        queue<pair<int,int>> q;
+        
+        for(int i = 0;i<r;i++){
+            for(int j = 0;j<c;j++){
+                if(isWater[i][j] == 1){
+                    q.push({i,j});
+                    ans[i][j] = 0;
                 }
             }
         }
-        int[] DIR = new int[]{0, 1, 0, -1, 0};
-        while (!bfs.isEmpty()) {
-            int[] top = bfs.poll();
-            int r = top[0], c = top[1];
-            for (int i = 0; i < 4; i++) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || height[nr][nc] != -1) continue;
-                height[nr][nc] = height[r][c] + 1;
-                bfs.offer(new int[]{nr, nc});
+        
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            int dx[4] = {0,0,-1,1};
+            int dy[4] = {-1,1,0,0};
+            
+            for(int i = 0;i<4;i++){
+                int xx = x + dx[i];
+                int yy = y + dy[i];
+                
+                if(xx>=0 && xx<r && yy>=0 && yy<c && ans[xx][yy] > 1 + ans[x][y]){
+                    ans[xx][yy] = 1 + ans[x][y];
+                    q.push({xx,yy});
+                }
             }
         }
-        return height;
+        return ans; 
     }
-}
+};
