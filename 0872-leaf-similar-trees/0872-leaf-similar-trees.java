a6c8096005aa87bee788a75763dc4010ccc8b725
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-08 08:19:30 +0530

    Time: 8 ms (42.48%), Space: 12.8 MB (63.83%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -1,33 +1,30 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+         stack<TreeNode*> s1 , s2;
         s1.push(root1); s2.push(root2);
         while (!s1.empty() && !s2.empty())
             if (dfs(s1) != dfs(s2)) return false;
         return s1.empty() && s2.empty();
     }
 
-    public int dfs(Stack<TreeNode> s) {
+    int dfs(stack<TreeNode*>& s) {
         while (true) {
-            TreeNode node = s.pop();
-            if (node.right != null) s.push(node.right);
-            if (node.left != null) s.push(node.left);
-            if (node.left == null && node.right == null) return node.val;
-        }
+            TreeNode* node = s.top(); s.pop();
+            if (node->right) s.push(node->right);
+            if (node->left) s.push(node->left);
+            if (!node->left && !node->right) return node->val;
+        }  
     }
-}
+};
