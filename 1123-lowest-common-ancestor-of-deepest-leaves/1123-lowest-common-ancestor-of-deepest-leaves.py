b33363a7605fa7cc5f9a99b4391abcf920d34448
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-08 09:47:34 +0530

    Time: 98 ms (54.28%), Space: 14.4 MB (34.39%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lcaDeepestLeaves(self, root):
+        def helper(root):
+            if not root: return 0, None
+            h1, lca1 = helper(root.left)
+            h2, lca2 = helper(root.right)
+            if h1 > h2: return h1 + 1, lca1
+            if h1 < h2: return h2 + 1, lca2
+            return h1 + 1, root
+        return helper(root)[1]
+        
