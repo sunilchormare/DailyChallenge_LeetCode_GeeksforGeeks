4f54ccdc8e86da9188055ce102fa9bee0d7d1836
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-08 08:55:58 +0530

    Time: 367 ms (10.84%), Space: 77.2 MB (90.09%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,14 +1,15 @@
 class Solution {
+public:
     long res = 0, total = 0, sub;
-    public int maxProduct(TreeNode root) {
-        total = s(root); s(root);
-        return (int)(res % (int)(1e9 + 7));
+    int maxProduct(TreeNode* root) {
+        total = s(root), s(root);
+        return res % (int)(1e9 + 7);
     }
 
-    private long s(TreeNode root) {
-        if (root == null) return 0;
-        sub = root.val + s(root.left) + s(root.right);
-        res = Math.max(res, sub * (total - sub));
+    int s(TreeNode* root) {
+        if (!root) return 0;
+        sub = root->val + s(root->left) + s(root->right);
+        res = max(res, sub * (total - sub));
         return sub;
     }
-}
+};
