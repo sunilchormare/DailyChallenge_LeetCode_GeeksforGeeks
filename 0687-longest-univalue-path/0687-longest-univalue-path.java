2506a9eedb52e85352e6d703ecebb804322c18aa
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-08 08:43:59 +0530

    Time: 2 ms (100.00%), Space: 45.9 MB (90.43%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    int len = 0; 
+public int longestUnivaluePath(TreeNode root) {
+    if (root == null) return 0;
+    len = 0;
+    getLen(root, root.val);
+    return len;
+}
+
+private int getLen(TreeNode node, int val) {
+    if (node == null) return 0;
+    int left = getLen(node.left, node.val);
+    int right = getLen(node.right, node.val);
+    len = Math.max(len, left + right);
+    if (val == node.val)  return Math.max(left, right) + 1;
+    return 0;
+}
+}
