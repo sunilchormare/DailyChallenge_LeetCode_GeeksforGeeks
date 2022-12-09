b853b2812dbef1135c8c8762d9bdbbf81d1dce73
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-09 07:46:44 +0530

    Time: 19 ms (93.68%), Space: 24.6 MB (76.61%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.cpp
@@ -1,23 +1,22 @@
 
 class Solution {
-    Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+public:
+     unordered_map<int, int> count;
     int maxCount = 0;
-
-    public int[] findFrequentTreeSum(TreeNode root) {
+    vector<int> findFrequentTreeSum(TreeNode* root) {
         dfs(root);
-        List<Integer> res = new ArrayList<>();
-        for (int s : count.keySet()) {
-            if (count.get(s) == maxCount)
-                res.add(s);
-        }
-        return res.stream().mapToInt(i->i).toArray();
+        vector<int> res;
+        for (auto & e: count)
+            if (e.second == maxCount)
+                res.push_back(e.first);
+        return res;
     }
 
-    private int dfs(TreeNode root) {
-        if (root == null) return 0;
-        int s = dfs(root.left) + dfs(root.right) + root.val;
-        count.put(s, count.getOrDefault(s, 0) + 1);
-        maxCount = Math.max(maxCount, count.get(s));
+
+    int dfs(TreeNode* root) {
+        if (root == NULL) return 0;
+        int s = dfs(root->left) + dfs(root->right) + root->val;
+        maxCount = max(maxCount, ++count[s]);
         return s;
     }
-}
+};
