5da24eb170aed16d472b5dfc260d76854980fc80
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-09 07:09:55 +0530

    Time: 3 ms (97.10%), Space: 9.6 MB (97.32%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -1,13 +1,15 @@
 
 class Solution {
-    public int maxAncestorDiff(TreeNode root) {
-        return dfs(root, root.val, root.val);
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root,root->val,root->val);
     }
-
-    public int dfs(TreeNode root, int mn, int mx) {
-        if (root == null) return mx - mn;
-        mx = Math.max(mx, root.val);
-        mn = Math.min(mn, root.val);
-        return Math.max(dfs(root.left, mn, mx), dfs(root.right, mn, mx));
-     }
-}
+    int dfs(TreeNode* root,int mini,int maxi)
+    {
+        if(root==NULL) return maxi-mini;
+        maxi=max(root->val,maxi);
+        mini=min(root->val,mini);
+        return max(dfs(root->left,mini,maxi),dfs(root->right,mini,maxi));
+        
+    }
+};
