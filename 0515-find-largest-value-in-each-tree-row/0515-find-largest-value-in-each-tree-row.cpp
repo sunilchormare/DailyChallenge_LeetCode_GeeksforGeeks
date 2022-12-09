42fbfce54fc15ab286bd0d6e5c8ecf4e8e0b0f97
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-09 07:38:34 +0530

    Time: 15 ms (87.30%), Space: 22.2 MB (70.86%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,24 +1,43 @@
 
 class Solution {
 public:
+//     vector<int> largestValues(TreeNode* root) {
+//         queue<TreeNode*> q;
+//         vector<int> values;
+        
+//         if(root != NULL) q.push(root);
+        
+//         while(!q.empty()) {
+//             int maxi =INT_MIN, n = q.size();
+//             for(int i = 0; i < n; i++) {
+//                 TreeNode* node = q.front();
+//                  q.pop();
+//                 maxi = max(maxi, node->val);
+//                 if(node->left != NULL) q.push(node->left);
+//                 if(node->right != NULL) q.push(node->right);
+//             }
+//             values.push_back(maxi);
+//         }
+        
+//         return values;
+//     }
+    public:
     vector<int> largestValues(TreeNode* root) {
-        queue<TreeNode*> q;
-        vector<int> values;
-        
-        if(root != NULL) q.push(root);
-        
-        while(!q.empty()) {
-            int maxi =INT_MIN, n = q.size();
-            for(int i = 0; i < n; i++) {
-                TreeNode* node = q.front();
-                 q.pop();
-                maxi = max(maxi, node->val);
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
-            }
-            values.push_back(maxi);
+        vector<int> res;
+        dfs(root, 1, res);
+        return res;
+    }
+
+private:
+    void dfs(TreeNode* node, int depth, vector<int>& res) {
+        if (!node) {
+            return;
         }
-        
-        return values;
+        dfs(node->left, depth + 1, res);
+        dfs(node->right, depth + 1, res);
+        if (depth > res.size()) {
+            res.resize(depth, INT_MIN);
+        }
+        res[depth - 1] = max(res[depth - 1], node->val);
     }
 };
