315ad1aa63b36d0b53d5cebb3549ddfb576301ee
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-12 07:21:13 +0530

    Time: 2 ms (99.93%), Space: 40.3 MB (85.32%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
-    vector<int> selfDividingNumbers(int left, int right) {
-        vector<int> res;
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
         for (int i = left; i <= right; i++)
-            if (dividingNumber(i)) res.push_back(i);
+            if (dividingNumber(i)) res.add(i);
         return res;
     }
 
-    bool dividingNumber(int num) {
+    boolean dividingNumber(int num) {
         for (int n = num; n > 0; n /= 10)
-            if (!(n % 10) || num % (n % 10)) return false;
+            if (n % 10 == 0 || num % (n % 10) != 0) return false;
         return true;
     }
-};
+}
