c31749bcaca39a10d1d66919da6a5e859439b2f9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-13 06:42:57 +0530

    Time: 22 ms (93.75%), Space: 43.1 MB (94.17%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -1,25 +1,16 @@
- //Stern Brocot Tree BFS
-
 class Solution {
-public:
-    vector<string> simplifiedFractions(int n) {
-      if (n==1) return {};
-        queue<tuple<int,int,int,int>> q;
-        vector<string> res;
-        q.emplace(0, 1, 1, 1);
-        while(!q.empty()) {
-            auto [num1, den1, num2, den2] = q.front();
-            int num = num1 + num2;
-            int den = den1 + den2;
-            q.pop();
-            res.push_back(to_string(num)+"/"+to_string(den));
-            if (den + den1 <= n) {
-                q.emplace(num1,den1,num,den);
-            }
-            if (den + den2 <= n) {
-                q.emplace(num,den,num2,den2);
+    public List<String> simplifiedFractions(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int denominator = 2; denominator <= n; ++denominator) {
+            for (int numerator = 1; numerator < denominator; ++numerator) {
+                if (gcd(numerator, denominator) == 1) {
+                    ans.add(numerator + "/" + denominator);
+                }
             }
         }
-        return res;   
+        return ans;
     }
-};
+    private int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y % x, x);
+    }
+}
