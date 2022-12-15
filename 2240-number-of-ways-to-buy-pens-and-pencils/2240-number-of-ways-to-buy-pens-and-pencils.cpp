b1c5181ba45c741f39c94f661c1dbe4deb7742ec
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-15 06:52:15 +0530

    Time: 20 ms (73.83%), Space: 5.9 MB (78.51%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -1,8 +1,9 @@
 class Solution {
-    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        long res = 0;
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+       long  long res = 0;
     for (int cnt = 0; cnt * cost1 <= total; ++cnt)
         res += (total - cnt * cost1) / cost2 + 1;
     return res;
     }
-}
+};
