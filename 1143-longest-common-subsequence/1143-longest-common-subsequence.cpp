3a49c01ef44b75b6dd29dd8bf114b7c68e479549
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-15 06:29:53 +0530

    Time: 13 ms (92.59%), Space: 10.7 MB (79.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-    //    short m[1001][1001] = {};
-    // for (auto i = 0; i < a.size(); ++i)
-    //     for (auto j = 0; j < b.size(); ++j)
-    //         m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
-    // return m[a.size()][b.size()];
+    //RECURSIVE
+//     int LCS(string x,string y,int n,int m)
+//     {
+//         if(m==0||n==0) return 0;
+//         if (x[n - 1] == y[m - 1])
+// 		return 1 + LCS(x, y, n - 1, m - 1); 
+// 	else
+// 		return max(LCS(x, y, n - 1, m), LCS(x, y, n, m - 1)); 
         
-        vector<vector<int>> dp(text1.length()+1,vector<int>(text2.length() + 1,0));
-        for (int i = 1; i <= text1.length(); i++) {
-            for (int j = 1; j <= text2.length(); j++) {
-                if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] =max(dp[i - 1][j], dp[i][j - 1]);
-            }
+//     }
+    
+    //TOP -DOWN DP - MEMOIZAION 
+     
+    int LCS(string X,string Y,int n,int m){
+//        int dp[1001][1001]={{-1}};
+//         if (n == 0 || m == 0)    
+// 		    dp[n][m] = 0;
+
+// 	if (dp[n][m] != -1)
+// 		return dp[n][m];
+
+ 
+// 	if (X[n - 1] == Y[m - 1])
+// 		dp[n][m] = 1 + LCS(X, Y, n - 1, m - 1);
+// 	else
+// 		dp[n][m] = max(LCS(X, Y, n - 1, m), LCS(X, Y, n, m - 1)); 
+
+// 	return dp[n][m];
+        int dp[n + 1][m + 1]; 
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= m; j++)
+			if (i == 0 || j == 0)
+				dp[i][j] = 0;
+	for (int i = 1; i <= n; i++)
+    {
+		for (int j = 1; j <= m; j++)
+        {
+			if (X[i - 1] == Y[j - 1]) 
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			 else
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            
         }
-        return dp[text1.length()][text2.length()];
+    }
+
+	return dp[n][m]; 
+}
+    int longestCommonSubsequence(string text1, string text2) {
+        return LCS(text1,text2,text1.size(),text2.size());
     }
 };
