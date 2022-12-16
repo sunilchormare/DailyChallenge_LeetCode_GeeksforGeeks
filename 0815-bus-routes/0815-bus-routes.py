5df526b8edc718513b1414ce91f62d2279148786
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-16 06:34:53 +0530

    Time: 492 ms (90.10%), Space: 56.8 MB (7.02%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,34 +1,17 @@
-class Solution {
-    public int numBusesToDestination(int[][] routes, int S, int T) {
-        int n = routes.length;
-        HashMap<Integer, HashSet<Integer>> to_routes = new HashMap<>();
-        for (int i = 0; i < routes.length; ++i) {
-            for (int j : routes[i]) {
-                if (!to_routes.containsKey(j))
-                    to_routes.put(j, new HashSet<Integer>());
-                to_routes.get(j).add(i);
-            }
-        }
-        Queue<int[]> bfs = new ArrayDeque();
-        bfs.offer(new int[] {S, 0});
-        HashSet<Integer> seen = new HashSet<>();
-        seen.add(S);
-        boolean[] seen_routes = new boolean[n];
-        while (!bfs.isEmpty()) {
-            int stop = bfs.peek()[0], bus = bfs.peek()[1];
-            bfs.poll();
-            if (stop == T) return bus;
-            for (int i : to_routes.get(stop)) {
-                if (seen_routes[i]) continue;
-                for (int j : routes[i]) {
-                    if (!seen.contains(j)) {
-                        seen.add(j);
-                        bfs.offer(new int[] {j, bus + 1});
-                    }
-                }
-                seen_routes[i] = true;
-            }
-        }
-        return -1;
-    }
-}
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], S: int, T: int) -> int:
+        to_routes = collections.defaultdict(set)
+        for i, route in enumerate(routes):
+            for j in route:
+                to_routes[j].add(i)
+        bfs = [(S, 0)]
+        seen = set([S])
+        for stop, bus in bfs:
+            if stop == T: return bus
+            for i in to_routes[stop]:
+                for j in routes[i]:
+                    if j not in seen:
+                        bfs.append((j, bus + 1))
+                        seen.add(j)
+                routes[i] = []  # seen route
+        return -1
