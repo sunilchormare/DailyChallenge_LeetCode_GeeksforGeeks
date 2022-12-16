485cacf412b16cdbb04dbd0cf6c382254f98bbde
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-16 06:42:05 +0530

    Time: 339 ms (14.13%), Space: 13.3 MB (50.79%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    public int findRotateSteps(String ring, String key) {
-        int n = ring.length();
-        int m = key.length();
-        int[][] dp = new int[m + 1][n];
+public:
+    int findRotateSteps(string ring, string key) {
+         int n = ring.size();
+        int m = key.size();
+        vector<vector<int>> dp(m+1,vector<int>(n,0));
         
         for (int i = m - 1; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
+                dp[i][j] = INT_MAX;
                 for (int k = 0; k < n; k++) {
-                    if (ring.charAt(k) == key.charAt(i)) {
-                        int diff = Math.abs(j - k);
-                        int step = Math.min(diff, n - diff);
-                        dp[i][j] = Math.min(dp[i][j], step + dp[i + 1][k]);
+                    if (ring[k] == key[i]) {
+                        int diff = abs(j - k);
+                        int step = min(diff, n - diff);
+                        dp[i][j] = min(dp[i][j], step + dp[i + 1][k]);
                     }
                 }
             }
         }
         
-        return dp[0][0] + m; 
+        return dp[0][0] + m;
     }
-}
+};
