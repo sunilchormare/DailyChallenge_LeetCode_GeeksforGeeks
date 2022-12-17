b1d227fddacdd145414ca49148941f9433d34cfc
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-17 07:16:38 +0530

    Time: 11 ms (81.39%), Space: 9.6 MB (40.04%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();++i)
-        {
-            m[nums[i]]++;
-        }
-        for(int i=0;i<nums.size();++i)
-        {
-            if(m[nums[i]]==1)
-                return nums[i];
-        }
-        return 0;
+        // unordered_map<int,int> m;
+        // for(int i=0;i<nums.size();++i)
+        // {
+        //     m[nums[i]]++;
+        // }
+        // for(int i=0;i<nums.size();++i)
+        // {
+        //     if(m[nums[i]]==1)
+        //         return nums[i];
+        // }
+        // return 0;
+        
+        int res = 0;
+	for (int i = 0; i < 32; i++) {
+		int sum = 0;
+		int mask = 1 << i;
+		for (auto n : nums) 
+			if (n & mask) 
+				sum++;
+		if (sum % 3 == 1)
+			res |= mask;
+	}
+	return res;
     }
 };
