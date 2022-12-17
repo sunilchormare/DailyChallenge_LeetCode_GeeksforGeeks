1972278a677dc50200130fa614c1a120f87efc63
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-17 07:04:28 +0530

    Time: 23 ms (82.95%), Space: 25.9 MB (97.26%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = size(inorder), postIdx = n-1;
+        return build(inorder, postorder, 0, n-1, postIdx);
+    }
+
+    TreeNode* build(vector<int>& in, vector<int>& post, int inStart, int inEnd, int& postIdx) {
+        if(inStart > inEnd) return nullptr;
+        TreeNode* root = new TreeNode(post[postIdx--]);
+        int inIdx = find(begin(in), end(in), root -> val) - begin(in);
+        root -> right = build(in, post, inIdx+1, inEnd, postIdx);
+        root -> left  = build(in, post, inStart, inIdx-1, postIdx);
+        return root;
+    }
+};
