9db4e29383a837ccec0014abe1bd88fe5a112d56
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-17 06:54:37 +0530

    Time: 63 ms (63.56%), Space: 21 MB (5.33%) - LeetHub

--- 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.java
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.java
@@ -1,26 +1,28 @@
 class Solution {
-     private static final int m = 1_000_000_007;
-    public int numWays(String s) {
-        int ones = 0, n = s.length();
-        for (int i = 0; i < n; ++i) {
-            ones += s.charAt(i) - '0';
+public:
+    int numWays(string s) {
+        long n=s.size();
+        vector<int>c(n,0);
+        long sum=0,zero=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=(s[i]-'0');
+            c[i]=sum;
+            if(s[i]-'0'==0)zero++;
         }
-        if (ones == 0) {
-            return (int)((n - 2L) * (n - 1L) / 2 % m);
+        
+        if(c[n-1]%3!=0)return 0;
+        if(zero==n){
+            return (n-1)*(n-2)/2%(1000000007);
         }
-        if (ones % 3 != 0) {
-            return 0;
+        
+        long dif=c[n-1]/3;
+        long l=0,r=0;
+        for(int i=0;i<n;i++){
+            if(c[i]==dif)l++;
+            if(c[n-1]-c[i]==dif)r++;
         }
-        int onesInEachSplitedBlock = ones / 3;
-        long waysOfFirstCut = 0, waysOfSecondCut = 0;
-        for (int i = 0, count = 0; i < n; ++i) {
-            count += s.charAt(i) - '0';
-            if (count == onesInEachSplitedBlock) {
-                ++waysOfFirstCut;
-            }else if (count == 2 * onesInEachSplitedBlock) {
-                ++waysOfSecondCut;
-            }
-        }
-        return (int)(waysOfFirstCut * waysOfSecondCut % m);  
+        
+       return l*r%(1000000007); 
     }
-}
+};
