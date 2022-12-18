f3da6af28ff42906a3ea19de911a39b73338536d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-18 07:57:19 +0530

    Time: 60 ms (78.44%), Space: 6.7 MB (87.13%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,23 +1,26 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
-          boolean[][] match=new boolean[s.length()+1][p.length()+1];
-        match[s.length()][p.length()]=true;
-        for(int i=p.length()-1;i>=0;i--){
-            if(p.charAt(i)!='*')
-                break;
-            else
-                match[s.length()][i]=true;
+public:
+    bool isMatch(string s, string p) {
+         vector<bool> dp(p.size() + 1, false);
+        dp[0] = true;
+        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+            dp[j + 1] = true;
         }
-        for(int i=s.length()-1;i>=0;i--){
-            for(int j=p.length()-1;j>=0;j--){
-                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
-                        match[i][j]=match[i+1][j+1];
-                else if(p.charAt(j)=='*')
-                        match[i][j]=match[i+1][j]||match[i][j+1];
-                else
-                    match[i][j]=false;
+        
+        for (int i = 1; i <= s.size(); ++i) {
+            dp[0] = false;
+            bool neighborLastRow = i == 1;
+            for (int j = 1; j <= p.size(); ++j) {
+                bool currLastRow = dp[j];
+                if (p[j - 1] == '*') {
+                    dp[j] = currLastRow || dp[j - 1];
+                } else {
+                    dp[j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && neighborLastRow;
+                }
+                neighborLastRow = currLastRow;
             }
         }
-        return match[0][0];
+
+        return dp[p.size()];
     }
-}
+};
