1b7afef4308793cff1a3b61b8a0fcb727d720b3c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-22 22:08:36 +0530

    Time: 11 ms (84.26%), Space: 14 MB (49.68%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+public:
+      void fill(vector<vector<int>>& image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
+    }
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) 
+    {
+        
+     if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+        
+        
+    }
+};
