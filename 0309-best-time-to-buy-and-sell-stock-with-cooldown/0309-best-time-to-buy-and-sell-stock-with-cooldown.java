3d5be73e5a302f21036badaa4053db4525275ab9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-23 09:16:23 +0530

    Time: 1 ms (90.22%), Space: 42.3 MB (42.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,21 +1,20 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-      if (prices.size() < 2) return 0;
+    public int maxProfit(int[] prices) {
+         if (prices.length < 2) return 0;
        
         int s0 = 0, s1 = -prices[0], s2 = 0;
-        for (int i = 1; i < prices.size(); ++i) {
+        for (int i = 1; i < prices.length ;++i) {
             int last_s2 = s2;
-           // cout<<"i= "<<i<<" last_s2 : "<<last_s2<<" ";
+            //cout<<"i= "<<i<<" last_s2 : "<<last_s2<<" ";
             s2 = s1 + prices[i];
             //cout<<" s2 : "<<s2<<" ";
-            s1 = max(s0 - prices[i], s1);
-           // cout<<" s1 : "<<s1<<" ";
+            s1 = Math.max(s0 - prices[i], s1);
+            //cout<<" s1 : "<<s1<<" ";
             
-            s0 = max(s0, last_s2);
-            // cout<<"s0 : "<<s0<<" ";
-            // cout<<endl;
+            s0 = Math.max(s0, last_s2);
+            //cout<<"s0 : "<<s0<<" ";
+            //cout<<endl;
         }
-        return max(s0, s2);
+        return Math.max(s0, s2);
     }
-};
+}
