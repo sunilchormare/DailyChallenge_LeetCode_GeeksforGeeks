23828ea7399f0712458221b183be9e3c3530d05c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-24 07:47:58 +0530

    Time: 86 ms (79.49%), Space: 28 MB (72.82%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,13 +3,17 @@ public:
     int numEnclaves(vector<vector<int>>& A, int res = 0) {
   queue<pair<int, int>> q;
   for (auto i = 0; i < A.size(); ++i)
-    for (auto j = 0; j < A[0].size(); ++j) {
+    for (auto j = 0; j < A[0].size(); ++j) 
+    {
       res += A[i][j];
-      if (i * j == 0 || i == A.size() - 1 || j == A[i].size() - 1) q.push({ i, j });
+      if (i * j == 0 || i == A.size() - 1 || j == A[i].size() - 1) 
+          q.push({ i, j });
     }
-  while (!q.empty()) {
+  while (!q.empty()) 
+  {
     auto x = q.front().first, y = q.front().second; q.pop();
-    if (x < 0 || y < 0 || x == A.size() || y == A[x].size() || A[x][y] != 1) continue;
+    if (x < 0 || y < 0 || x == A.size() || y == A[x].size() || A[x][y] != 1) 
+        continue;
     A[x][y] = 0;
     --res;
     q.push({ x + 1, y }), q.push({ x - 1, y }), q.push({ x, y + 1 }), q.push({ x, y - 1 });
