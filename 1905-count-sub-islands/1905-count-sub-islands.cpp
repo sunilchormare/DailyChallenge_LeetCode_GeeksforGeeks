0858fc4812d60cdd4ef235b26bb4e68897d781c4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-24 07:55:46 +0530

    Time: 315 ms (97.98%), Space: 88.3 MB (93.93%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,6 +1,7 @@
 class Solution {
-     public int countSubIslands(int[][] B, int[][] A) {
-        int m = A.length, n = A[0].length, res = 0;
+public:
+    int countSubIslands(vector<vector<int>>& B, vector<vector<int>>& A) {
+        int m = A.size(), n = A[0].size(), res = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
                 if (A[i][j] == 1)
@@ -9,8 +10,8 @@ class Solution {
     }
 
 
-    private int dfs(int[][] B, int[][] A, int i, int j) {
-        int m = A.length, n = A[0].length, res = 1;
+    int dfs(vector<vector<int>>& B, vector<vector<int>>& A, int i, int j) {
+        int m = A.size(), n = A[0].size(), res = 1;
         if (i < 0 || i == m || j < 0 || j == n || A[i][j] == 0) return 1;
         A[i][j] = 0;
         res &= dfs(B, A, i - 1, j);
@@ -19,4 +20,4 @@ class Solution {
         res &= dfs(B, A, i, j + 1);
         return res & B[i][j];
     }
-}
+};
