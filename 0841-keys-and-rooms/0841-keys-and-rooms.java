66ac8bcc9f09a9fb8efb8d3e873ccbbe410e06b5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-28 07:23:52 +0530

    Time: 17 ms (54.85%), Space: 10.7 MB (20.00%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,16 +1,17 @@
 class Solution {
-    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Stack<Integer> dfs = new Stack<>(); dfs.add(0);
-        HashSet<Integer> seen = new HashSet<Integer>(); seen.add(0);
-        while (!dfs.isEmpty()) {
-            int i = dfs.pop();
-            for (int j : rooms.get(i))
-                if (!seen.contains(j)) {
-                    dfs.add(j);
-                    seen.add(j);
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        stack<int> dfs; dfs.push(0);
+        unordered_set<int> seen = {0};
+        while (!dfs.empty()) {
+            int i = dfs.top(); dfs.pop();
+            for (int j : rooms[i])
+                if (seen.count(j) == 0) {
+                    dfs.push(j);
+                    seen.insert(j);
                     if (rooms.size() == seen.size()) return true;
                 }
         }
         return rooms.size() == seen.size();
     }
-}
+};
