633ab6a070951d8d8f6e2ab62cae73e4d51686a0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-28 07:49:13 +0530

    Time: 648 ms (41.90%), Space: 0B (100.00%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,7 @@
+select t.Request_at Day,
+       ROUND((count(IF(t.status!='completed',TRUE,null))/count(*)),2) as 'Cancellation Rate'
+from Trips t where 
+t.Client_Id in (Select Users_Id from Users where Banned='No') 
+and t.Driver_Id in (Select Users_Id from Users where Banned='No')
+and t.Request_at between '2013-10-01' and '2013-10-03'
+group by t.Request_at;
