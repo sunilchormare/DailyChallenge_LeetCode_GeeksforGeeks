8084e2293704f22d30f8640fa29ee457a0647d78
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-28 07:06:22 +0530

    Time: 538 ms (93.45%), Space: 98.8 MB (94.14%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,16 +1,28 @@
 class Solution {
-     public int minStoneSum(int[] A, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)->b - a);
-        int res = 0;
-        for (int a : A) {
-            pq.add(a);
-            res += a;
-        }
-        while (k-- > 0) {
-            int a = pq.poll();
-            pq.add(a - a / 2);
+public:
+    //int minStoneSum(vector<int>& piles, int k) {
+//         int sum=0,n=piles.size();
+//         //if(k>n) return 1;
+//         sort(piles.begin(),piles.end());
+        
+//             for(int i=n-1;i>=0,k>0;i--,k--)
+//                 piles[i]=piles[i]-floor(piles[i]/2);
+//           for(int i=0;i<n;++i)
+//               sum+=piles[i];
+        
+//         return sum;
+//}  
+        
+        int minStoneSum(vector<int>& A, int k) {
+        priority_queue<int> pq(A.begin(), A.end());
+        int res = accumulate(A.begin(), A.end(), 0);
+        while (k--) {
+            int a = pq.top();
+            pq.pop();
+            pq.push(a - a / 2);
             res -= a / 2;
         }
         return res;
     }
-}
+    
+};
