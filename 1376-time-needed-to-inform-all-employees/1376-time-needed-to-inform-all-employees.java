4095b343a825ced0c76db11ab0cdb80f7caf1703
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-30 06:29:38 +0530

    Time: 171 ms (98.64%), Space: 90.7 MB (95.92%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,15 +1,17 @@
 class Solution {
-     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+public:
+   int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int res = 0;
         for (int i = 0; i < n; ++i)
-            res = Math.max(res, dfs(i, manager, informTime));
+            res = max(res, dfs(i, manager, informTime));
         return res;
     }
-    public int dfs(int i, int[] manager, int[] informTime) {
+
+    int dfs(int i, vector<int>& manager, vector<int>& informTime) {
         if (manager[i] != -1) {
             informTime[i] += dfs(manager[i], manager, informTime);
             manager[i] = -1;
         }
         return informTime[i];
     }
-}
+};
