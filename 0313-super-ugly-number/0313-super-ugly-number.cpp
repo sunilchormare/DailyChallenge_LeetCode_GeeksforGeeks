b8a18d3e05adb3c2b0c7db891fa3d592272afc08
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2022-12-30 07:30:13 +0530

    Time: 739 ms (34.70%), Space: 33.8 MB (26.97%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-      vector<int> vec(n,0);
-        vec[0]=1;
-        int total_primes=primes.size();
-        vector<int> pointers(total_primes,0);
-        for(int i=1;i<n;i++){
-            long long mn=INT_MAX;
-            for(int j=0;j<total_primes;j++){
-                mn=min(mn,(long long)primes[j]*(long long)vec[pointers[j]]);
-                
-            }
-            vec[i]=mn;
-            for(int j=0;j<total_primes;j++){
-                if(mn == (long long)primes[j] * (long long)vec[pointers[j]])
+     vector<long long> ind(primes.size(),0);
+        vector<long long> res;
+        unordered_map<long long,int> m;
+        res.push_back(1);
+        while(res.size()<n)
+        {
+            long long t=INT_MAX;
+            int p=0;
+            for(int i=0;i<primes.size();i++)
+            {
+                if(t>primes[i]*res[ind[i]])
                 {
-                    pointers[j]++;
+                    t=primes[i]*res[ind[i]];
+                    p=i;
                 }
             }
+            ind[p]++;
+            if(m.find(t)==m.end())
+            {
+                res.push_back(t);
+                m[t]=1;
+            }
         }
-        return vec[n-1];
+        return res[n-1];
     }
 };
