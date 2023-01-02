6980fd95984a3c874f510414630213f6eeac13b1
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-02 05:57:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.62%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    public int integerReplacement(int n) {
-        if (n == Integer.MAX_VALUE) return 32; //n = 2^31-1;
+public:
+    int integerReplacement(int n) {
+        if (n == INT_MAX) return 32;
     int count = 0;
     while (n > 1){
         if (n % 2 == 0) n  /= 2;
         else{
-            if ( (n + 1) % 4 == 0 && (n - 1 != 2) ) n++;
+            if( (n + 1) % 4 == 0 && (n - 1 != 2) ) n++;
             else n--;
         }
         count++;
     }
     return count;
     }
-}
+};
