fbc64998d6c4db986b44f07ca229ae4bbef7c864
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-02 06:06:35 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (83.92%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,30 +1,36 @@
 class Solution {
-public:
-    string decodeString(const string& s, int& i) {
-        string res;
-        
-        while (i < s.length() && s[i] != ']') {
-            if (!isdigit(s[i]))
-                res += s[i++];
-            else {
-                int n = 0;
-                while (i < s.length() && isdigit(s[i]))
-                    n = n * 10 + s[i++] - '0';
-                    
-                i++; // '['
-                string t = decodeString(s, i);
-                i++; // ']'
-                
-                while (n-- > 0)
-                    res += t;
+    public String decodeString(String s) {
+    int[] i = new int[1];
+    i[0] = 0;
+    StringBuilder str = new StringBuilder(s);
+    return decodeString(str, i);
+}
+
+private String decodeString(StringBuilder s, int[] i) {
+    int n = 0;
+    StringBuilder result = new StringBuilder();
+
+    while (i[0] < s.length()) { 
+        if (Character.isDigit(s.charAt(i[0]))) {
+            while (Character.isDigit(s.charAt(i[0]))) {
+                n = n * 10 + s.charAt(i[0]) - '0';
+                i[0]++;
             }
+            i[0]++; // '['
+            String nested = decodeString(s, i);
+
+            while (n > 0) {
+                result.append(nested);
+                n--;
+            }
+        } else if (Character.isLetter(s.charAt(i[0]))) {
+            result.append(s.charAt(i[0]++));
+        } else if (s.charAt(i[0]) == ']') {
+            i[0]++;
+            return result.toString();
         }
-        
-        return res;
     }
 
-    string decodeString(string s) {
-        int i = 0;
-        return decodeString(s, i);
-    }
-};
+    return result.toString();
+}
+}
