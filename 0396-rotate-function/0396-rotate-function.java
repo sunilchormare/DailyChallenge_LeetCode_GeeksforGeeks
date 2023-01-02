229edf854291b46092d07356ff8119d2f7ab6576
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-02 06:02:17 +0530

    Time: 3 ms (100.00%), Space: 54.8 MB (72.82%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.java
+++ 0396-rotate-function/0396-rotate-function.java
@@ -1,18 +1,17 @@
 class Solution {
-public:
-    int maxRotateFunction(vector<int>& A) {
-        int allSum = 0;
-int len = A.size();
+    public int maxRotateFunction(int[] A) {
+       int allSum = 0;
+int len = A.length;
 int F = 0;
 for (int i = 0; i < len; i++) {
     F += i * A[i];
     allSum += A[i];
 }
-int maxi = F;
+int max = F;
 for (int i = len - 1; i >= 1; i--) {
     F = F + allSum - len * A[i];
-    maxi = max(F, maxi);
+    max = Math.max(F, max);
 }
-return maxi;
+return max; 
     }
-};
+}
