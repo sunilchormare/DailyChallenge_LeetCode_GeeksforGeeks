dfaa79f7790fecf96209392090a12e71fcd9329b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-03 07:39:28 +0530

    Time: 570 ms (44.85%), Space: 127.7 MB (94.02%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+  vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+    int i = 0, j = 0, cur_d = 0, d[5] = {0, 1, 0, -1, 0};
+    vector<vector<int>> res(m, vector<int>(n, -1));
+    for (; head != nullptr; head = head->next) {
+        res[i][j] = head->val;
+        int ni = i + d[cur_d], nj = j + d[cur_d + 1];
+        if (min(ni, nj) < 0 || ni >= m || nj >= n || res[ni][nj] != -1)
+            cur_d = (cur_d + 1) % 4;
+        i += d[cur_d];
+        j += d[cur_d + 1];
+    }
+    return res;
+}
+};
