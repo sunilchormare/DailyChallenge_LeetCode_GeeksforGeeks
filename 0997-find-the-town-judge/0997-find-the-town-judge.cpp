4858ba6951e6d2717120637898b6ff554249686c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-03 07:33:39 +0530

    Time: 320 ms (48.33%), Space: 60.7 MB (85.53%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,13 +1,12 @@
 class Solution {
-  public int findJudge(int N, int[][] trust) {
-        int[] count = new int[N+1];
-        for (int[] t: trust) {
-            count[t[0]]--;
-            count[t[1]]++;
-        }
+public:
+   int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> count(N + 1, 0);
+        for (auto& t : trust)
+            count[t[0]]--, count[t[1]]++;
         for (int i = 1; i <= N; ++i) {
             if (count[i] == N - 1) return i;
         }
         return -1;
     }
-}
+};
