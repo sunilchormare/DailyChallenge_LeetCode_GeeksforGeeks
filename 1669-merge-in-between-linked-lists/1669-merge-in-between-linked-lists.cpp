f9bf9a765968cc9d54efa9c314e87b1543448a86
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-03 07:41:34 +0530

    Time: 658 ms (48.67%), Space: 94.5 MB (60.75%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -1,27 +1,26 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode end = list1, start = null;
-        for (int i = 0; i < b; ++i, end = end.next) { // locate b upon termination of the loop.
-            if (i == a - 1) { // locate the node right before a.
-                start = end;
-            }
-        }
-        start.next = list2; // connect the node right before a to the head of list2, hence cut off a from list1.
-        while (list2.next != null) { // traverse till the end of list2. 
-            list2 = list2.next;
-        }
-        list2.next = end.next; // connect end of list2 to the node right after b.
-        end.next = null; // cut off b from list1.
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode *f=list1,*s=list1;
+        for(int i=0;i<a-1;i++) f=f->next;
+        
+        for(int i=0;i<b;i++) s=s->next;
+        
+        f->next=list2;
+        while(list2->next!=NULL)
+            list2=list2->next;
+        
+        list2->next=s->next;
         return list1;
     }
-}
+};
