0c1cbdb26537050403018bf82df921193e546e3a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-05 06:51:52 +0530

    Time: 92 ms (58.28%), Space: 100.7 MB (16.47%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,10 +1,8 @@
-bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
 class Solution {
-public:  
-    int findMinArrowShots(vector<vector<int>>& segments) {
-        sort(segments.begin(), segments.end(), cmp);
+    public int findMinArrowShots(int[][] segments) {
+          Arrays.sort(segments, (a, b) -> Integer.compare(a[1], b[1]));
         int ans = 0, arrow = 0;
-        for (int i = 0; i < segments.size(); i ++) {
+        for (int i = 0; i < segments.length; i ++) {
             if (ans == 0 || segments[i][0] > arrow) {
                 ans ++;
                 arrow = segments[i][1];
@@ -12,4 +10,4 @@ public:
         }
         return ans;
     }
-};
+}
