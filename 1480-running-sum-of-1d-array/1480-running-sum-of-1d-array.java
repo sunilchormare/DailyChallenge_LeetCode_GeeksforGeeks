c1a2533341524c711680c23fdbc0ee11aad62f4a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-06 16:48:45 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (41.36%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,25 @@
 class Solution {
-    public int[] runningSum(int[] nums) {
-         int res[]=new int[nums.length];
-        res[0]=nums[0];
-        for(int i=1;i<nums.length;i++)
-            res[i]=res[i-1]+nums[i];
+public:
+    vector<int> runningSum(vector<int>& nums) {
+//         vector<int> res;
+//          res[0]=nums[0];
+//         for(int i=1;i<nums.size();++i)
+//         {
+                
+//             int j=0,sum=0;
+//             while(j<=i)
+//                 sum+=nums[j],++j;
+            
+//             res[i]=sum;
+            
+//         }
+//         return res;
         
+           vector<int> res(nums.size(),0);
+        res[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            res[i]=res[i-1]+nums[i];
+        }
         return res;
     }
-}
+};
