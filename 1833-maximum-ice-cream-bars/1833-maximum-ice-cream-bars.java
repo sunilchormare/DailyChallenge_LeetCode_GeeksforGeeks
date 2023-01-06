5d65de4cf1c094fffe0a9aa436f7155404e9b252
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-06 16:44:11 +0530

    Time: 59 ms (14.22%), Space: 80.3 MB (6.87%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,14 +1,14 @@
 class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
-         sort(costs.begin(),costs.end());
-        int n = costs.size();
+    public int maxIceCream(int[] costs, int coins) {
+         Arrays.sort(costs);
+        int n = costs.length;
         int answer = 0;
+        
         while (answer < n && costs[answer] <= coins) {
+            // We can buy this icecream, reduce the cost from the coins. 
             coins -= costs[answer];
             answer += 1;
         }
         return answer;
-        
     }
-};
+}
