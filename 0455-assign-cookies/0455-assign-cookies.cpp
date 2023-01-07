f8e6fad0c6719d6e4ff8a60d0221d85cd4c752c3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-07 17:07:52 +0530

    Time: 29 ms (90.99%), Space: 17.5 MB (81.23%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,11 +1,12 @@
 class Solution {
-    public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-Arrays.sort(s);
-int i = 0;
-for(int j=0;i<g.length && j<s.length;j++) {
-	if(g[i]<=s[j]) i++;
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+       sort(g.begin(),g.end());
+sort(s.begin(),s.end());
+int m = g.size(), n = s.size(), i=0, j=0;
+for(; i<m && j<n; j++){
+if(g[i] <= s[j])i++;
 }
 return i;
     }
-}
+};
