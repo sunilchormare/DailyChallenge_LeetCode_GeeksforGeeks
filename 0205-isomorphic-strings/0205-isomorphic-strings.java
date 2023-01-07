a23a036e12c81b45a8b1e1cdc4ec08db3983fc24
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-07 14:54:02 +0530

    Time: 7 ms (81.26%), Space: 6.9 MB (74.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,13 +1,16 @@
 class Solution {
-    public boolean isIsomorphic (String s, String t) {
-        int[] m1 = new int[256];
-        int[] m2 = new int[256];
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
-            m1[s.charAt(i)] = i + 1;
-            m2[t.charAt(i)] = i + 1;
+public:
+    bool isIsomorphic(string s, string t) {
+        int len = s.length();
+        int m1[256], m2[256];
+        for (int i = 0; i < 256; i++) {
+            m1[i] = m2[i] = -1;
+        }
+        
+        for (int i = 0; i < len; i++) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = m2[t[i]] = i;
         }
         return true;
     }
-}
+};
