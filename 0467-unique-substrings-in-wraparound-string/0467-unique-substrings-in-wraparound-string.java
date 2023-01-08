228a84ca392a5b4bf75519fb3148e1545c605dd9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-08 10:49:58 +0530

    Time: 10 ms (55.94%), Space: 42.6 MB (60.14%) - LeetHub

--- 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.java
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.java
@@ -1,19 +1,18 @@
 class Solution {
-public:
-    int findSubstringInWraproundString(string p) {
-       int ans=1,prev_ans=1;
-        vector<int> arr(26,0);
-        arr[p[0]-'a']=1;
-        for (int i=1;i<p.size();i++){
-            if ((p[i-1]-'a'+1)%26==p[i]-'a') // Checking for continuation
+    public int findSubstringInWraproundString(String p) {
+         int ans=1,prev_ans=1;
+       int arr[]=new int[26];
+        arr[p.charAt(0)-'a']=1;
+        for (int i=1;i<p.length();i++){
+            if ((p.charAt(i-1)-'a'+1)%26==p.charAt(i)-'a') // Checking for continuation
                 prev_ans++;
             else
                 prev_ans=1;
-            if (arr[p[i]-'a']<prev_ans){
-                ans+=prev_ans-arr[p[i]-'a'];
-                arr[p[i]-'a']=prev_ans;
+            if (arr[p.charAt(i)-'a']<prev_ans){
+                ans+=prev_ans-arr[p.charAt(i)-'a'];
+                arr[p.charAt(i)-'a']=prev_ans;
             }
         }
-        return ans;  
+        return ans;
     }
-};
+}
