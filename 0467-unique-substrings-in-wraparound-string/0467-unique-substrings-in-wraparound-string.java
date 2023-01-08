efcfbd5a2b7c9938f2697e81d892c9dfe6011930
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-08 10:46:59 +0530

    Time: 10 ms (74.25%), Space: 8 MB (14.16%) - LeetHub

---
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.java
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findSubstringInWraproundString(string p) {
+       int ans=1,prev_ans=1;
+        vector<int> arr(26,0);
+        arr[p[0]-'a']=1;
+        for (int i=1;i<p.size();i++){
+            if ((p[i-1]-'a'+1)%26==p[i]-'a') // Checking for continuation
+                prev_ans++;
+            else
+                prev_ans=1;
+            if (arr[p[i]-'a']<prev_ans){
+                ans+=prev_ans-arr[p[i]-'a'];
+                arr[p[i]-'a']=prev_ans;
+            }
+        }
+        return ans;  
+    }
+};
