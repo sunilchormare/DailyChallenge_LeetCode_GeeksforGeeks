3cc31e5251529e7c5faa458cd1629e3c41729a42
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-09 07:09:28 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (94.38%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,29 +1,32 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
-        if(root==NULL) return res;
-        dfs(root,res);
-        return res;
-    }
-    void dfs(TreeNode* root,vector<int> &res)
-    {
-        
-           res.push_back(root->val);
-        if(root->left!=NULL)
-           dfs(root->left,res);
-        if(root->right!=NULL)
-           dfs(root->right,res);
-    }
-};
+   public List<Integer> preorderTraversal(TreeNode node) {
+	List<Integer> list = new LinkedList<Integer>();
+	Stack<TreeNode> rights = new Stack<TreeNode>();
+	while(node != null) {
+		list.add(node.val);
+		if (node.right != null) {
+			rights.push(node.right);
+		}
+		node = node.left;
+		if (node == null && !rights.isEmpty()) {
+			node = rights.pop();
+		}
+	}
+    return list;
+}
+}
