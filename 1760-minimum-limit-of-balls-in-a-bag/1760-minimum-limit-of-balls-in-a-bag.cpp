b4508def0ffafdc4a9d0cb798265a2efa57c95cf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-09 10:31:42 +0530

    Time: 189 ms (88.58%), Space: 56 MB (34.88%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,6 +1,7 @@
 class Solution {
-   public int minimumSize(int[] A, int k) {
-        int left = 1, right = 1_000_000_000;
+public:
+     int minimumSize(vector<int>& A, int k) {
+        int left = 1, right = 1e9;
         while (left < right) {
             int mid = (left + right) / 2, count = 0;
             for (int a : A)
@@ -12,5 +13,4 @@ class Solution {
         }
         return left;
     }
-
-}
+};
