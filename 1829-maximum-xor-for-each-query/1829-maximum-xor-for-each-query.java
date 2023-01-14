86c022634ce1791df4ec618590de712566c36818
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-14 07:15:58 +0530

    Time: 2 ms (100.00%), Space: 53.2 MB (63.09%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,10 +1,9 @@
 class Solution {
-public:
-    vector<int> getMaximumXor(vector<int>& n, int maximumBit) {
-     vector<int> res(n.size());
+    public int[] getMaximumXor(int[] n, int maximumBit) {
+         int[] res = new int[n.length];
     int val = (1 << maximumBit) - 1;
-    for (int i = 0; i < n.size(); ++i)
-        res[n.size() - i - 1] = val ^= n[i];
-    return res;   
+    for (int i = 0; i < n.length; ++i)
+        res[n.length - i - 1] = val ^= n[i];        
+    return res;
     }
-};
+}
