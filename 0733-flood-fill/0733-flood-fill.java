1ecb4e028f7bd967dae39b4f428d5f86780764b8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-14 06:39:27 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (51.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,20 +1,16 @@
 class Solution {
-public:
-      void fill(vector<vector<int>>& image, int sr, int sc, int color, int newColor) {
-        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != color) return;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+    
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
         image[sr][sc] = newColor;
         fill(image, sr + 1, sc, color, newColor);
         fill(image, sr - 1, sc, color, newColor);
         fill(image, sr, sc + 1, color, newColor);
         fill(image, sr, sc - 1, color, newColor);
     }
-vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) 
-    {
-        
-     if (image[sr][sc] == newColor) return image;
-        fill(image, sr, sc, image[sr][sc], newColor);
-        return image;
-        
-        
-    }
-};
+}
