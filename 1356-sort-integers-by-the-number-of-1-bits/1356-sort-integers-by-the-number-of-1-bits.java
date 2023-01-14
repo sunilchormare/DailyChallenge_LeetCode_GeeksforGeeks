0d66c583dddf95c6362b6263346fe61fc4a9b804
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-14 07:04:17 +0530

    Time: 3 ms (91.35%), Space: 42.6 MB (82.88%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,24 @@
+class Solution {
+   public int[] sortByBits(int[] arr) {
+        int n = arr.length, res[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = countBitOne(arr[i])*10001 + arr[i];
+        }
+        
+        Arrays.sort(res);
+        for (int i = 0; i < n; i++) {
+            res[i] %= 10001;
+        }
+        
+        return res;
+    }
+    
+    private int countBitOne(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
