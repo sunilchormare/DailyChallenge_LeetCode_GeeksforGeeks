f744c8ac6181d715ae685a36e5840247ce2b0405
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-15 10:55:28 +0530

    Time: 504 ms (21.16%), Space: 137.4 MB (5.29%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countArrangement(int n) {
+    vector<int>vis(n+1,0);
+    return helper(n,1,vis);
+}
+int helper(int n, int j, vector<int>vis) {
+    if(j>n) {
+        return 1;
+    }
+    int cnt = 0;
+    for(int i=1;i<=n;i++) {
+        if(!vis[i]&&(j%i==0 || i%j==0)) {
+            vis[i]=1;
+            cnt += helper(n,j+1,vis);
+            vis[i]=0;
+        }
+    }
+    return cnt;
+}
+};
