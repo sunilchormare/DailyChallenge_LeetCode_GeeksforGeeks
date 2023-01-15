460d733215a745e32dc79698a4a9c5485106c099
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-15 09:17:22 +0530

    Time: 91 ms (5.53%), Space: 42.1 MB (5.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,7 +6,7 @@ class Solution {
         //     return fib(n - 1) + fib(n - 2);
         
         int[] fib_cache = new int[31];
-if(N <= 1)
+        if(N <= 1)
             return N;
         else if(fib_cache[N] != 0)
             return fib_cache[N];
