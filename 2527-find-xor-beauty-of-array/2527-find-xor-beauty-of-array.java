dc068571019f139748dd176945fbf6de7574907a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-17 11:30:09 +0530

    Time: 1 ms (100.00%), Space: 58.7 MB (72.90%) - LeetHub

---
+++ 2527-find-xor-beauty-of-array/2527-find-xor-beauty-of-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int xorBeauty(int[] nums) {
+         int xor = 0;
+        for (int i : nums) {
+            xor ^= i;
+        }
+        
+        return xor;
+    
+    }
+}
