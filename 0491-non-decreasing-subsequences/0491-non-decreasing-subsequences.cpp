6336616d9923a9a334bf51ac72492dad87a2f0d2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-18 05:48:34 +0530

    Time: 47 ms (89.17%), Space: 21.9 MB (89.17%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -8,7 +8,8 @@ public:
     }
     
     void dfs(vector<vector<int>>& res, vector<int>& seq, vector<int>& nums, int pos) {
-        if(seq.size() > 1) res.push_back(seq);
+        if(seq.size() > 1) 
+            res.push_back(seq);
         unordered_set<int> hash;
         for(int i = pos; i < nums.size(); ++i) {
             if((seq.empty() || nums[i] >= seq.back()) && hash.find(nums[i]) == hash.end()) {
