b6f80245bb8f24d77a9ea236182f74f7704c4812
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-18 05:37:41 +0530

    Time: 4 ms (82.94%), Space: 47.2 MB (91.12%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,21 +1,20 @@
 class Solution {
-public:
-    int maxSubarraySumCircular(vector<int>& A) {
-      int sum=0,max_sum=INT_MIN;
+    public int maxSubarraySumCircular(int[] A) {
+      
+          int sum=0,max_sum=Integer.MIN_VALUE;
        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
-        for (int& a : A) {
-            curMax = max(curMax + a, a);
-           // cout<<" a = "<<a<<" curMax : "<<curMax<<endl;
-            maxSum = max(maxSum, curMax);
-           // cout<<" a = "<<a<<" maxSum : "<<maxSum<<endl;
-            curMin = min(curMin + a, a);
-            //cout<<" a = "<<a<<" curMin : "<<curMin<<endl;
-            minSum = min(minSum, curMin);
-            //cout<<" a = "<<a<<" minSum : "<<minSum<<endl;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+         
+            maxSum = Math.max(maxSum, curMax);
+          
+            curMin = Math.min(curMin + a, a);
+            
+            minSum = Math.min(minSum, curMin);
+            
             total += a;
-           // cout<<" a = "<<a<<" total : "<<total<<endl;
-            //cout<<endl;
+         
         }
-        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;   
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;   
     }
-};
+}
