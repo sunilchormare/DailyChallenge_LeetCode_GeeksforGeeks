17aa75c1a611858571f44420ad6560f2be012b9e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-18 06:01:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.09%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,5 +1,8 @@
 class Solution {
-    public int kthGrammar(int N, int K) {
-         return Integer.bitCount(K - 1) & 1;
+public:
+    int kthGrammar(int N, int K) {
+        int n;
+        for (n = 0, K -= 1; K ; K &= (K - 1)) n++;
+        return n & 1;
     }
-}
+};
