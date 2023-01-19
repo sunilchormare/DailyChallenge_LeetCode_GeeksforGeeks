474feed3ef8f27d8e55cac13de803624a37f5b0a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-19 16:30:08 +0530

    Time: 1507 ms (54.65%), Space: 209.4 MB (53.04%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
@@ -4,8 +4,8 @@ public:
           unordered_set<int> res, cur, cur2;
         for (int i: A) {
             cur2 = {i};
-            for (int j: cur) cur2.insert(i|j);
-            for (int j: cur = cur2) res.insert(j);
+            for(int j: cur) cur2.insert(i|j);
+            for(int j: cur = cur2) res.insert(j);
         }
         return res.size();
     }
