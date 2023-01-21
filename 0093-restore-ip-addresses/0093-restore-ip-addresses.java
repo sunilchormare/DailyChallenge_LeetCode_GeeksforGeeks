57f6a82bd5f87b142549ab0d3b80fb3b346de1a7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-21 10:04:04 +0530

    Time: 2 ms (93.10%), Space: 40.9 MB (96.98%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,23 +1,28 @@
 class Solution {
-public:
-    vector<string> restoreIpAddresses(string s) {
-        vector<string> ret;
-        string ans;
-        
-        for (int a=1; a<=3; a++)
-        for (int b=1; b<=3; b++)
-        for (int c=1; c<=3; c++)
-        for (int d=1; d<=3; d++)
-            if (a+b+c+d == s.length()) {
-                int A = stoi(s.substr(0, a));
-                int B = stoi(s.substr(a, b));
-                int C = stoi(s.substr(a+b, c));
-                int D = stoi(s.substr(a+b+c, d));
-                if (A<=255 && B<=255 && C<=255 && D<=255)
-                    if ( (ans=to_string(A)+"."+to_string(B)+"."+to_string(C)+"."+to_string(D)).length() == s.length()+3)
-                        ret.push_back(ans);
-            }    
-        
-        return ret;
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ret = new ArrayList<>();
+		
+		StringBuffer ip = new StringBuffer();
+		for(int a = 1 ; a < 4 ; ++ a)
+		for(int b = 1 ; b < 4 ; ++ b)
+	        for(int c = 1 ; c < 4 ; ++ c)
+		for(int d = 1 ; d < 4 ; ++ d)
+		{
+			if(a + b + c + d == s.length() )
+			{
+				int n1 = Integer.parseInt(s.substring(0, a));
+				int n2 = Integer.parseInt(s.substring(a, a+b));
+				int n3 = Integer.parseInt(s.substring(a+b, a+b+c));
+				int n4 = Integer.parseInt(s.substring(a+b+c));
+				if(n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255)
+				{
+					ip.append(n1).append('.').append(n2)
+						.append('.').append(n3).append('.').append(n4);
+					if(ip.length() == s.length() + 3) ret.add(ip.toString());
+					ip.delete(0, ip.length());
+				}
+			}
+		}
+		return ret;
     }
-};
+}
