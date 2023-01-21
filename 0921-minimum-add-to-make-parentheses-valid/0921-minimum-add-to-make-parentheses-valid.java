37924a3f8265c487d7dde5775e0d205c595c4812
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-21 11:51:37 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (76.05%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+         int left = 0, right = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '(') {
+                right++;
+            } else if (right > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left + right;
+    }
+}
