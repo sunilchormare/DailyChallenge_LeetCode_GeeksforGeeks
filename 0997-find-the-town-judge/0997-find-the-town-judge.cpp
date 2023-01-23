d041d312bdc9e1cdbb44d9e25a171fc959496f43
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-23 08:30:51 +0530

    Time: 166 ms (96.90%), Space: 60.8 MB (73.70%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -5,7 +5,8 @@ public:
         for (auto& t : trust)
             count[t[0]]--, count[t[1]]++;
         for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
+            if (count[i] == N - 1) 
+                return i;
         }
         return -1;
     }
