a9a195cb6f5335553f84d3838b3e8d90a0c5179b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-24 08:39:04 +0530

    Time: 8 ms (39.02%), Space: 14 MB (36.59%) - LeetHub

---
+++ 0971-flip-binary-tree-to-match-preorder-traversal/0971-flip-binary-tree-to-match-preorder-traversal.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<int> res;
+    int i = 0;
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& v) {
+        return dfs(root, v) ? res : vector<int>{-1};
+    }
+
+    bool dfs(TreeNode* node, vector<int>& v) {
+        if (!node) return true;
+        if (node->val != v[i++]) return false;
+        if (node->left && node->left->val != v[i]) {
+            res.push_back(node->val);
+            return dfs(node->right, v) && dfs(node->left, v);
+        }
+        return dfs(node->left, v) && dfs(node->right, v);
+    }
+};
