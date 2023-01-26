88b9e9e1e5c79cb3ee87b435ee606b9069771577
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-26 13:37:17 +0530

    Time: 7 ms (70.63%), Space: 52 MB (28.17%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
@@ -0,0 +1,32 @@
+class Solution {
+    int result = 0;
+    public int countPairs(TreeNode root, int distance) {
+        dfs(root,distance);
+        return result;
+    }
+    
+    int[] dfs(TreeNode root,int distance){
+        if(root == null)
+            return new int[distance+1];
+        if(root.left == null && root.right == null){
+            int res[] = new int[distance+1];
+            res[1]++;
+            return res;
+        }
+        int[] left = dfs(root.left,distance);
+        int[] right = dfs(root.right,distance);
+        for(int l=1;l<left.length;l++){
+            for(int r = distance-1;r>=0;r--){
+                if(l+r <=distance)
+                result += left[l]*right[r];
+            }
+        }
+        int res[] = new int[distance+1];
+        //shift by 1
+        for(int i=res.length-2;i>=1;i--){
+            res[i+1] = left[i]+right[i];
+        }
+        
+        return res;
+    }
+}
