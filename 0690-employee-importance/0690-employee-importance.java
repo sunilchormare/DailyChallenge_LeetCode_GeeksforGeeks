4ac9e0f102a878f698c204470a2f62650cad022f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-29 07:42:31 +0530

    Time: 5 ms (73.70%), Space: 42.9 MB (72.86%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        int total = 0;
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(map.get(id));
+        while (!queue.isEmpty()) {
+            Employee current = queue.poll();
+            total += current.importance;
+            for (int subordinate : current.subordinates) {
+                queue.offer(map.get(subordinate));
+            }
+        }
+        return total;
+    }
+}
