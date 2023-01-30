0fc18676d0768f760f42f0590870d3da9254f716
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-01-30 08:33:18 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (42.35%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,11 +1,13 @@
 class Solution {
-public:
-    int tribonacci(int n) {
-        vector<int> dp={0,1,1};
+    public int tribonacci(int n) {
+         int dp[]=new int[38];
+         dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+       // dp[]={0,1,1};
         for(int i=3;i<=n;i++)
-            dp.push_back(dp[i-1]+dp[i-2]+dp[i-3]);
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         
         return dp[n];
-        
     }
-};
+}
