30b28f7b61391d1db5250f7feece0d04ba7c0bd4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-04 16:27:54 +0530

    Time: 10 ms (62.19%), Space: 41.4 MB (97.51%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
@@ -1,12 +1,16 @@
-class Solution {
-public:
-    int subarrayGCD(vector<int>& nums, int k) {
-       int res = 0;
-    for (int i = 0; i < nums.size(); ++i)
-        for (int j = i; j < nums.size() && nums[j] % k == 0; ++j) {
-            nums[i] = gcd(nums[i], nums[j]);
-            res += nums[i] == k;
-        }
-    return res;
+class Solution {  
+    private int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
-};
+    public int subarrayGCD(int[] nums, int k) {
+       int res = 0;
+    for (int i = 0; i < nums.length; ++i)
+        for (int j = i; j < nums.length && nums[j] % k == 0; ++j) {
+            nums[i] = gcd(nums[i], nums[j]);
+            res += nums[i] == k?1:0;
+        }
+    return res;  
+    }
+}
