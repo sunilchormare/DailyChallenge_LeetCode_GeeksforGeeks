2f628b6331d9e5059998eeebfae6cf749425793a
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-07 23:10:51 +0700

    Solution.java

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,32 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-//         HashMap<Character,Integer> mpp=new HashMap<Character,Integer>();
-//         int right=0,left=0;
-//         int n=s.length()-1;
-//         int len=0;
-//         if(n==0) return 1;
-//         while(right<n)
-//         {
-//             if(mpp.containsKey(s.charAt(right)))
-//                 left=Math.max(left,mpp.get(s.charAt(right))+1);
-//             mpp.put(s.charAt(right),right);
-//             len=Math.max(len,right-left+1);
-//             right++;
-//         }
-        
-//         return len;
-        
-         if (s.length()==0) return 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i)
-        {
-            if (map.containsKey(s.charAt(i)))
-                j = Math.max(j,map.get(s.charAt(i))+1);
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int maxLength = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                maxLength = Math.max(maxLength, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
         }
-        return max; 
-        
+        return maxLength;
     }
 }
