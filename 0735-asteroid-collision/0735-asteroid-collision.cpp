3b327fd4331a4d0bc15f5b4f325dcd7059424b87
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-07 07:47:49 +0530

    Time: 10 ms (94.64%), Space: 17.5 MB (89.51%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+     vector<int> s; 
+        for (int i : a) {
+            if(i > 0) 
+                s.push_back(i);
+            else
+            {
+                while(!s.empty() && s.back() > 0 && s.back() < abs(i)) s.pop_back();
+                if(s.empty() || s.back() < 0) 
+                    s.push_back(i);
+                else if(s.back() == -i)
+                    s.pop_back();
+            }
+        }
+        return s;
+    }
+};
