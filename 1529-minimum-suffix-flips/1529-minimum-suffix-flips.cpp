bacae2bfb0dedba55f658f270f646d14f97297a0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-10 13:21:36 +0530

    Time: 15 ms (68.85%), Space: 9.5 MB (8.58%) - LeetHub

---
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+   int minFlips(string target) {
+    int cnt = 0, state = 0;
+    for (auto b : target)
+        if (b - '0' != state) {
+            state = b - '0';
+            ++cnt;
+        }
+    return cnt;
+}
+};
