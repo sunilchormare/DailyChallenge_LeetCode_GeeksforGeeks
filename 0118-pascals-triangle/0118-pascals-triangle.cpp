2a4f2097c9e9921c748687ed4df59305affb1889
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-11 10:40:54 +0530

    Time: 8 ms (5.20%), Space: 6.7 MB (55.46%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+  vector<vector<int>> res(n);
+        for(int i=0;i<n;++i)
+        {
+            res[i].resize(i+1);
+            res[i][0]=res[i][i]=1;
+            for(int j=1;j<i;++j)
+            {
+                res[i][j]=res[i-1][j-1]+res[i-1][j];
+               cout<<res[i][j]<<" = "<<res[i-1][j-1]<<" + "<<res[i-1][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        return res;
+    }
+};
