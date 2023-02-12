d0291e207fdb6b2458f271582da93b5ec8e9f7b3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-12 17:55:20 +0530

    Time: 604 ms (80.62%), Space: 191.4 MB (72.79%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,22 +1,21 @@
 class Solution {
-    long ans = 0; int s;
-public long minimumFuelCost(int[][] roads, int seats) {
-    List<List<Integer>> graph = new ArrayList(); s = seats;
-    for (int i = 0; i < roads.length + 1; i++) graph.add(new ArrayList());
-    for (int[] r: roads) {
-        graph.get(r[0]).add(r[1]);
-        graph.get(r[1]).add(r[0]);
+public:
+    long long ans = 0; int s;
+long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+    vector<vector<int>> graph(roads.size() + 1); s = seats;
+    for (vector<int>& r: roads) {
+        graph[r[0]].push_back(r[1]);
+        graph[r[1]].push_back(r[0]);
     }
     dfs(0, 0, graph);
     return ans;
 }
-private int dfs(int i, int prev, List<List<Integer>> graph) {
-    int people = 1;
-    for (int x: graph.get(i)) {
+int dfs(int i, int prev, vector<vector<int>>& graph, int people = 1) {
+    for (int& x: graph[i]) {
         if (x == prev) continue;
         people += dfs(x, i, graph);
     }
     if (i != 0) ans += (people + s - 1) / s;
     return people;
 }
-}
+};
