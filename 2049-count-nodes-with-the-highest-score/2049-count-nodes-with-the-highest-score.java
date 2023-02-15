934c8808a0d671c1a2668eecffc1e812cc521c6d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-15 17:07:17 +0530

    Time: 100 ms (57.30%), Space: 77.8 MB (55.06%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
@@ -1,20 +1,23 @@
 class Solution {
-public:
-    int dfs(vector<vector<int>> &al, vector<long long> &s, int i) {
-    long long prod = 1, sum = 1;
-    for (int j : al[i]) {
-        int cnt = dfs(al, s, j);
+    long dfs(List<List<Integer>> al, long[] s, int i) {
+    long prod = 1, sum = 1;
+    for (int j : al.get(i)) {
+        long cnt = dfs(al, s, j);
         prod *= cnt;
         sum += cnt;
     }
-    s[i] = prod * (max(1ll, (long long)al.size() - sum));
-    return i != 0 ? sum : count(begin(s), end(s), *max_element(begin(s), end(s)));
+    s[i] = prod * (Math.max(1, al.size() - sum));
+    return sum;
+}    
+public int countHighestScoreNodes(int[] p) {
+    List<List<Integer>> al = new ArrayList<>();
+    for (int i = 0; i < p.length; ++i)
+        al.add(new ArrayList<Integer>());         
+    long[] s = new long[p.length];
+    for (int i = 1; i < p.length; ++i)
+        al.get(p[i]).add(i);
+    dfs(al, s, 0);
+    long max_val = Arrays.stream(s).max().getAsLong();
+    return (int)Arrays.stream(s).filter(v -> v == max_val).count();
 }
-int countHighestScoreNodes(vector<int>& p) {
-    vector<vector<int>> al(p.size());
-    vector<long long> s(p.size());
-    for (int i = 1; i < p.size(); ++i)
-        al[p[i]].push_back(i);
-    return dfs(al, s, 0);
 }
-};
