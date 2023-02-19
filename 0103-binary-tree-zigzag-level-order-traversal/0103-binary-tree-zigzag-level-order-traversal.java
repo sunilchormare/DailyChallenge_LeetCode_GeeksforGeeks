8315e900ca8879eeed17022f545b88e9ecf58053
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-19 20:37:07 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (98.85%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,53 +1,26 @@
 class Solution {
-public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-       vector<vector<int>> result; 
-       queue<TreeNode*> q;
-       if(root==NULL) return result;
-        q.push(root);
-        bool leftToRight=true;
-        while(!q.empty())
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+         List<List<Integer>> result=new ArrayList<>(); 
+       Queue<TreeNode> q=new LinkedList<>();
+       if(root==null) return result;
+        q.offer(root);
+        boolean leftToRight=true;
+        while(!q.isEmpty())
         {
           int sz=q.size();
-          vector<int> row(sz);
+           List<Integer> row = Arrays.asList(new Integer[sz]);
             for(int i=0;i<sz;++i)
             {
-                TreeNode *node=q.front();q.pop();
+                TreeNode node=q.poll();
                 int index=(leftToRight)?i:(sz-1-i);
-                row[index]=node->val;
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
+                row.set(index,node.val);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
                 
             }
             leftToRight=!leftToRight;
-            result.push_back(row);
-        }
-        
-//         return result;
-//          vector<vector<int>> result;
-//         if(root==NULL) return result;
-//         queue<TreeNode*> nodesQueue;
-//         nodesQueue.push(root);
-//         bool leftToRight=true;
-//         while(!nodesQueue.empty())
-//         {
-//             int size=nodesQueue.size();
-//             vector<int> row(size);
-//             for(int i=0;i<size;++i)
-//             {
-//                 TreeNode *node=nodesQueue.front();
-//                 nodesQueue.pop();
-//                 int index=(leftToRight) ? i : (size-1-i);
-//                 row[index]=node->val;
-//                 if(node->left)
-//                     nodesQueue.push(node->left);
-//                 if(node->right)
-//                     nodesQueue.push(node->right);
-//             }
-//             leftToRight=!leftToRight;
-//             result.push_back(row);
-            
-//         }
-        return result;}
-    
-};
+            result.add(row);
+    }
+        return result;
+    }
+}
