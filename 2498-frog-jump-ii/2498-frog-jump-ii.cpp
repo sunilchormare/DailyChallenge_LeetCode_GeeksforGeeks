e42fac4d5b8b5a2dd242fc783a2cd774f09ce382
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-22 08:19:00 +0530

    Time: 293 ms (26.31%), Space: 65.4 MB (36.47%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,8 +1,9 @@
 class Solution {
-    public int maxJump(int[] A) {
-        int res = A[1] - A[0], n = A.length;
-        for(int i = 2; i < n; ++i)
-            res = Math.max(res, A[i] - A[i - 2]);
+public:
+     int maxJump(vector<int>& A) {
+        int res = A[1] - A[0], n = A.size();
+        for (int i = 2; i < n; ++i)
+            res = max(res, A[i] - A[i - 2]);
         return res;
     }
-}
+};
