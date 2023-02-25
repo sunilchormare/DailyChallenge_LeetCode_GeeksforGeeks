2eb34ae03e8d0d7c91db6a39dd5ad837cdd6e081
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-02-25 06:37:26 +0530

    Time: 2 ms (63.52%), Space: 59.4 MB (21.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,30 +1,12 @@
-class Solution{
-public:
-    int maxProfit(vector<int>& prices) {
-//         int maxi=0,y=0;
-//         for(int i=0;i<prices.size()-1;i++)
-//         {
-//             for(int j=i+1;j<prices.size();++j)
-//             {
-//                maxi=max(prices[j]-prices[i],maxi);
-//                 if(maxi>y)
-//                 {
-//                   y=maxi;
-//                 }
-//             }
-//           //  y=maxi;
-//         }
-        
-//         return maxi;
-        
-       
+class Solution {
+    public int maxProfit(int[] prices) {
         int maxProfit=0;
-        int minPrice=INT_MAX;
-        for(int i=0;i<prices.size();++i)
+        int minPrice=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;++i)
         {
-            minPrice=min(minPrice,prices[i]);
-            maxProfit=max(maxProfit,prices[i]-minPrice);
+            minPrice=Math.min(minPrice,prices[i]);
+            maxProfit=Math.max(maxProfit,prices[i]-minPrice);
         }
         return maxProfit;
     }
-};
+}
