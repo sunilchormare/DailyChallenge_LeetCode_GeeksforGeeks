54e563a8fda1a5e2983b4f613959bfc9bafb1f4c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-04 07:05:50 +0530

    Time: 12 ms (83.63%), Space: 43.7 MB (37.08%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,31 @@
 class Solution {
-public:
-    int takeCharacters(string s, int k) {
-        int ca=0,cb=0,cc=0;
-        int n=s.size();
-        int ans=n;
-        for(int i=0;i<n;i++){
-            if(s[i]=='a') ca++;
-            if(s[i]=='b') cb++;
-            if(s[i]=='c') cc++;
-        }
-        if(ca<k||cb<k||cc<k) return -1;
-        int i=n-1,j=n-1;
-        while(i>=0){
-            if(s[i]=='a') ca--;
-            if(s[i]=='b') cb--;
-            if(s[i]=='c') cc--;
-            while(ca<k||cb<k||cc<k){
-            if(s[j]=='a') ca++;
-            if(s[j]=='b') cb++;
-            if(s[j]=='c') cc++;
-                j--;
+    public int takeCharacters(String s, int k) {
+        int[] arr=new int[3];
+        // count all the frq of string
+        for(char ch:s.toCharArray()) arr[ch-'a']++;
+        
+        // if total frq of any char is less than k return -1;
+        if(arr[0]<k||arr[1]<k||arr[2]<k) return -1;
+        int ans=arr[0]+arr[1]+arr[2];
+        int st=0;
+        
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']--;
+            
+            // if any char length is less than k then increase start position
+            if(arr[0]<k||arr[1]<k||arr[2]<k){
+                //get the min ans
+                ans=Math.min(arr[0]+arr[1]+arr[2]+1,ans);
+                while(s.charAt(st)!=ch){
+                    arr[s.charAt(st)-'a']++;
+                    st++;
+                }
+                arr[ch-'a']++;
+                st++;
             }
-            ans=min(ans,i+n-1-j); i--;
         }
+        ans=Math.min(arr[0]+arr[1]+arr[2],ans);
+                
         return ans;
     }
-};
+}
