68b4d5952527dc5d411cbe7bb2487407c6b40466
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-10 07:26:11 +0530

    Time: 23 ms (69.60%), Space: 22.6 MB (64.32%) - LeetHub

---
+++ 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class CBTInserter {
+    public:
+vector<TreeNode*> tree;
+    CBTInserter(TreeNode* root) {
+        tree.push_back(root);
+        for(int i = 0; i < tree.size();++i) {
+            if (tree[i]->left) tree.push_back(tree[i]->left);
+            if (tree[i]->right) tree.push_back(tree[i]->right);
+        }
+    }
+
+    int insert(int v) {
+        int N = tree.size();
+        TreeNode* node = new TreeNode(v);
+        tree.push_back(node);
+        if (N % 2)
+            tree[(N - 1) / 2]->left = node;
+        else
+            tree[(N - 1) / 2]->right = node;
+        return tree[(N - 1) / 2]->val;
+    }
+
+    TreeNode* get_root() {
+        return tree[0];
+    }
+};
+
