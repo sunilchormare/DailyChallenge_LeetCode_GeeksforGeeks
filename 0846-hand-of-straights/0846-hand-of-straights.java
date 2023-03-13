310d707083c536c5f0e3595e66863d35af50f02e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-13 07:43:05 +0530

    Time: 85 ms (12.39%), Space: 44.1 MB (24.35%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,7 +1,9 @@
 class Solution {
   public boolean isNStraightHand(int[] hand, int W) {
         Map<Integer, Integer> c = new TreeMap<>();
-        for (int i : hand) c.put(i, c.getOrDefault(i, 0)+1);
+        for (int i : hand) 
+            c.put(i, c.getOrDefault(i, 0)+1);
+      
         for (int it : c.keySet())
             if (c.get(it) > 0)
                 for (int i = W - 1; i >= 0; --i) {
