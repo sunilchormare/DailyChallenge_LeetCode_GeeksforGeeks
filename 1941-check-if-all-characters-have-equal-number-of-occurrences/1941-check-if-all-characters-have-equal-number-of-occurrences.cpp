6db2873f42c00c72153fff9e63022edffb16206a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-14 19:50:10 +0530

    Time: 4 ms (69.23%), Space: 6.6 MB (91.04%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -1,13 +1,11 @@
 class Solution {
-    public boolean areOccurrencesEqual(String s) {
-      int[] freq = new int[26];
+public:
+    bool areOccurrencesEqual(string s) {
+         unordered_map<char, int> freq;
+        for (auto c : s) freq[c]++;
+        int val = freq[s[0]];
+        for (auto [a, b] : freq) if (b != val) return false;
+        return true;
         
-        for (int i = 0; i < s.length(); i++) freq[s.charAt(i)-'a']++;
-
-        int val = freq[s.charAt(0) - 'a'];
-        for (int i = 0; i < 26; i++)
-            if (freq[i] != 0 && freq[i] != val) return false; 
-
-        return true;  
     }
-}
+};
