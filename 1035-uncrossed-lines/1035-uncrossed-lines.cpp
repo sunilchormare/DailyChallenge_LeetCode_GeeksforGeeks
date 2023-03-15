c235783906a14e237907ae13f5de541c00a3b19e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-15 20:46:06 +0530

    Time: 7 ms (98.30%), Space: 9.8 MB (89.51%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,12 +1,11 @@
 class Solution {
-   public int maxUncrossedLines(int[] A, int[] B) {
-        int m = A.length, n = B.length, dp[][] = new int[m + 1][n + 1];
+public:
+   int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int m = A.size(), n = B.size(), dp[m+1][n+1];
+        memset(dp, 0, sizeof(dp));
         for (int i = 1; i <= m; ++i)
             for (int j = 1; j <= n; ++j)
-                if (A[i - 1] == B[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] = A[i - 1] == B[j - 1] ? dp[i - 1][j - 1] + 1 : max(dp[i][j - 1], dp[i - 1][j]);
         return dp[m][n];
     }
-}
+};
