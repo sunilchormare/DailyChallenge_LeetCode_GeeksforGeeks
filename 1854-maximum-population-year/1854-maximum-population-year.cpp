d1a14102dc736ebc48ac362c3e7bb3c2705bad5c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-17 18:23:53 +0530

    Time: 7 ms (43.86%), Space: 7.9 MB (94.17%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,14 +1,15 @@
 class Solution {
-   public int maximumPopulation(int[][] logs) {
-    int pop[] = new int[2051], res = 0;
-    for (var l : logs) {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+    int pop[2051] = {}, res = 0;
+    for (auto &l : logs) {
         ++pop[l[0]];
         --pop[l[1]];
     }
-    for (int i = 1950; i < 2050; ++i) {
+    for (auto i = 1950; i < 2050; ++i) {
         pop[i] += pop[i - 1];
         res = pop[i] > pop[res] ? i : res;
     }
     return res;
 }
-}
+};
