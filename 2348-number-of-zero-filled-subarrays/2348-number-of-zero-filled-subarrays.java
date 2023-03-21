9736404a73df581ca1be1803a765afced91a9219
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-21 07:18:48 +0530

    Time: 3 ms (99.69%), Space: 59.6 MB (80.56%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,12 +1,11 @@
 class Solution {
-public:
-    long long zeroFilledSubarray(vector<int>& nums) {
-         long long res = 0;
-    for (int i = 0, j = 0; i < nums.size(); ++i) {
+    public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
+    for (int i = 0, j = 0; i < nums.length; ++i) {
         if (nums[i] != 0)
             j = i + 1;
          res += i - j + 1;
     }
     return res;
     }
-};
+}
