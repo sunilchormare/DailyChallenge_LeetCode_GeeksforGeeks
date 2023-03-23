a77033950df5c56b8a61cfb9756fc4d1d3e10b50
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-23 07:32:07 +0530

    Time: 144 ms (54.33%), Space: 56 MB (21.45%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,21 +1,31 @@
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-    if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-        int components = n;
-        for (int[] c : connections) {
-            int p1 = findParent(parent, c[0]);
-            int p2 = findParent(parent, c[1]);
-            if (p1 != p2) {
-                parent[p1] = p2; // Union 2 component
-                components--;
-            }
+private:
+    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+    {
+        visited[src] = true;
+        for(int i : adj[src])
+            if(!visited[i])
+                dfs(adj, visited, i);
+    }
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n - 1)
+            return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
         }
-        return components - 1; // Need (components-1) cables to connect components together
+        vector<bool> visited(n, false);
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i])
+            {
+                dfs(adj, visited, i);
+                components++;
+            }
+        return components - 1;
     }
-    private int findParent(int[] parent, int i) {
-        while (i != parent[i]) i = parent[i];
-        return i; // Without Path Compression
-    }
-}
+};
