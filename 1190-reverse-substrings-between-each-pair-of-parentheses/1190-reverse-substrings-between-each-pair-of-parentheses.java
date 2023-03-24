af82daefea5af949f5558e6d5504ae6f9144f2ef
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-24 10:44:04 +0530

    Time: 1 ms (99.57%), Space: 40.9 MB (65.67%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,25 +1,26 @@
 class Solution {
-public:
-    string reverseParentheses(string s) {
+     public String reverseParentheses(String s) {
         int n = s.length();
-        vector<int> opened, pair(n);
+        Stack<Integer> opened = new Stack<>();
+        int[] pair = new int[n];
         for (int i = 0; i < n; ++i) {
-            if (s[i] == '(')
-                opened.push_back(i);
-            if (s[i] == ')') {
-                int j = opened.back();
-                opened.pop_back();
+            if (s.charAt(i) == '(')
+                opened.push(i);
+            if (s.charAt(i) == ')') {
+                int j = opened.pop();
                 pair[i] = j;
                 pair[j] = i;
             }
         }
-        string res;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0, d = 1; i < n; i += d) {
-            if (s[i] == '(' || s[i] == ')')
-                i = pair[i], d = -d;
-            else
-                res += s[i];
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                i = pair[i];
+                d = -d;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
-        return res;
+        return sb.toString();
     }
-};
+}
