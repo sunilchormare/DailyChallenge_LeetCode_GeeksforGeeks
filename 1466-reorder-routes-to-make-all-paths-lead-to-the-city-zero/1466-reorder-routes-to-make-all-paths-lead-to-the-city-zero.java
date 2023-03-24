cbc46b767f87d19f0dafa96c6e2d31fc1318ce0c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-24 10:23:38 +0530

    Time: 367 ms (79.24%), Space: 106.2 MB (79.59%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,20 +1,19 @@
 class Solution {
-   int dfs(List<List<Integer>> al, boolean[] visited, int from) {
-    int change = 0;
+public:
+  int dfs(vector<vector<int>> &al, vector<bool> &visited, int from) {
+    auto change = 0;
     visited[from] = true;
-    for (var to : al.get(from))
-        if (!visited[Math.abs(to)])
-            change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
-    return change;   
+    for (auto to : al[from])
+        if (!visited[abs(to)])
+            change += dfs(al, visited, abs(to)) + (to > 0);
+    return change;        
 }
-public int minReorder(int n, int[][] connections) {
-    List<List<Integer>> al = new ArrayList<>();
-    for(int i = 0; i < n; ++i) 
-        al.add(new ArrayList<>());
-    for (var c : connections) {
-        al.get(c[0]).add(c[1]);
-        al.get(c[1]).add(-c[0]);
+int minReorder(int n, vector<vector<int>>& connections) {
+    vector<vector<int>> al(n);
+    for (auto &c : connections) {
+        al[c[0]].push_back(c[1]);
+        al[c[1]].push_back(-c[0]);
     }
-    return dfs(al, new boolean[n], 0);
+    return dfs(al, vector<bool>(n) = {}, 0);
 }
-}
+};
