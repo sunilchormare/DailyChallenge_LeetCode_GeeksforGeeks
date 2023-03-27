c50094b4b7c77ac5bd24589808c9b0199cb7d1d3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-27 07:45:23 +0530

    Time: 14 ms (49.01%), Space: 43.2 MB (41.89%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.java
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.java
@@ -1,17 +1,19 @@
 class Solution {
-public:
-    int minNumberOfFrogs(string croakOfFrogs) {
-        int cnt[5] = {}, frogs = 0, max_frogs = 0;
-    for (auto ch : croakOfFrogs) {
-        auto n = string("croak").find(ch);
+    public int minNumberOfFrogs(String croakOfFrogs) {
+        int cnt[] = new int[5];
+    int frogs = 0, max_frogs = 0;
+    for (var i = 0; i < croakOfFrogs.length(); ++i) {
+        var ch = croakOfFrogs.charAt(i);
+        var n = "croak".indexOf(ch);
         ++cnt[n];
         if (n == 0)
-            max_frogs = max(max_frogs, ++frogs);
+            max_frogs = Math.max(max_frogs, ++frogs);
         else if (--cnt[n - 1] < 0)
             return -1;
         else if (n == 4)
             --frogs;
     }
-    return frogs == 0 ? max_frogs : -1;
+    return frogs == 0 ? max_frogs : -1;    
+
     }
-};
+}
