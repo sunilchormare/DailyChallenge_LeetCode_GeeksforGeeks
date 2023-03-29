74495c5df8b05cbda9e7b1347ff831b6bd4b1cfa
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-29 07:47:20 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (66.75%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,22 +1,31 @@
 class Solution {
     public int maxSatisfaction(int[] s) {
-         int maxi=0;
-        Arrays.sort(s);
-        int n=s.length,start=n-1;
-        for(int i=n-1;i>=0;--i)
-        {
-            maxi+=s[i];
-            if(maxi<0) break;
-            start--;
-        }
-        start++;
-        int k=1;
-        maxi=0;
-        for(int i=start;i<n;++i)
-        {
-            maxi+=(k++)*s[i];
-        }
+//          int maxi=0;
+//         Arrays.sort(s);
+//         int n=s.length,start=n-1;
+//         for(int i=n-1;i>=0;--i)
+//         {
+//             maxi+=s[i];
+//             if(maxi<0) break;
+//             start--;
+//         }
+//         start++;
+//         int k=1;
+//         maxi=0;
+//         for(int i=start;i<n;++i)
+//         {
+//             maxi+=(k++)*s[i];
+//         }
         
-        return maxi;
+//         return maxi;
+        
+        
+          Arrays.sort(s);
+        int res = 0, total = 0, n = s.length;
+        for (int i = n - 1; i >= 0 && s[i] > -total; --i) {
+            total += s[i];
+            res += total;
+        }
+        return res;
     }
 }
