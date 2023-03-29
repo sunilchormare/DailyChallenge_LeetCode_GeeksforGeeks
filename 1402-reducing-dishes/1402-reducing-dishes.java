f034f207e8d5778208ca490434c5d097a55e92fd
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-29 07:42:11 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (54.90%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,9 +1,8 @@
 class Solution {
-public:
-    int maxSatisfaction(vector<int>& s) {
-        int maxi=0;
-        sort(s.begin(),s.end());
-        int n=s.size(),start=n-1;
+    public int maxSatisfaction(int[] s) {
+         int maxi=0;
+        Arrays.sort(s);
+        int n=s.length,start=n-1;
         for(int i=n-1;i>=0;--i)
         {
             maxi+=s[i];
@@ -20,4 +19,4 @@ public:
         
         return maxi;
     }
-};
+}
