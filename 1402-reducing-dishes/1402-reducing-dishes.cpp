578fb338915f30e59957c14bc1bff5a2f8667458
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-03-29 07:48:52 +0530

    Time: 3 ms (94.52%), Space: 8 MB (98.26%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int maxSatisfaction(vector<int>& s) {
-        int maxi=0;
-        sort(s.begin(),s.end());
-        int n=s.size(),start=n-1;
-        for(int i=n-1;i>=0;--i)
-        {
-            maxi+=s[i];
-            if(maxi<0) break;
-            start--;
-        }
-        start++;
-        int k=1;
-        maxi=0;
-        for(int i=start;i<n;++i)
-        {
-            maxi+=(k++)*s[i];
-        }
+//         int maxi=0;
+//         sort(s.begin(),s.end());
+//         int n=s.size(),start=n-1;
+//         for(int i=n-1;i>=0;--i)
+//         {
+//             maxi+=s[i];
+//             if(maxi<0) break;
+//             start--;
+//         }
+//         start++;
+//         int k=1;
+//         maxi=0;
+//         for(int i=start;i<n;++i)
+//         {
+//             maxi+=(k++)*s[i];
+//         }
         
-        return maxi;
+//         return maxi;
+          sort(s.begin(), s.end());
+        int res = 0, total = 0, n = s.size();
+        for (int i = n - 1; i >= 0 && s[i] > -total; --i) {
+            total += s[i];
+            res += total;
+        }
+        return res;
     }
 };
