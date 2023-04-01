48d6ca7e46865b2e7344df16792020ff433a520a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-01 07:25:19 +0530

    Time: 556 ms (52.65%), Space: 146.5 MB (44.80%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& t) {
+      vector<int> res(3);
+    for (auto &s : triplets)
+        if (s[0] <= t[0] && s[1] <= t[1] && s[2] <= t[2])
+            res = { max(res[0], s[0]), max(res[1], s[1]), max(res[2], s[2]) };
+    return res == t;
+    }
+};
