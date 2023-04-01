efa01b13cf23db1a765e5ae1b112588079e21eec
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-01 07:04:04 +0530

    Time: 46 ms (22.06%), Space: 27.6 MB (18.34%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-         int low = 0;
+          int low = 0;
         int high = nums.size() - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
