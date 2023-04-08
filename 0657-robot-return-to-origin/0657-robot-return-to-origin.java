f906030ece570478b766acbcab0d1ffa96977bda
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-08 19:16:13 +0530

    Time: 5 ms (91.97%), Space: 42.4 MB (66.07%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+         int x = 0;
+        int y = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'R') x++;
+            else if (ch == 'L') x--;
+        }
+        return x == 0 && y == 0;
+    }
+}
