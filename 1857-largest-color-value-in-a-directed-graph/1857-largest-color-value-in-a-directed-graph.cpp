dfaa60560474738b8c2905989e9c50aecd56e1ab
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-09 21:13:50 +0530

    Time: 703 ms (38.46%), Space: 175.2 MB (52.99%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,45 +1,43 @@
 class Solution {
-    public int largestPathValue(String colors, int[][] edges) {
-        int n = colors.length();
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        int n = colors.size();
         int k = 26;
-        int[] indegrees = new int[n];
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
+        vector<int> indegrees(n, 0);
+        vector<vector<int>> graph(n, vector<int>());
+        for (vector<int>& edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            graph.get(u).add(v);
+            graph[u].push_back(v);
             indegrees[v]++;
         }
-        Set<Integer> zero_indegree = new HashSet<>();
+        unordered_set<int> zero_indegree;
         for (int i = 0; i < n; i++) {
             if (indegrees[i] == 0) {
-                zero_indegree.add(i);
+                zero_indegree.insert(i);
             }
         }
-        int[][] counts = new int[n][k];
+        vector<vector<int>> counts(n, vector<int>(k, 0));
         for (int i = 0; i < n; i++) {
-            counts[i][colors.charAt(i) - 'a']++;
+            counts[i][colors[i] - 'a']++;
         }
         int max_count = 0;
         int visited = 0;
-        while (!zero_indegree.isEmpty()) {
-            int u = zero_indegree.iterator().next();
-            zero_indegree.remove(u);
+        while (!zero_indegree.empty()) {
+            int u = *zero_indegree.begin();
+            zero_indegree.erase(u);
             visited++;
-            for (int v : graph.get(u)) {
+            for (int v : graph[u]) {
                 for (int i = 0; i < k; i++) {
-                    counts[v][i] = Math.max(counts[v][i], counts[u][i] + (colors.charAt(v) - 'a' == i ? 1 : 0));
+                    counts[v][i] = max(counts[v][i], counts[u][i] + (colors[v] - 'a' == i ? 1 : 0));
                 }
                 indegrees[v]--;
                 if (indegrees[v] == 0) {
-                    zero_indegree.add(v);
+                    zero_indegree.insert(v);
                 }
             }
-            max_count = Math.max(max_count, Arrays.stream(counts[u]).max().getAsInt());
+            max_count = max(max_count, *max_element(counts[u].begin(), counts[u].end()));
         }
         return visited == n ? max_count : -1;
     }
-}
+};
