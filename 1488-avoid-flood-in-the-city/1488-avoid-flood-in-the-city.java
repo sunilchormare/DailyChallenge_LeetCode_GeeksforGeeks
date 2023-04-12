6b876f85fcf3a57cacc063ec52a2e34bc7a081dc
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-12 07:17:26 +0530

    Time: 75 ms (54.79%), Space: 60.9 MB (58.42%) - LeetHub

---
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        // the previous appeared idx of rains[i]
+        Map<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> zeros = new TreeSet<>();
+        int[] res = new int[rains.length];
+        for (int i = 0; i < rains.length; i++) {
+            if (rains[i] == 0) {
+                zeros.add(i);
+            } else {
+                if (map.containsKey(rains[i])) {
+                    // find the location of zero that can be used to empty rains[i]
+                    Integer next = zeros.ceiling(map.get(rains[i]));
+                    if (next == null) return new int[0];
+                    res[next] = rains[i];
+                    zeros.remove(next);
+                }
+                res[i] = -1;
+				map.put(rains[i], i);
+            }
+        }
+        for (int i : zeros) res[i] = 1;
+        return res;
+    }
+}
