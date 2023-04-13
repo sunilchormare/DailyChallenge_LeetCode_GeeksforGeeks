ea265feb708e8f4732862dc15f69e983c5b2169a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-13 07:27:17 +0530

    Time: 4 ms (28.10%), Space: 42.2 MB (54.27%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,30 +1,14 @@
 class Solution {
-public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-//          stack<int> stack;
-//         int i = 0;
-//         for (int x : pushed) {
-//             stack.push(x);
-            
-//             while (stack.size() && stack.top() == popped[i]) {
-//                 stack.pop();
-//                 i++;
-//             }
-//         }
-//         return stack.size() == 0;
-        
-        
-        int i = 0, j = 0;
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
         for (int x : pushed) {
-            pushed[i++] = x;
-           // cout<<" i= "<<i<<" pushed["<<i<<"] = "<<pushed[i]<<endl;
-            while (i > 0 && pushed[i - 1] == popped[j])
-            {
-                --i, ++j;
-                //cout<<" i= "<<i<<" pushed["<<i<<"] = "<<pushed[i]<<endl;
+            stack.push(x);
+            while (!stack.empty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
             }
-            //cout<<endl;
         }
-        return i == 0;
+        return stack.empty();
     }
-};
+}
