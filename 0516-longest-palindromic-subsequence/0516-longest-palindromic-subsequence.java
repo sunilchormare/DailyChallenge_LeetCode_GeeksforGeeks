a3d29745c80457a45602a4c5526342402f1ac1cb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-14 07:39:10 +0530

    Time: 53 ms (73.13%), Space: 52 MB (35.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,15 +1,17 @@
 class Solution {
-public:
-    int memo[1000][1000] = {};
-    int longestPalindromeSubseq(const string& s) {
-        return dp(s, 0, s.size() - 1);
+    public int longestPalindromeSubseq(String s) {
+         int[][] dp = new int[s.length()][s.length()];
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][s.length()-1];
     }
-    int dp(const string& s, int l, int r) {
-        if (l > r) return 0; // Return 0 since it's empty string
-        if (l == r) return 1; // Return 1 since it has 1 character
-        if (memo[l][r] != 0) return memo[l][r];
-        if (s[l] == s[r])
-            return memo[l][r] = dp(s, l+1, r-1) + 2;
-        return memo[l][r] = max(dp(s, l+1, r), dp(s, l, r-1));
-    }
-};
+}
