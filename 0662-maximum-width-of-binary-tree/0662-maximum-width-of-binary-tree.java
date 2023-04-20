a8ecf9a6c51dca198532c4f4b2988c0061e36ac3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-20 07:15:54 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (37.26%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,39 +1,17 @@
 class Solution {
-public:
-    int widthOfBinaryTree(TreeNode* root) {
-    
-     if(!root) return 0;
-        
-        //similar to level order
-        queue<pair<TreeNode *, unsigned long long int>> q;
-        int maxwidth =0;
-        q.push(make_pair(root, 0));
-        
-        while(!q.empty()){
-            
-            
-            unsigned long long int left_index = q.front().second, right_index = 0;
-            unsigned long long int sz = q.size();
-            
-            
-            for(unsigned long long int i=0; i<sz; i++){
-                
-                pair<TreeNode *, unsigned long long int> p = q.front();
-                q.pop();
-                TreeNode *temp = p.first;
-                right_index = p.second;
-                
-                if(temp->left)
-                    q.push(make_pair(temp->left, 2*right_index + 1));
-                if(temp->right)
-                    q.push(make_pair(temp->right, 2*right_index + 2));
-                
-            }
-            
-            maxwidth = max(maxwidth, int(right_index - left_index + 1));
-        }
-        
-        return maxwidth;
-        
+   public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 0, 1, new ArrayList<Integer>(), new ArrayList<Integer>());
     }
-};
+    
+    public int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end){
+        if(root == null)return 0;
+        if(start.size() == level){
+            start.add(order); end.add(order);
+        }
+        else end.set(level, order);
+        int cur = end.get(level) - start.get(level) + 1;
+        int left = dfs(root.left, level + 1, 2*order, start, end);
+        int right = dfs(root.right, level + 1, 2*order + 1, start, end);
+        return Math.max(cur, Math.max(left, right));
+    }
+}
