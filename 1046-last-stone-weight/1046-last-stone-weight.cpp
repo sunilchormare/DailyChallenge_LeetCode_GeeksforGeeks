0a1f6bbf022a24e16663268e0f994fb0d237a3f6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-24 07:15:23 +0530

    Time: 5 ms (28.30%), Space: 7.6 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,7 +5,8 @@ public:
         while (pq.size() > 1) {
             int x = pq.top(); pq.pop();
             int y = pq.top(); pq.pop();
-            if (x > y) pq.push(x - y);
+            if (x > y) 
+                pq.push(x - y);
         }
         return pq.empty() ? 0 : pq.top();
     }
