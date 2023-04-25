f430b8a503667af9fb7bbb7b6278b60b0b36a70d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-25 08:06:10 +0530

    Time: 54 ms (79.71%), Space: 89 MB (17.39%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -1,20 +1,22 @@
 class Solution {
-public:
-    vector<int> productQueries(int n, vector<vector<int>>& queries) {
-       int m = 1e9+7;
-        vector<int> bint, ans;
-        //create power array 
-        for(int i = 0; i < 32; ++i){
-            if((n & (1<<i)) != 0) bint.push_back(1<<i);
-        }
-        for(auto q: queries){
-            int i = q[0];
-            long p = bint[i++];
-            while(i <= q[1]){
-                p = (p * bint[i++])%m;
-            }
-            ans.push_back(p);
-        }
-        return ans;  
+    public int[] productQueries(int n, int[][] queries) {
+         int m = 1000000007;
+    int ans[]=new int[queries.length];
+    ArrayList<Integer> bint=new ArrayList<>();
+    //create power array 
+    for(int i = 0; i < 32; ++i){
+        if((n & (1<<i)) != 0) bint.add(1<<i);
     }
-};
+    int k=0;
+    for(int q[]: queries){
+        int i = q[0];
+        long p = bint.get(i++);
+        while(i <= q[1]){
+            p = (p * bint.get(i++))%m;
+        }
+        ans[k++]=(int)p;
+    }
+    return ans;
+
+    }
+}
