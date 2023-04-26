7b1909765b0a46c5e7a35cb7351e64c542be3e37
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-04-26 07:35:46 +0530

    Time: 39 ms (69.42%), Space: 56.4 MB (44.63%) - LeetHub

--- 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.java
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.java
@@ -1,20 +1,19 @@
 class Solution {
-public:
-    int maxNonOverlapping(vector<int>& nums, int target) {
-      unordered_map<int, int> dp;
-    dp[0] = -1;
-    int sum = 0, right = -1, cnt = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-        sum += nums[i];
-        if (dp.count(sum - target)) {
-            int left = dp[sum - target];
-            if (right <= left) {
-                ++cnt;
-                right = i;
-            }
-        }
-        dp[sum] = i;
+    public int maxNonOverlapping(int[] nums, int target) {
+      Map<Integer, Integer> map = new HashMap<>();
+map.put(0, 0);
+
+int res = 0;
+int sum = 0;
+
+for (int i = 0; i < nums.length; ++i) {
+	sum += nums[i];
+	if (map.containsKey(sum - target)) {
+		res = Math.max(res, map.get(sum - target) + 1);
+	}
+	map.put(sum, res);
+}
+
+return res;  
     }
-    return cnt;  
-    }
-};
+}
