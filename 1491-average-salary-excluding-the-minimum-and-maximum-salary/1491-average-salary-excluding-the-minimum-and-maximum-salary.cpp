e3df33b655c36f993946aaa7c58f2a134e93439f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-01 10:24:04 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (84.06%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        sort(salary.begin(),salary.end());
+//         sort(salary.begin(),salary.end());
         
-        double sum=0.0,c=0.0;
-        for(int i=1;i<salary.size()-1;++i)
-        {
-            sum+=salary[i];
-            c++;
-        }
-        return (sum/c);
+//         double sum=0.0,c=0.0;
+//         for(int i=1;i<salary.size()-1;++i)
+//         {
+//             sum+=salary[i];
+//             c++;
+//         }
+//         return (sum/c);
+        
+        int mi=INT_MAX,ma=INT_MIN,s=0;
+        for(auto i:salary)
+            mi=min(mi,i),ma=max(ma,i),s+=i;
+        return (s-ma-mi)*1.0/(salary.size()-2);
     }
 };
