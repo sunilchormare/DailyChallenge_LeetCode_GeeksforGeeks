d39ef77ce3b755eec58f74f4b191c45a10b423d5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-17 10:57:00 +0530

    Time: 165 ms (36.69%), Space: 94.2 MB (94.96%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,13 +1,14 @@
 class Solution {
- public int waysToMakeFair(int[] A) {
-        int res = 0, n = A.length, left[] = new int[2], right[] = new int[2];
-        for (int i = 0; i < n; i++)
-            right[i%2] += A[i]; 
-        for (int i = 0; i < n; i++) {
-            right[i%2] -= A[i];
-            if (left[0]+right[1] == left[1]+right[0]) res++;
-            left[i%2] += A[i];
+public:
+     int waysToMakeFair(vector<int>& A) {
+        int res = 0, n = A.size(), left[2] = {}, right[2] = {};
+        for (int i = 0; i < n; ++i)
+            right[i % 2] += A[i];
+        for (int i = 0; i < n; ++i) {
+            right[i % 2] -= A[i];
+            res += left[0] + right[1] == left[1] + right[0];
+            left[i % 2] += A[i];
         }
         return res;
     }
-}
+};
