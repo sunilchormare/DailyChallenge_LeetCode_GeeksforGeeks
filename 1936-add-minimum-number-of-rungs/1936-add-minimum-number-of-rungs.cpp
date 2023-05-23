ae90e5745820f0c53df4f2b9a4a79205ded39a8f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-23 05:40:42 +0530

    Time: 127 ms (15.30%), Space: 54.1 MB (66.04%) - LeetHub

---
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+   int addRungs(vector<int>& A, int dist) {
+        int pre = 0, res = 0;
+        for (int a: A) {
+            res += (a - pre - 1) / dist;
+            pre = a;
+        }
+        return res;
+    }
+};
