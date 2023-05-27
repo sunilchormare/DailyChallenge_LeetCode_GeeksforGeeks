92aaf868059b060c70c2c45c4c6da7a4f2a00b3b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-27 18:28:02 +0530

    Time: 231 ms (89.34%), Space: 132.3 MB (72.27%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,15 +1,16 @@
 class Solution {
-     public String stoneGameIII(int[] A) {
-        int n = A.length, dp[] = new int[n+1];
+public:
+    string stoneGameIII(vector<int>& A) {
+        int n = A.size();
+        vector<int> dp(n, -1e9);
         for (int i = n - 1; i >= 0; --i) {
-            dp[i] = Integer.MIN_VALUE;
             for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
                 take += A[i + k];
-                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+                dp[i] = max(dp[i], take - (i + k + 1 < n ? dp[i + k + 1] : 0));
             }
         }
         if (dp[0] > 0) return "Alice";
         if (dp[0] < 0) return "Bob";
         return "Tie";
     }
-}
+};
