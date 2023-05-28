6b623f4f37d76ebf0de6dcb7ab59fd7e2c2ec1d7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-28 07:26:51 +0530

    Time: 463 ms (11.66%), Space: 419.5 MB (8.27%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
@@ -1,8 +1,10 @@
-class Solution:
-    def largestMerge(self, s1, s2):
-        if s1 >= s2 > '':
-            return s1[0] + self.largestMerge(s1[1:], s2)
-        if s2 >= s1 > '':
-            return s2[0] + self.largestMerge(s1, s2[1:])
-        return s1 + s2
-        
+class Solution {
+public:
+     string largestMerge(string s1, string s2) {
+        if (s1.size() == 0  || s2.size() == 0)
+            return s1 + s2;
+        if (s1 > s2)
+            return s1[0] + largestMerge(s1.substr(1), s2);
+        return s2[0] + largestMerge(s1, s2.substr(1));
+    }
+};
