7fc11edd8a3f7a6924e78ee1364c2864f8770830
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-05-30 07:21:13 +0530

    Time: 264 ms (23.72%), Space: 54.2 MB (5.11%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.java
@@ -1,27 +1,25 @@
 class Solution {
-public:
     int n, sessionTime;
-    int memo[1<<14][15] = {}; 
-    int minSessions(vector<int>& tasks, int sessionTime) {
-        n = tasks.size();
-        this->sessionTime = sessionTime;
-        memset(memo, -1, sizeof(memo));
+    Integer[][] memo = new Integer[1<<14][15];
+    public int minSessions(int[] tasks, int sessionTime) {
+        n = tasks.length;
+        this.sessionTime = sessionTime;
         return dp(tasks, (1 << n) - 1, 0);
     }
-    int dp(vector<int>& tasks, int mask, int remainTime) {
+    int dp(int[] tasks, int mask, int remainTime) {
         if (mask == 0) return 0;
-        if (memo[mask][remainTime] != -1) return memo[mask][remainTime];
+        if (memo[mask][remainTime] != null) return memo[mask][remainTime];
         int ans = n;  // There is up to N work sessions
         for (int i = 0; i < n; ++i) {
-            if ((mask >> i) & 1) {
+            if (((mask >> i) & 1) == 1) {
                 int newMask = ~(1 << i) & mask; // clear i th bit
                 if (tasks[i] <= remainTime) {
-                    ans = min(ans, dp(tasks, newMask, remainTime - tasks[i])); // Consume current session
+                    ans = Math.min(ans, dp(tasks, newMask, remainTime - tasks[i])); // Consume current session
                 } else {
-                    ans = min(ans, dp(tasks, newMask, sessionTime - tasks[i]) + 1); // Create new session
+                    ans = Math.min(ans, dp(tasks, newMask, sessionTime - tasks[i]) + 1); // Create new session
                 }
             }
         }
         return memo[mask][remainTime] = ans;
     }
-};
+}
