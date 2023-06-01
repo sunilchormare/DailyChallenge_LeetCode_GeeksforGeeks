a3f9496f4645abb4dda89f4bfc59e2395d867240
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-01 07:27:31 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (16.92%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        if (ops == null || ops.length == 0) {
+            return m * n;
+        }
+        
+        int row = Integer.MAX_VALUE, col = Integer.MAX_VALUE;
+        for(int[] op : ops) {
+            row = Math.min(row, op[0]);
+            col = Math.min(col, op[1]);
+        }
+        
+        return row * col;
+    }
+}
