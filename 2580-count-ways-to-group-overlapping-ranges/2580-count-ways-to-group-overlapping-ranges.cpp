672d4176ec7fc098cfa8ff4bc137858fb9b3e956
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-02 07:49:21 +0530

    Time: 397 ms (18.82%), Space: 82.1 MB (26.97%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -1,12 +1,13 @@
 class Solution {
-    public int countWays(int[][] ranges) {
-        int res = 1, last = -1, mod = (int)1e9 + 7;
-        Arrays.sort(ranges, (a, b) -> a[0] - b[0]);
-        for (int[] r: ranges) {   
+public:
+    int countWays(vector<vector<int>>& ranges) {
+        int res = 1, last = -1, mod = 1e9 + 7;
+        sort(ranges.begin(), ranges.end());
+        for (auto r: ranges) {   
             if (last < r[0])
                 res = res * 2 % mod;
-            last = Math.max(last, r[1]);
+            last = max(last, r[1]);
         }
         return res;
     }
-}
+};
