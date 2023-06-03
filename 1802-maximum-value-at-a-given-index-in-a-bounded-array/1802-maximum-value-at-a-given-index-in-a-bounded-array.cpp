9b8a1d37ac6b4797160ba3d1b728e7fbb61f984f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-03 10:55:30 +0530

    Time: 4 ms (40.44%), Space: 5.9 MB (93.33%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
-     public int maxValue(int n, int index, int maxSum) {
+public:
+    int maxValue(int n, int index, int maxSum) {
         maxSum -= n;
         int left = 0, right = maxSum, mid;
         while (left < right) {
@@ -11,12 +12,12 @@ class Solution {
         }
         return left + 1;
     }
-    
-    private long test(int n, int index, int a) {
-        int b = Math.max(a - index, 0);
-        long res = (long)(a + b) * (a - b + 1) / 2;
-        b = Math.max(a - ((n - 1) - index), 0);
-        res += (long)(a + b) * (a - b + 1) / 2;
+
+    long test(int n, int index, int a) {
+        int b = max(a - index, 0);
+        long res = long(a + b) * (a - b + 1) / 2;
+        b = max(a - ((n - 1) - index), 0);
+        res += long(a + b) * (a - b + 1) / 2;
         return res - a;
     }
-}
+};
