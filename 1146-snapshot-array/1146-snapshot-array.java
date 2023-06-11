87e51f6b390142e9e470bd8bf2449bf8d240f3df
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-11 08:28:28 +0530

    Time: 66 ms (61.43%), Space: 77.4 MB (60.86%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -1,25 +1,23 @@
 class SnapshotArray {
-private:
-    map<int, map<int, int>> snaps;
-    int snapId = 0;
-public:
-    SnapshotArray(int length) {
+    TreeMap<Integer, Integer>[] A;
+    int snap_id = 0;
+    public SnapshotArray(int length) {
+        A = new TreeMap[length];
         for (int i = 0; i < length; i++) {
-            map<int, int> mp; mp[0] = 0;
-            snaps[i] = mp;
+            A[i] = new TreeMap<Integer, Integer>();
+            A[i].put(0, 0);
         }
     }
-    
-    void set(int index, int val) {
-        snaps[index][snapId] = val;
+
+    public void set(int index, int val) {
+        A[index].put(snap_id, val);
     }
-    
-    int snap() {
-        return snapId++;
+
+    public int snap() {
+        return snap_id++;
     }
-    
-    int get(int index, int snap_id) {
-        auto it = snaps[index].upper_bound(snap_id); it--;
-        return it->second;
+
+    public int get(int index, int snap_id) {
+        return A[index].floorEntry(snap_id).getValue();
     }
-};
+}
