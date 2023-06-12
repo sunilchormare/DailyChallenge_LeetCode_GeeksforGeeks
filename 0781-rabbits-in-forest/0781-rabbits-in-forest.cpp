751f193f81736b8fcfd21ebaa13c95f4ce990aae
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-12 07:25:03 +0530

    Time: 9 ms (24.43%), Space: 8.7 MB (38.19%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> c;
+        for (int i : answers) c[i]++;
+        int res = 0;
+        for (auto i : c) res += (i.second + i.first) / (i.first + 1) * (i.first + 1);
+        return res;
+    }
+};
