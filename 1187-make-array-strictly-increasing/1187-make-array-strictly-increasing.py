c986c06ec4d73a8833eff603c53b28e9bef3ee84
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-17 07:22:21 +0530

    Time: 682 ms (78.46%), Space: 16.7 MB (74.62%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
@@ -1,42 +1,17 @@
-class Solution {
-    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        Arrays.sort(arr2);
-        int n = arr2.length;
-        dp.put(-1, 0);
+class Solution:
+    def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
+        dp = {-1: 0}
+        arr2.sort()
+        n = len(arr2)
+        
+        for i in range(len(arr1)):
+            new_dp = collections.defaultdict(lambda: float('inf'))
+            for prev in dp:
+                if arr1[i] > prev:
+                    new_dp[arr1[i]] = min(new_dp[arr1[i]], dp[prev])
+                idx = bisect.bisect_right(arr2, prev)
+                if idx < n:
+                    new_dp[arr2[idx]] = min(new_dp[arr2[idx]], 1 + dp[prev])
+            dp = new_dp
 
-        for (int i = 0; i < arr1.length; i++) {
-            Map<Integer, Integer> newDp = new HashMap<>();
-            for (int prev : dp.keySet()) {
-                if (arr1[i] > prev) {
-                    newDp.put(arr1[i], Math.min(newDp.getOrDefault(arr1[i], Integer.MAX_VALUE), dp.get(prev)));
-                }
-                int idx = bisectRight(arr2, prev);
-                if (idx < n) {
-                    newDp.put(arr2[idx], Math.min(newDp.getOrDefault(arr2[idx], Integer.MAX_VALUE), 1 + dp.get(prev)));
-                }
-            }
-            dp = newDp;
-        }
-        
-        int answer = Integer.MAX_VALUE;
-        for (int value : dp.values()) {
-            answer = Math.min(answer, value);
-        }
-        
-        return answer == Integer.MAX_VALUE ? -1 : answer;
-    }
-    
-    private static int bisectRight(int[] arr, int value) {
-        int left = 0, right = arr.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] <= value) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }
-}
+        return min(dp.values()) if dp else -1
