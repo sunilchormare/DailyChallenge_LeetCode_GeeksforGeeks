7c4eb6a1cb43cdd4a45d9354d3bdb998dfb0b799
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-22 07:58:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (60.00%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,13 +1,10 @@
 class Solution {
-    public int minimumOneBitOperations(int n) {
-        return minimumOneBitOperations(n, 0);
-    }
-
-    public int minimumOneBitOperations(int n, int res) {
+public:
+     int minimumOneBitOperations(int n, int res = 0) {
         if (n == 0) return res;
         int b = 1;
         while ((b << 1) <= n)
             b = b << 1;
         return minimumOneBitOperations((b >> 1) ^ b ^ n, res + b);
     }
-}
+};
