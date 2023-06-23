12f2b7719d7f35070eaaaea80246caa40e569212
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-06-23 10:33:32 +0530

    Time: 273 ms (88.44%), Space: 86.6 MB (82.41%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
-   public long countExcellentPairs(int[] A, int k) {
-        long cnt[] = new long[30], res = 0;
-        Set<Integer> set = new HashSet<>();
-        for (int a : A)
-            set.add(a);
-        for (int a : set)
-            cnt[Integer.bitCount(a)]++;
+public:
+  long long countExcellentPairs(vector<int>& A, int k) {
+        long long cnt[30] = {}, res = 0;
+        for (int a : unordered_set<int>(begin(A), end(A)))
+            ++cnt[__builtin_popcount(a)];
         for (int i = 1; i < 30; ++i)
             for (int j = 1; j < 30; ++j)
                 if (i + j >= k)
                     res += cnt[i] * cnt[j];
         return res;
     }
-}
+};
