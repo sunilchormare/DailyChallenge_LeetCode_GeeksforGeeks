3e8854473146e6b9b5e30b53b2a07f1cbd0cef1b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-02 08:52:49 +0530

    Time: 96 ms (86.27%), Space: 8.9 MB (51.96%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int ans=0;
-    void helper(int i,int l,vector<int>&temp,vector<vector<int>>&v)
+    //int ans=0;
+    void helper(int i,int l,vector<int>&temp,vector<vector<int>>&v,int& ans)
     {
        if(i==v.size())
        {
@@ -9,19 +9,20 @@ public:
            ans=max(ans,l);
            return;
        }
-        helper(i+1,l,temp,v);
+        helper(i+1,l,temp,v,ans);
         temp[v[i][0]]--;
         temp[v[i][1]]++;
         
-        helper(i+1,l+1,temp,v);
+        helper(i+1,l+1,temp,v,ans);
         temp[v[i][0]]++;
         temp[v[i][1]]--;
-        
     }
     
-    int maximumRequests(int n, vector<vector<int>>& requests) {
+    int maximumRequests(int n, vector<vector<int>>& requests)
+    {
+        int ans=0;
         vector<int> temp(n,0);
-        helper(0,0,temp,requests);
+         helper(0,0,temp,requests,ans);
         return ans;
     }
 };
