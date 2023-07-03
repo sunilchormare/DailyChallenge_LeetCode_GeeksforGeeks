76fc7623223deca4bf4a69abda4d38c883c3e1b0
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-03 07:39:12 +0530

    Time: 593 ms (75.82%), Space: 30.1 MB (95.60%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,12 +1,9 @@
-class Solution {
-   public long maxRunTime(int n, int[] A) {
-        Arrays.sort(A);
-        long sum = 0;
-        for (int a: A)
-            sum += a;
-        int k = 0, na = A.length;
-        while (A[na - 1 - k] > sum / (n - k))
-            sum -= A[na - 1 - k++];
-        return sum / (n - k);
-    }
-}
+class Solution:
+     def maxRunTime(self, n, A):
+        A.sort()
+        su = sum(A)
+        while A[-1] > su / n:
+            n -= 1
+            su -= A.pop()
+        return int(su / n)
+        
