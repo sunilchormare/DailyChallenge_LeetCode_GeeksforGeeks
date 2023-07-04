479f7fc5ab81f642ec213e492bb3b11e97e0dcce
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-04 07:14:40 +0530

    Time: 5 ms (78.81%), Space: 6.3 MB (91.53%) - LeetHub

--- 0903-valid-permutations-for-di-sequence/0903-valid-permutations-for-di-sequence.cpp
+++ 0903-valid-permutations-for-di-sequence/0903-valid-permutations-for-di-sequence.cpp
@@ -1,14 +1,16 @@
-class Solution:
-    def numPermsDISequence(self, S):
-        dp = [1] * (len(S) + 1)
-        for c in S:
-            if c == "I":
-                dp = dp[:-1]
-                for i in range(1, len(dp)):
-                    dp[i] += dp[i - 1]
-            else:
-                dp = dp[1:]
-                for i in range(len(dp) - 1)[::-1]:
-                    dp[i] += dp[i + 1]
-        return dp[0] % (10**9 + 7)
-        
+class Solution {
+public:
+    int numPermsDISequence(string S) {
+        int n = S.length(), mod = 1e9 + 7;
+        vector<int> dp(n + 1, 1), dp2(n);
+        for (int i = 0; i < n; dp = dp2, i++) {
+            if (S[i] == 'I')
+                for (int j = 0, cur = 0; j < n - i; j++)
+                    dp2[j] = cur = (cur + dp[j]) % mod;
+            else
+                for (int j = n - i - 1, cur = 0; j >= 0; j--)
+                    dp2[j] = cur = (cur + dp[j + 1]) % mod;
+        }
+        return dp[0];
+    }
+};
