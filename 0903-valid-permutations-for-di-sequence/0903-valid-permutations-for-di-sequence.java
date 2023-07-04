ffc5888e9cea49c8192bd6ce03d3ecee283e3674
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-04 07:13:29 +0530

    Time: 2 ms (100.00%), Space: 41.6 MB (50.00%) - LeetHub

--- 0903-valid-permutations-for-di-sequence/0903-valid-permutations-for-di-sequence.java
+++ 0903-valid-permutations-for-di-sequence/0903-valid-permutations-for-di-sequence.java
@@ -1,16 +1,17 @@
 class Solution {
-public:
-    int numPermsDISequence(string S) {
-        int n = S.length(), mod = 1e9 + 7;
-        vector<int> dp(n + 1, 1), dp2(n);
-        for (int i = 0; i < n; dp = dp2, i++) {
-            if (S[i] == 'I')
+    public int numPermsDISequence(String S) {
+        int n = S.length(), mod = (int)1e9 + 7;
+        int[] dp = new int[n + 1], dp2 = new int[n];;
+        for (int j = 0; j <= n; j++) dp[j] = 1;
+        for (int i = 0; i < n; i++) {
+            if (S.charAt(i) == 'I')
                 for (int j = 0, cur = 0; j < n - i; j++)
                     dp2[j] = cur = (cur + dp[j]) % mod;
             else
                 for (int j = n - i - 1, cur = 0; j >= 0; j--)
                     dp2[j] = cur = (cur + dp[j + 1]) % mod;
+            dp = Arrays.copyOf(dp2, n);
         }
         return dp[0];
     }
-};
+}
