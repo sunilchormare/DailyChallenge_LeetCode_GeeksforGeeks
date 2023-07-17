e95885509467d6b6ed70ee1442690a2d7015477a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-17 06:55:10 +0530

    Time: 4 ms (94.20%), Space: 41.1 MB (77.54%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.java
@@ -0,0 +1,10 @@
+class Solution {
+    Map<Integer, Integer> dp = new HashMap<>();
+public int minDays(int n) {
+    if (n <= 1)
+        return n;
+    if (!dp.containsKey(n))
+        dp.put(n, 1 + Math.min(n % 2 + minDays(n / 2), n % 3 + minDays(n / 3)));
+    return dp.get(n);
+}
+}
