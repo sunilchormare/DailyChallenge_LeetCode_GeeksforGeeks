ea79ae2a76a09b12ae0ef31cce51776485001151
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-26 07:20:07 +0530

    Time: 202 ms (67.47%), Space: 58.9 MB (81.66%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,23 +1,22 @@
 class Solution {
-public:
-    int minSpeedOnTime(vector<int>& dist, double hour) {
-         int n = dist.size();
-        int mini = 1, maxi = 10000000;
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int n = dist.length;
+        int min = 1, max = 10000000;
         int ans = -1;
-        while(mini <= maxi){
-            int mid = (maxi + mini)/2;
+        while(min <= max){
+            int mid = (max + min)/2;
             double sum = 0;
             for(int i = 0; i<n-1; ++i){
-                sum += ceil( ( (double) dist[i]) /mid);
+                sum += Math.ceil( ( (double) dist[i]) /mid);
             }
             sum = sum + ( ( (double) dist[n-1]) /mid);
             if(sum > hour){
-                mini = mid+1;
+                min = mid+1;
             }else{
                 ans = mid;
-                maxi = mid-1;
+                max = mid-1;
             }
         }
         return ans;
     }
-};
+}
