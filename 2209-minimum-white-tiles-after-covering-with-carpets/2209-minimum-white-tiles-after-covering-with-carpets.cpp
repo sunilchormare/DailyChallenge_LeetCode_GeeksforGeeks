067f50f875c7055c52c3536b03a99a652bf1c7b7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-07-28 06:54:10 +0530

    Time: 680 ms (79.09%), Space: 254.4 MB (36.36%) - LeetHub

---
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   int minimumWhiteTiles(string s, int nc, int l) {
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(nc + 1));
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 0; k <= nc; ++k) {
+                int jump = dp[i - 1][k] + s[i - 1] - '0';
+                int cover = k > 0 ? dp[max(i - l, 0)][k - 1] : 1000;
+                dp[i][k] = min(cover, jump);
+            }
+        }
+        return dp[n][nc];
+    }
+};
