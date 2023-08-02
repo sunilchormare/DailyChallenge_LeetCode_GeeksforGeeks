971a9d499fd054642c7001304b67394339748bbd
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-02 06:50:24 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (84.41%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(auto i:nums)
-            m[i]++;
-        int sum=0;
-        for(int i=0;i<nums.size();++i)
-        {
-            if(m[nums[i]]<=1)
-                sum+=nums[i];
-        }
-        return sum;
+        //Solution 1 : MAP
+        // unordered_map<int,int> m;
+        // for(auto i:nums)
+        //     m[i]++;
+        // int sum=0;
+        // for(int i=0;i<nums.size();++i)
+        // {
+        //     if(m[nums[i]]<=1)
+        //         sum+=nums[i];
+        // }
+        // return sum;
+        
+        //Solution 2: VECTOR
+        
+        int cnt[101] = {}, res = 0;
+    for (auto n : nums)
+        ++cnt[n];
+    for (auto i = 1; i <= 100; ++i)
+        if (cnt[i] == 1)
+            res += i;
+    return res;
     }
 };
