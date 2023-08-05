8e9405fa4e43f7d605a8754388d10b56dc603e59
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-05 07:03:24 +0530

    Time: 1 ms (99.88%), Space: 43.5 MB (78.67%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -1,26 +1,42 @@
+
 class Solution {
-public:
-    vector<TreeNode*> rec( int start , int end  ){
-      if( start > end ){
-        return {NULL};
-      }
-      vector<TreeNode*> leftlist, rightlist,result;
-      for( int i = start ; i <= end ; i++ ){
-        leftlist = rec( start , i-1 );
-        rightlist = rec( i+1,end);
-        for( auto l : leftlist ){
-          for( auto r : rightlist ){
-            TreeNode* node = new TreeNode(i);
-            node->left = l;
-            node->right = r;
-            result.push_back(node);
-          }
+   public List<TreeNode> generateTrees(int n) {
+    
+    if(n==0){
+        return null;
+    }
+    
+    return solve(1 ,n);   
+}
+
+public List<TreeNode> solve(int start ,int end){
+    
+    if(start>end){
+        ArrayList<TreeNode> al = new ArrayList();
+        al.add(null);
+        return al;
+    }
+    
+    List<TreeNode> treeList = new ArrayList();
+    
+    for(int curr=start;curr<=end;curr++){
+        
+        List<TreeNode> leftTrees = solve(start ,curr-1);
+        List<TreeNode> rightTrees = solve(curr+1 ,end);
+        
+        for(int l=0;l<leftTrees.size();l++){
+            for(int r=0;r<rightTrees.size();r++){
+                
+                TreeNode node = new TreeNode(curr);
+                
+                node.left = leftTrees.get(l);
+                node.right = rightTrees.get(r);
+                
+                treeList.add(node);
+            }   
         }
-      }
-      
-      return result;
     }
-    vector<TreeNode*> generateTrees(int n) {
-        return rec( 1, n );
-    }
-};
+    
+    return treeList; 
+}
+}
