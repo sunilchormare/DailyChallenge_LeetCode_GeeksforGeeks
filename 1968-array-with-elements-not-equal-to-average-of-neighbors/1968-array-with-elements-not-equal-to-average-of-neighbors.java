9313c4c74350575fd4939ffe104948d3cec180bf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-07 07:07:25 +0530

    Time: 43 ms (80.84%), Space: 60.9 MB (87.72%) - LeetHub

---
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -0,0 +1,11 @@
+class Solution {
+     public int[] rearrangeArray(int[] A) {
+        Arrays.sort(A);
+        for (int i = 1; i < A.length; i += 2) {
+            int tmp = A[i];
+            A[i] = A[i - 1];
+            A[i - 1] = tmp;
+        }
+        return A;
+    }
+}
