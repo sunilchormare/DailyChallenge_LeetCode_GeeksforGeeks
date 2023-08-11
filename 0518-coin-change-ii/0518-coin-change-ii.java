a2ca6806bce160b4eb9a42f1b043c82adcc86b22
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-11 07:01:53 +0530

    Time: 2 ms (100.00%), Space: 39.6 MB (98.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,13 +1,12 @@
 class Solution {
-public:
-    int change(int amount, vector<int>& coins) {
-         vector<int> dp(amount + 1);
+    public int change(int amount, int[] coins) {
+          int[] dp=new int[amount + 1];
         dp[0] = 1;
         for (int coin : coins) {
             for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i-coin];
             }
         }
-        return dp[amount];
+        return dp[amount]; 
     }
-};
+}
