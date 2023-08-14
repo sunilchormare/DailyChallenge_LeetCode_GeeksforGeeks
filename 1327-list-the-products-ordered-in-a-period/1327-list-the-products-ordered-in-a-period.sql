ea0e30da005a0ab720caec64aab6838280e06ded
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-14 07:29:04 +0530

    Time: 2024 ms (10.24%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,8 +1,15 @@
-select p.product_name,
-sum(o.unit) as unit
-from Products p
-join Orders o
-on p.product_id = o.product_id
-where Left(order_date, 7) = '2020-02'
-group by p.product_name
-having sum(o.unit) >= 100
+# select p.product_name,
+# sum(o.unit) as unit
+# from Products p
+# join Orders o
+# on p.product_id = o.product_id
+# where Left(order_date, 7) = '2020-02'
+# group by p.product_name
+# having sum(o.unit) >= 100
+
+
+SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
+JOIN Orders o USING (product_id)
+WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
+GROUP BY p.product_id
+HAVING SUM(o.unit)>=100
