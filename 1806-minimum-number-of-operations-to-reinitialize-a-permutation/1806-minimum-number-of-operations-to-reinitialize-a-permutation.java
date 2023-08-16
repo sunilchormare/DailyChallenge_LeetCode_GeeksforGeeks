871d7841023cc29ffce450d3f867154798d6be12
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-16 07:38:38 +0530

    Time: 2 ms (81.18%), Space: 5.9 MB (96.24%) - LeetHub

--- 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.java
+++ 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.java
@@ -1,10 +1,11 @@
 class Solution {
-    public int reinitializePermutation(int n) {
-    int res = 0, i = 1;
+public:
+    int reinitializePermutation(int n) {
+        int res = 0, i = 1;
         while (res == 0 || i > 1) {
             i = i * 2 % (n - 1);
             res++;
         }
-        return res;    
+        return res;
     }
-}
+};
