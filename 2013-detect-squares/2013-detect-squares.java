029df0026070eccc4f549733afbfe5e2fdea5744
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-22 18:55:23 +0530

    Time: 69 ms (79.07%), Space: 56.9 MB (23.94%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,20 +1,20 @@
-class DetectSquares { // 216 ms, faster than 66.67%
-public:
-    int cntPoints[1001][1001] = {};
-    vector<pair<int, int>> points;
+class DetectSquares { // 128 ms, faster than 60.00%
+    int[][] cntPoints = new int[1001][1001];
+    List<int[]> points = new ArrayList<>();
 
-    void add(vector<int> p) {
-        cntPoints[p[0]][p[1]]++;
-        points.emplace_back(p[0], p[1]);
+    public void add(int[] p) {
+        cntPoints[p[0]][p[1]] += 1;
+        points.add(p);
     }
 
-    int count(vector<int> p1) {
+    public int count(int[] p1) {
         int x1 = p1[0], y1 = p1[1], ans = 0;
-        for (auto& [x3, y3] : points) {
-            if (abs(x1-x3) == 0 || abs(x1-x3) != abs(y1-y3))
+        for (int[] p3 : points) {
+            int x3 = p3[0], y3 = p3[1];
+            if (Math.abs(x1-x3) == 0 || Math.abs(x1-x3) != Math.abs(y1-y3))
                 continue; // Skip empty square or invalid square point!
             ans += cntPoints[x1][y3] * cntPoints[x3][y1];
         }
         return ans;
     }
-};
+}
