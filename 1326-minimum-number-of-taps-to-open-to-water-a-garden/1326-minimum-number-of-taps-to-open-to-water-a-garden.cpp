a511af63d8dee0aa108451251fb5f2b5cf84a5fc
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-08-31 21:47:52 +0530

    Time: 45 ms (41.86%), Space: 14.6 MB (72.16%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    public int minTaps(int n, int[] A) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, n + 2);
+public:
+     int minTaps(int n, vector<int>& A) {
+        vector<int> dp(n + 1, n + 2);
         dp[0] = 0;
         for (int i = 0; i <= n; ++i)
-            for (int j = Math.max(i - A[i] + 1, 0); j <= Math.min(i + A[i], n); ++j)
-                dp[j] = Math.min(dp[j], dp[Math.max(0, i - A[i])] + 1);
+            for (int j = max(i - A[i] + 1, 0); j <= min(i + A[i], n); ++j)
+                dp[j] = min(dp[j], dp[max(0, i - A[i])] + 1);
         return dp[n]  < n + 2 ? dp[n] : -1;
     }
-}
+};
