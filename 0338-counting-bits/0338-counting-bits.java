a94df3962cf7ac413017ec121e95dc3dadc4dd44
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-01 06:34:49 +0530

    Time: 1 ms (99.63%), Space: 46.7 MB (25.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,8 @@
 class Solution {
-public:
-    vector<int> countBits(int num) {
-   vector<int> f(num + 1);
+    public int[] countBits(int num) {
+    int[] f = new int[num + 1];
     for (int i=1; i<=num; i++) 
         f[i] = f[i >> 1] + (i & 1);
-    return f;     
-    }
-};
+    return f;
+}
+}
