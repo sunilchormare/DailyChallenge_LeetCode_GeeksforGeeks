0f05ec4d52b009f45eb4aa76f6265127ad2cde59
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-03 10:41:20 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (27.86%) - LeetHub

---
+++ 0553-optimal-division/0553-optimal-division.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        int n=nums.size();
+        string ans;
+		
+		// check for size if its 1 then we can't use parantheses and 
+		// if its 2 then also we should not use it.
+		
+        if(n==1) return ans=to_string(nums[0]);
+        if(n==2) return ans=to_string(nums[0])+"/"+to_string(nums[1]);
+		
+		//  for size greater than 2 add paranthese after first number 
+		
+        ans=to_string(nums[0]);
+        ans.append("/(");
+        for(int i=1;i<n-1;i++){
+            ans.append(to_string(nums[i])+"/");
+        }
+        ans.append(to_string(nums[n-1]));
+        ans.append(")");
+		
+		// finally this becomes as a/(b/c/d/....) which is our answer
+		
+        return ans;
+    }
+};
