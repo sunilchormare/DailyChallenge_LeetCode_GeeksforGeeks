b73ef5a29ce6b059e3adbd0a9b1925723c965a9d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-16 12:03:08 +0530

    Time: 16 ms (51.44%), Space: 43.6 MB (35.29%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,9 @@
+class Solution {
+      public String mostCommonWord(String p, String[] banned) {
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = p.replaceAll("\\W+" , " ").toLowerCase().split("\\s+");
+        for (String w : words) if (!ban.contains(w)) count.put(w, count.getOrDefault(w, 0) + 1);
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
