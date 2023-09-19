71b279ba1432a11e99f69e542517b7390b583027
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-19 06:37:18 +0530

    Time: 65 ms (98.01%), Space: 61.7 MB (25.80%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     // int n=nums.size()-1;
+     //    int sumArr=0;
+     //    for(int i:nums)
+     //        sumArr+=i;     
+     //    int sum=(n*(n+1))/2;
+     //    cout<<sum;
+     //    return sumArr-sum;
+   int idx = 0;
+        while (nums[idx] == idx) ++idx;
+        
+        while (nums[idx] != idx) {
+            int tmp = idx;
+            idx = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return idx;
+    }
+};
