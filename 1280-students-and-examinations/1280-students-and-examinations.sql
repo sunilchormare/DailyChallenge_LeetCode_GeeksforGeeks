c12516db70de2673bd1300ef9228390a983d4916
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-19 06:47:49 +0530

    Time: 1502 ms (98.39%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,6 +1,19 @@
-SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
-FROM Students s
-CROSS JOIN Subjects sub
-LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
-GROUP BY s.student_id, s.student_name, sub.subject_name
-ORDER BY s.student_id, sub.subject_name;
+# SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+# FROM Students s
+# CROSS JOIN Subjects sub
+# LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+# GROUP BY s.student_id, s.student_name, sub.subject_name
+# ORDER BY s.student_id, sub.subject_name;
+
+SELECT
+    Students.student_id,
+    Students.student_name,
+    Subjects.subject_name,
+    COUNT(Examinations.subject_name) AS attended_exams
+FROM Students
+JOIN Subjects
+LEFT JOIN Examinations
+ON Students.student_id = Examinations.student_id
+AND Subjects.subject_name = Examinations.subject_name
+GROUP BY Students.student_id, Subjects.subject_name
+ORDER BY student_id ASC, subject_name ASC
