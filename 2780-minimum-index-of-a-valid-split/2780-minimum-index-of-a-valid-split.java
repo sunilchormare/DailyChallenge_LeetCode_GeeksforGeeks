c77248699fca08428327fe663bf5ac3b3f40758a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-25 09:09:28 +0530

    Time: 35 ms (42.78%), Space: 56.6 MB (57.75%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.java
@@ -1,20 +1,22 @@
 class Solution {
-public:
-    int minimumIndex(vector<int>& nums) {
-        unordered_map<int,int> m1,m2;
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++)
-            m2[nums[i]]++;
-        
-        for(int i=0;i<n;i++) {
-            int x = ++m1[nums[i]];
-            int y = --m2[nums[i]];
-            
-            if(x*2>(i+1) && y*2>(n-i-1)) //i+1 (total element in left), n-i-1 (total elements in right)
-            return i;
-        }
-        
-        return -1;
+  public int minimumIndex(List<Integer> nums) {
+    var map = new HashMap<Integer, Integer>();
+    map.put(0, 0);
+    int max = 0, m = nums.size();
+    
+    for (var n : nums) {
+      map.put(n, map.getOrDefault(n, 0) + 1);
+      
+      if (map.get(n) > map.get(max))
+        max = n;
     }
-};
+    var freq = 0;
+    for (var i=0; i<m; i++) {
+      if (nums.get(i) == max) freq++;
+      
+      if (freq * 2 > i+1 && (map.get(max) - freq) * 2 > (m-i-1))
+        return i;
+    }
+    return -1;
+  }
+}
