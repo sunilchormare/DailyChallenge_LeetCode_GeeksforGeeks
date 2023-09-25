af203de9ba00c60ddc68ef3313d87eda31a711d9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-25 09:00:00 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (71.69%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,20 +1,18 @@
 class Solution {
-public:
-    char findTheDifference(string s, string t) {
-      //sort(s.begin(),s.end());
-      sort(s.begin(),s.end());
-    
-    sort(t.begin(),t.end());
-    
-    
-    for(int i=0;i<s.length();i++)
-    {
-      if(s[i]!=t[i])
-      {
-        return t[i];
-      }
+    public char findTheDifference(String s, String t) {
+        // char[] chs = s.toCharArray(), cht = t.toCharArray();
+        // int sums = 0, sumt = 0;
+        // for(char c : chs)
+        //     sums += (int)c;
+        // for(char c : cht)
+        //     sumt += (int)c;
+        // return (char)(sumt-sums);
+        
+        int sum = 0;
+        char[] charsS = s.toCharArray();
+        char[] charsT = t.toCharArray();
+        for (int i = 0; i < charsS.length; i++)
+            sum = sum + charsT[i] - charsS[i];
+        return (char) (sum + charsT[charsT.length - 1]);
     }
-    
-    return t[t.length()-1];
-    }
-};
+}
