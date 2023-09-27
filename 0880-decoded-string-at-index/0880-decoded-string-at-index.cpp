ef7cc0d15bf12d4792dc0c948fdc22a7b2c048f6
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-27 07:08:51 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (31.79%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -1,14 +1,22 @@
 class Solution {
-public:
-   string decodeAtIndex(string S, int K) {
-        long N = 0, i;
-        for (i = 0; N < K; ++i)
-            N = isdigit(S[i]) ? N * (S[i] - '0') : N + 1;
-        while (i--)
-            if (isdigit(S[i]))
-                N /= S[i] - '0', K %= N;
-            else if (K % N-- == 0)
-                return string(1, S[i]);
-        return "lee215";
+     public String decodeAtIndex(String S, int K) {
+        int i;
+        long N = 0;
+        for (i = 0; N < K; i++) {
+            N = Character.isDigit(S.charAt(i)) ? N * (S.charAt(i) - '0') : N + 1;
+        }
+        for (i--; i > 0; i--) {
+            if (Character.isDigit(S.charAt(i))) {
+                N /= S.charAt(i) - '0';
+                K %= N;
+            }
+            else {
+                if (K % N == 0) {
+                    break;
+                }
+                N--;
+            }
+        }
+        return Character.toString(S.charAt(i));
     }
-};
+}
