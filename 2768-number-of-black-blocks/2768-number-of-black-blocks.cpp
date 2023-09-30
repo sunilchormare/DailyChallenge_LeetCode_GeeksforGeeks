4afc4592ef8180d9bee172e897f995c97bc888a7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-09-30 15:54:25 +0530

    Time: 123 ms (59.24%), Space: 55.2 MB (47.77%) - LeetHub

--- 2768-number-of-black-blocks/2768-number-of-black-blocks.cpp
+++ 2768-number-of-black-blocks/2768-number-of-black-blocks.cpp
@@ -1,16 +1,16 @@
 class Solution {
-public:
-vector<long long> countBlackBlocks(int m, int n, vector<vector<int>>& coordinates) {
-        unordered_map<long long, int> cnt;
-        vector<long long> res = {(n - 1L) * (m - 1), 0, 0, 0, 0};
-        for (auto& c: coordinates)
-            for (int i = c[0]; i < c[0] + 2; ++i)
-                for (int j = c[1]; j < c[1] + 2; ++j)
+    public long[] countBlackBlocks(int m, int n, int[][] coordinates) {
+        HashMap<Long, Integer> cnt = new HashMap<>();
+        long[] res = {(n - 1L) * (m - 1), 0, 0, 0, 0};
+        for (int[] c : coordinates)
+            for (int i = c[0]; i < c[0] + 2; i++)
+                for (int j = c[1]; j < c[1] + 2; j++)
                     if (0 < i && i < m && 0 < j && j < n) {
-                        res[cnt[i * 100000L + j]]--;
-                        cnt[i * 100000L + j]++;
-                        res[cnt[i * 100000L + j]]++;
+                        long idx = i * 100000L + j;
+                        res[cnt.getOrDefault(idx, 0)]--;
+                        cnt.put(idx, cnt.getOrDefault(idx, 0) + 1);
+                        res[cnt.getOrDefault(idx, 0)]++;
                     }
         return res;
     }
-};
+}
