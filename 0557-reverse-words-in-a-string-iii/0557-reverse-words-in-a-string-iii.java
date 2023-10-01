8d64bc2c32ba25e94314df967ff7a853c71827a4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-10-01 09:54:04 +0530

    Time: 3 ms (98.87%), Space: 44.5 MB (30.04%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public String reverseWords(String s) {
+        char[] ca = s.toCharArray();
+        for (int i = 0; i < ca.length; i++) {
+            if (ca[i] != ' ') {  
+                int j = i;
+                while (j + 1 < ca.length && ca[j + 1] != ' ') 
+                { j++; } 
+                reverse(ca, i, j);
+                i = j;
+            }
+        }
+        return new String(ca);
+    }
+
+    private void reverse(char[] ca, int i, int j) {
+       while(i < j){
+            char tmp = ca[i];
+            ca[i] = ca[j];
+            ca[j] = tmp;
+            i++;j--;
+        }
+    }
+}
