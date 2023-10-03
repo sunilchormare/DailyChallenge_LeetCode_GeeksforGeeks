6852cbd8d3fec52ca215986d5b883768afc97751
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-10-03 06:55:25 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (15.22%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,11 +1,9 @@
 class Solution {
-public:
-    int numIdenticalPairs(vector<int>& nums) 
-    {
-    int res = 0;
-        unordered_map<int, int> count;
-        for (int a: nums) 
+    public int numIdenticalPairs(int[] A) {
+      int res = 0, count[] = new int[101];
+        for (int a: A) {
             res += count[a]++;
+        }
         return res;
     }
-};
+}
