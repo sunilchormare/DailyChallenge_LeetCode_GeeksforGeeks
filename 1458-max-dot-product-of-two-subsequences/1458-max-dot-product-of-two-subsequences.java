6e20598bce49c8ef162ee308b5bcd946012a9eb3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-10-08 10:32:23 +0530

    Time: 13 ms (36.11%), Space: 42.9 MB (86.11%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,18 +1,14 @@
 class Solution {
-public:
-      int maxDotProduct(vector<int>& A, vector<int>& B) {
-        int n = A.size(), m = B.size();
-        vector<vector<int>> dp(n, vector<int>(m));
+     public int maxDotProduct(int[] A, int[] B) {
+        int n = A.length, m = B.length, dp[][] = new int[n][m];
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 dp[i][j] = A[i] * B[j];
-                if (i && j) dp[i][j] += max(dp[i - 1][j - 1], 0);
-                if (i) dp[i][j] = max(dp[i][j], dp[i - 1][j]);
-                if (j) dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+                if (i > 0 && j > 0) dp[i][j] += Math.max(dp[i-1][j-1], 0);
+                if (i > 0) dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                if (j > 0) dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
             }
         }
-        return dp[n - 1][m - 1];
+        return dp[n-1][m-1];
     }
-};
-
-
+}
