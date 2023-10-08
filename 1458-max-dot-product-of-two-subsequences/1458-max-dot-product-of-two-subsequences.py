364992bb43dd65b0f82bb728c9d91e4497d98825
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-10-08 10:35:56 +0530

    Time: 296 ms (25.00%), Space: 15.7 MB (75.00%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -1,15 +1,12 @@
-class Solution {
-     public int maxDotProduct(int[] A, int[] B) {
-        int n = A.length, m = B.length, dp[][] = new int[n][m];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                dp[i][j] = A[i] * B[j];
-                if (i > 0 && j > 0) dp[i][j] += Math.max(dp[i-1][j-1], 0);
-                if (i > 0) dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-                if (j > 0) dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
-            }
-        }
-        return dp[n-1][m-1];
-    }
-}
-
+class Solution(object):
+     def maxDotProduct(self, A, B):
+        n, m = len(A), len(B)
+        dp = [[0] * (m) for i in xrange(n)]
+        for i in xrange(n):
+            for j in xrange(m):
+                dp[i][j] = A[i] * B[j]
+                if i and j: dp[i][j] += max(dp[i - 1][j - 1], 0)
+                if i: dp[i][j] = max(dp[i][j], dp[i - 1][j])
+                if j: dp[i][j] = max(dp[i][j], dp[i][j - 1])
+        return dp[-1][-1]
+        
