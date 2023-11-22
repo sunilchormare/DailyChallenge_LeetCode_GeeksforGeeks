556dff910a75d13fe04fcd08374f79d80278cc26
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-11-22 06:44:14 +0530

    Time: 36 ms (41.53%), Space: 69.2 MB (61.33%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int n = 0, i = 0, maxKey = 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int r = nums.size() - 1; r >= 0; --r) { // The values from the bottom rows are the starting values of the diagonals.
+            for (int c = 0; c < nums.get(r).size(); ++c) {
+                map.putIfAbsent(r + c, new ArrayList<>());
+                map.get(r + c).add(nums.get(r).get(c));
+                maxKey = Math.max(maxKey, r + c);
+                n++;
+            }
+        }
+        int[] ans = new int[n];
+        for (int key = 0; key <= maxKey; ++key) {
+            List<Integer> value = map.get(key);
+            if (value == null) continue;
+            for (int v : value) ans[i++] = v;
+        }
+        return ans;
+    }
+}
