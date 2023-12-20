6eee40ae050feb7ec14ba93e28a39818bc71743d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2023-12-20 08:43:44 +0530

    Time: 31 ms (5.34%), Space: 47.1 MB (29.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,17 +1,30 @@
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& prices, int money) {
+        
+// //         sort(prices.begin(),prices.end());
+// //         for(int i=1;i<prices.size();++i)
+// //         {
+// //             if(prices[i]+prices[i-1]<=money)
+// //             {
+// //                 return (money-(prices[i]+prices[i-1]));
+// //             }
+                
+// //         }
+        
+// //         return money;
+//     }
+// };
+
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        
-        sort(prices.begin(),prices.end());
-        for(int i=1;i<prices.size();++i)
-        {
-            if(prices[i]+prices[i-1]<=money)
-            {
-                return (money-(prices[i]+prices[i-1]));
-            }
-                
+        sort(prices.begin(), prices.end());
+        int minCost = prices[0] + prices[1];
+
+        if (minCost <= money) {
+            return money - minCost;
         }
-        
         return money;
     }
 };
