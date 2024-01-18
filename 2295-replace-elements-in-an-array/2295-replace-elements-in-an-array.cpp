d876c78df7bf0f617fdbf2d9626928691f6055b5
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-01-18 06:07:07 +0530

    Time: 410 ms (38.33%), Space: 141.2 MB (34.00%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& A, vector<vector<int>>& op) {
+    unordered_map<int,int> store;
+        
+    for(int i=0;i<A.size();i++) store[A[i]]=i;
+        
+    for(auto i:op){
+        A[store[i[0]]]= i[1];           //replace value to its index
+        store[i[1]]= store[i[0]];       //update new value with its index
+    }
+    return A;
+} 
+};
