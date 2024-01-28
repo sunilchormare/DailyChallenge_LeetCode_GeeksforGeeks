ac9f5e9eb2f00149ba7626e69195684dba5955e8
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-01-28 10:45:54 +0530

    Time: 280 ms (34.66%), Space: 159.8 MB (7.95%) - LeetHub

--- 1686-stone-game-vi/1686-stone-game-vi.cpp
+++ 1686-stone-game-vi/1686-stone-game-vi.cpp
@@ -5,6 +5,7 @@ public:
         int res[2] = {0, 0}, n = A.size();
         for (int i = 0; i < n; ++i)
             C.push_back({ -A[i] - B[i], A[i], B[i]});
+        
         sort(begin(C), end(C));
         for (int i = 0; i < n; ++i)
             res[i % 2] += C[i][1 + i % 2];
