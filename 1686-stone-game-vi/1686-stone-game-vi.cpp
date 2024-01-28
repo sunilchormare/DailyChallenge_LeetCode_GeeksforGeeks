8b6223ace49f9e465de136f8caa79350593d1117
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-01-28 10:45:01 +0530

    Time: 257 ms (44.32%), Space: 159.8 MB (7.95%) - LeetHub

---
+++ 1686-stone-game-vi/1686-stone-game-vi.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int stoneGameVI(vector<int>& A, vector<int>& B) {
+        vector<vector<int>> C;
+        int res[2] = {0, 0}, n = A.size();
+        for (int i = 0; i < n; ++i)
+            C.push_back({ -A[i] - B[i], A[i], B[i]});
+        sort(begin(C), end(C));
+        for (int i = 0; i < n; ++i)
+            res[i % 2] += C[i][1 + i % 2];
+        return res[0] == res[1] ? 0 : res[0] > res[1] ? 1 : -1;
+    }
+};
