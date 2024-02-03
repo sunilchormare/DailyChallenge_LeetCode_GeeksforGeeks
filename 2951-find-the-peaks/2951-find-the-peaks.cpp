8309f99fb13500a8316d91d7f4f5e615568ea96a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-02-03 08:55:05 +0530

    Time: 3 ms (88.68%), Space: 23.6 MB (8.44%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& m) {
+        
+        vector<int> ans;
+        
+        for(int i=1;i<m.size()-1;++i)
+        {
+            if(m[i]>m[i-1]&&m[i]>m[i+1])
+                ans.push_back(i);
+            
+        }
+        
+        
+        return ans;
+    }
+};
