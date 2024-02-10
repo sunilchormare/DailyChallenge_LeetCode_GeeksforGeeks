daa16a05d85472d926d3a95d4c86a584b1c74dc9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-02-10 09:05:18 +0530

    Time: 2 ms (96.29%), Space: 41.8 MB (43.63%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,25 @@
+class Solution {
+    int count =1;
+    public int countSubstrings(String s) 
+    {
+     if(s.length()==0) 
+        return 0;
+    for(int i=0; i<s.length()-1; i++)
+    {
+        checkPalindrome(s,i,i);     //To check the palindrome of odd length palindromic sub-string
+        checkPalindrome(s,i,i+1);   //To check the palindrome of even length palindromic sub-string
+    }
+    return count;
+}    
+
+private void checkPalindrome(String s, int i, int j)
+{
+    while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j))
+    {    //Check for the palindrome string 
+        count++;    //Increment the count if palindromin substring found
+        i--;    //To trace string in left direction
+        j++;    //To trace string in right direction
+    }
+}   
+    
+}
