0232cbc14eded2f35477bbc503ca005898fc877a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-03-01 09:22:57 +0530

    Time: 6 ms (34.24%), Space: 10.6 MB (8.86%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -3,7 +3,7 @@ public:
     string maximumOddBinaryNumber(string s) {
         sort(s.rbegin(),s.rend());
         for(int i = s.size()-1;i>=0;i--){
-            if(s[i]=='1')swap(s[i],s[s.size()-1]);
+            if(s[i]=='1') swap(s[i],s[s.size()-1]);
         }
         return s;
     }
