3ee448acc01219216411e57a8a7ecff706f45c39
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-03-14 07:46:13 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (76.57%) - LeetHub

---
+++ 0949-largest-time-for-given-digits/0949-largest-time-for-given-digits.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& a) {
+     string ans = ""; int mx = -1, h1 = -1, h2 = -1, m1 = -1, m2 = -1;
+        
+        for(int i=0; i<4; ++i) {
+            if(a[i] > 2) continue;
+            
+            for(int j=0; j<4; ++j) {
+                if(j == i) continue;
+                if(a[i] == 2 && a[j] > 3) continue;
+                
+                for(int k=0; k<4; ++k) {
+                    if(k == j || k == i) continue;
+                    if(a[k] > 5) continue;
+                    
+                    int l = 6-i-j-k;
+                    if(l == k || l == j || l == i) continue;
+
+                    int val = (a[l] + (a[k] * 10)) + (a[j] + (a[i] * 10)) * 60; // value of time in minutes.
+
+                    if(mx < val) {
+                        mx = val;
+                        h1 = a[i], h2 = a[j], m1 = a[k], m2 = a[l];
+                    }
+                }
+            }
+        }
+        
+        if(h1 == -1 || h2 == -1 || m1 == -1 || m2 == -1) return "";
+        
+        ans = to_string(h1) + to_string(h2) + ":" + to_string(m1) + to_string(m2);
+        
+        return ans;   
+    }
+};
