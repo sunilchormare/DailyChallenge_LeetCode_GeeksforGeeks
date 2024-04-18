5ad53663580dc10237da985ef915f1a405c881db
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-04-18 06:33:50 +0530

    Time: 6 ms (37.46%), Space: 8.4 MB (56.70%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.cpp
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -1,17 +1,24 @@
 class Solution {
-     public int countLargestGroup(int n) {
-        int maxFreq = 0, dp[] = new int[n + 1], res = 1;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            dp[i] = i % 10 + dp[i / 10];
-            map.put(dp[i], map.getOrDefault(dp[i], 0) + 1);
+public:
+    map<int,int> mpp ;
+    int sum(int num){
+        int s = 0 ;
+        while(num){
+            s += num % 10 ;
+            num /= 10 ;
         }
-        for (int key : map.keySet()) {
-            if (map.get(key) > maxFreq) {
-                maxFreq = map.get(key);
-                res = 1;
-            } else if (map.get(key) == maxFreq) res++;
-        }
-        return res;
+        return s ;
     }
-}
+    int countLargestGroup(int n) {
+        for(int i = 1 ; i <= n ; ++i ) ++mpp[sum(i)] ;
+        
+        int maxi = 0 ;
+        for(auto &x : mpp) maxi = max(maxi,(int)x.second) ;
+        
+        int ans = 0 ;
+        for(auto &x : mpp) ans += ((int)x.second == maxi) ;
+        return ans ;
+        
+        
+    }
+};
