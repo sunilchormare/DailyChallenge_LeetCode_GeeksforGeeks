2166109a4938577e602c03fba87cefd675c86f54
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-04-29 07:47:22 +0530

    Time: 104 ms (76.61%), Space: 91.2 MB (85.86%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<int>& A, int k) {
+         for (int x: A) {
+            k ^= x;
+        }
+        int res = 0;
+        while (k > 0) {
+            res += (k & 1);
+            k >>= 1;
+        }
+        return res;
+    }
+};
