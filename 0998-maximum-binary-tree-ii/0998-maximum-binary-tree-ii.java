6044578b24bf32c6a18684f056dccf33a820d4bf
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-04-30 06:13:38 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (94.08%) - LeetHub

---
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+          if (root != null && root.val > val) {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
+        TreeNode node = new TreeNode(val);
+        node.left = root;
+        return node;
+    }
+}
