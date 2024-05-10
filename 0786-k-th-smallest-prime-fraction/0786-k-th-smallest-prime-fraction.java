9c8a23024cd27dbb40445198c7cbb6ce5bf05606
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-05-10 07:06:44 +0530

    Time: 230 ms (69.87%), Space: 45.3 MB (66.38%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        // Create a priority queue to store pairs of fractions
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        // Push the fractions formed by dividing each element by
+        // the largest element into the priority queue
+        for (int i = 0; i < arr.length; i++) {
+            pq.offer(new double[] {
+                -1.0 * arr[i] / arr[arr.length - 1], 
+                i, 
+                arr.length - 1
+            });
+        }
+
+        // Iteratively remove the top element (smallest fraction) 
+        // and replace it with the next smallest fraction
+        while (--k > 0) {
+            double[] cur = pq.poll();
+            int numeratorIndex = (int) cur[1];
+            int denominatorIndex = (int) cur[2] - 1;
+            if (denominatorIndex > numeratorIndex) {
+                pq.offer(new double[] {
+                        -1.0 * arr[numeratorIndex] / arr[denominatorIndex], 
+                        numeratorIndex, 
+                        denominatorIndex
+                });
+            }
+        }
+
+        // Retrieve the kth smallest fraction from 
+        // the top of the priority queue
+        double[] result = pq.poll();
+        return new int[]{arr[(int) result[1]], arr[(int) result[2]]};
+    }
+}
