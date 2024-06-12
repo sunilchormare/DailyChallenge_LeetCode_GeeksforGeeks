ccf44c9c63480c760bcbe4ce0fe446eef4b02692
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-06-12 05:52:46 +0530

    Time: 10 ms (91.78%), Space: 45.4 MB (53.57%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.java
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -1,23 +1,25 @@
 class Solution {
-public:
-    string largestPalindromic(string s) {
-        vector<int> v(10);
-        for(char c:s)v[c-'0']++;
-        string ans;
-        int single=-1;
+    public String largestPalindromic(String s) {
+        int[] v=new int[10];
+        for(int i=0;i<s.length();i++){
+            v[(int)s.charAt(i)-48]++;
+        }
+        StringBuffer ans = new StringBuffer();
+        int single = -1;
         for(int i=9;i>=0;i--){
-            if(ans.empty() && i==0) continue;
+            if(ans.length()==0 && i==0)continue;
             while(v[i]>1){
-                ans.push_back(i+'0');
+                ans.append((char)(i+48));
                 v[i]-=2;
             }
-            if(v[i]==1 && single==-1)single=i;
+            if(v[i]==1 && single == -1)single = i;
         }
-        string res=string(ans.rbegin(),ans.rend());
-        if(ans.empty() && single==-1) return "0";
-        if(single!=-1)
-            ans.push_back(single+'0');
-        ans+=res;
-        return ans;
+        if(ans.length()==0 && single==-1)return "0";
+        int i = ans.length()-1;
+        if(single!=-1)ans.append((char)(single+48));
+        for(;i>=0;i--){
+            ans.append(ans.charAt(i));
+        }
+        return ans.toString();
     }
-};
+}
