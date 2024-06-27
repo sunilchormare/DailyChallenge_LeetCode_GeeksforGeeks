d1ae3a2f6bac2b08009ee18e625163ccb70f9c38
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-06-27 06:55:32 +0530

    Time: 4 ms (94.87%), Space: 61 MB (64.10%) - LeetHub

--- 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.java
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.java
@@ -1,16 +1,15 @@
 class Solution {
-public:
-    int maxNonDecreasingLength(vector<int>& A, vector<int>& B) {
-          int res = 1, dp1 = 1, dp2 = 1, n = A.size(), t11, t12, t21, t22;
+    public int maxNonDecreasingLength(int[] A, int[] B) {
+        int res = 1, dp1 = 1, dp2 = 1, n = A.length, t11, t12, t21, t22;
         for (int i = 1; i < n; ++i) {
             t11 =  A[i - 1] <= A[i] ? dp1 + 1 : 1;
             t12 = A[i - 1] <= B[i] ? dp1 + 1 : 1;
             t21 = B[i - 1] <= A[i] ? dp2 + 1 : 1;
             t22 = B[i - 1] <= B[i] ? dp2 + 1 : 1;
-            dp1 = max(t11, t21);
-            dp2 = max(t12, t22);
-            res = max(res, max(dp1, dp2));
+            dp1 = Math.max(t11, t21);
+            dp2 = Math.max(t12, t22);
+            res = Math.max(res, Math.max(dp1, dp2));
         }
         return res;
     }
-};
+}
