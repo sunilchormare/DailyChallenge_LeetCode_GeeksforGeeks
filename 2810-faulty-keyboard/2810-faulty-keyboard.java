29f7ddb468660bf5efca9a494631fbe93788c5db
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-07-05 06:27:05 +0530

    Time: 3 ms (98.64%), Space: 44.7 MB (55.34%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String finalString(String s) {
+         StringBuilder nm=new StringBuilder();
+        for(char c:s.toCharArray())
+        {
+            if(c=='i')
+            {
+                nm.reverse();
+            }
+            else
+            {
+                nm.append(c);
+            }
+        }
+        return nm.toString();
+    
+    }
+}
