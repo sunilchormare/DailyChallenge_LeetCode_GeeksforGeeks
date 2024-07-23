47c077c401eb33846bcac27df472674e45a29311
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-07-23 06:35:37 +0530

    Time: 8 ms (46.12%), Space: 44.4 MB (41.14%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,34 +1,21 @@
 class Solution {
-public:
-    bool static comp(pair<int,int> a,pair<int,int> b)
-    {
-        if(a.second==b.second)
-        return a>b;   
-        else
-        return a.second<b.second;
+   public int[] frequencySort(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int a : nums) 
+        {
+            al.add(a);
+            hm.put(a,hm.getOrDefault(a,0)+1);
+        }
+        Collections.sort(al,(a,b)->{
+             int f1 = hm.get(a);
+            int f2 = hm.get(b);
+            if(f1!=f2) return f1-f2;
+            return b-a;
+         });
+        int i=0;
+        int arr[] = new int[al.size()];
+        for(int val : al) arr[i++] = val;
+        return arr;
     }
-    vector<int> frequencySort(vector<int>& nums) {
-         unordered_map<int,int> mp;
-         for(int i=0;i<nums.size();i++)
-         {
-             mp[nums[i]]++;
-         }
-         vector<pair<int,int> > vec;
-         for(auto pr:mp)
-         {
-             vec.push_back(pr);
-         }
-         sort(vec.begin(),vec.end(),comp);
-         
- vector<int> ans;
-    for(int i=0;i<vec.size();i++)
-    {
-     while(vec[i].second>0)
-     {
-         ans.push_back(vec[i].first);
-         vec[i].second--;
-     }
-    }
-        return ans;
-    }
-};
+}
