6bb9a611c0c61c106c7d2806392e8441d0ae699c
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-07-29 07:22:28 +0530

    Time: 3 ms (25.11%), Space: 7.7 MB (7.47%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.cpp
@@ -1,14 +1,29 @@
-
-public class Solution {
-
-    public String complexNumberMultiply(String a, String b) {
-        String x[] = a.split("\\+|i");
-        String y[] = b.split("\\+|i");
-        int a_real = Integer.parseInt(x[0]);
-        int a_img = Integer.parseInt(x[1]);
-        int b_real = Integer.parseInt(y[0]);
-        int b_img = Integer.parseInt(y[1]);
-        return (a_real * b_real - a_img * b_img) + "+" + (a_real * b_img + a_img * b_real) + "i";
-
+class Solution {
+public:
+    
+    pair <int,int> extract(string s)
+    {
+        int l=s.length(),i=1,a,b;
+        while(i<l&&s[i]!='+')
+            i++;
+        a=stoi(s.substr(0,i));
+        b=stoi(s.substr(i+1));
+        return make_pair(a,b);
     }
-}
+    
+    string complexNumberMultiply(string a, string b) 
+    {
+        if(a=="")
+            return b;
+        if(b=="")
+            return a;
+        auto p1=extract(a);
+        auto p2=extract(b);
+        int x,y;
+        x=p1.first*p2.first-p1.second*p2.second;
+        y=p1.second*p2.first+p1.first*p2.second;
+        string s;
+        s=to_string(x)+"+"+to_string(y)+"i";
+        return s;
+    }
+};
