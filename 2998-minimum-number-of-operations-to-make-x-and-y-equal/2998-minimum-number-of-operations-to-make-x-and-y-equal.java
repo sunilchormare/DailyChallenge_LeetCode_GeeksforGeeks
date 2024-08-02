224b950b93601c590d52c01383169675f6cf0269
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-08-02 09:12:17 +0530

    Time: 1 ms (94.18%), Space: 40.8 MB (89.87%) - LeetHub

---
+++ 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumOperationsToMakeEqual(int x, int y) {
+        if (x <= y)
+            return y - x;
+        int res = x - y;
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 5, y) + 1 + x % 5);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 5 + 1, y) + 1 + 5 - x % 5);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 11, y) + 1 + x % 11);
+        res = Math.min(res, minimumOperationsToMakeEqual(x / 11 + 1, y) + 1 + 11 - x % 11);
+        return res;
+    }
+}
