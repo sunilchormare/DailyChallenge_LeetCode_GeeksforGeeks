67255bbe2f809a048aa60062767ef1c9b21697d7
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-08-04 08:26:43 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (95.00%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
@@ -1,7 +1,6 @@
 class Solution {
-public:
-    int minimumSum(int n, int k) {
-         int a = min(k / 2, n), b = n - a;
+    public int minimumSum(int n, int k) {
+         int a = Math.min(k / 2, n), b = n - a;
         return (1 + a) * a / 2 + (k + k + b - 1) * b / 2;
     }
-};
+}
