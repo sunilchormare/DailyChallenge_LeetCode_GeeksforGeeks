d2d6140fd84afdf93c75c31860946858670b9383
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-08-07 06:27:40 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (92.31%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,32 +1,108 @@
 class Solution {
-    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
-
-public String numberToWords(int num) {
-    if (num == 0) return "Zero";
-
-    int i = 0;
-    String words = "";
-    
-    while (num > 0) {
-        if (num % 1000 != 0)
-    	    words = helper(num % 1000) +THOUSANDS[i] + " " + words;
-    	num /= 1000;
-    	i++;
+public:
+    string one(int num) {
+    switch(num) {
+      case 1: return "One";
+      case 2: return "Two";
+      case 3: return "Three";
+      case 4: return "Four";
+      case 5: return "Five";
+      case 6: return "Six";
+      case 7: return "Seven";
+      case 8: return "Eight";
+      case 9: return "Nine";
     }
-    
-    return words.trim();
-}
+    return "";
+  }
 
-private String helper(int num) {
+  string twoLessThan20(int num) {
+    switch(num) {
+      case 10: return "Ten";
+      case 11: return "Eleven";
+      case 12: return "Twelve";
+      case 13: return "Thirteen";
+      case 14: return "Fourteen";
+      case 15: return "Fifteen";
+      case 16: return "Sixteen";
+      case 17: return "Seventeen";
+      case 18: return "Eighteen";
+      case 19: return "Nineteen";
+    }
+    return "";
+  }
+
+  string ten(int num) {
+    switch(num) {
+      case 2: return "Twenty";
+      case 3: return "Thirty";
+      case 4: return "Forty";
+      case 5: return "Fifty";
+      case 6: return "Sixty";
+      case 7: return "Seventy";
+      case 8: return "Eighty";
+      case 9: return "Ninety";
+    }
+    return "";
+  }
+
+  string two(int num) {
     if (num == 0)
-        return "";
+      return "";
+    else if (num < 10)
+      return one(num);
     else if (num < 20)
-        return LESS_THAN_20[num] + " ";
-    else if (num < 100)
-        return TENS[num / 10] + " " + helper(num % 10);
-    else
-        return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
-}
-}
+      return twoLessThan20(num);
+    else {
+      int tenner = num / 10;
+      int rest = num - tenner * 10;
+      if (rest != 0)
+        return ten(tenner) + " " + one(rest);
+      else
+        return ten(tenner);
+    }
+  }
+
+  string three(int num) {
+    int hundred = num / 100;
+    int rest = num - hundred * 100;
+    string res = "";
+    if (hundred*rest != 0)
+      res = one(hundred) + " Hundred " + two(rest);
+    else if ((hundred == 0) && (rest != 0))
+      res = two(rest);
+    else if ((hundred != 0) && (rest == 0))
+      res = one(hundred) + " Hundred";
+    return res;
+  }
+
+    string numberToWords(int num) {
+        if (num == 0)
+      return "Zero";
+
+    int billion = num / 1000000000;
+    int million = (num - billion * 1000000000) / 1000000;
+    int thousand = (num - billion * 1000000000 - million * 1000000) / 1000;
+    int rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000;
+
+    string result = "";
+    if (billion != 0)
+      result = three(billion) + " Billion";
+    if (million != 0) {
+      if (result.size()!=0)
+        result += " ";
+      result += three(million) + " Million";
+    }
+    if (thousand != 0) {
+      if (result.size()!=0)
+        result += " ";
+      result += three(thousand) + " Thousand";
+    }
+    if (rest != 0) {
+      if (result.size()!=0)
+        result += " ";
+      result += three(rest);
+    }
+    return result;
+    }
+
+};
