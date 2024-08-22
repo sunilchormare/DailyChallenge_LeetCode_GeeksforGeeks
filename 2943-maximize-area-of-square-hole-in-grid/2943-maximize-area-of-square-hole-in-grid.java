ab24dc05e02bc4ae1a85ca48e0e5110fb5f5ffc4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-08-22 07:03:24 +0530

    Time: 2 ms (100.00%), Space: 42.6 MB (93.33%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
@@ -1,16 +1,15 @@
 class Solution {
-public:
-    int getMaxGap(vector<int> bars){
-      sort(bars.begin(), bars.end());
-      int count = 2, res = 2;
-      for(int i = 1; i < bars.size(); ++i) {
-          count = (bars[i-1]+1 == bars[i])? count + 1: 2;
-          res = max(res, count);
-      }
-      return res;
-  }
-    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        int gap = min(getMaxGap(hBars), getMaxGap(vBars));
-      return gap * gap;
+     public int getMaxGap(int[] bars){
+        Arrays.sort(bars);
+        int count = 2, res = 2;
+        for(int i = 1; i < bars.length; ++i){
+            count = (bars[i-1] + 1 == bars[i])? count + 1: 2;
+            res = Math.max(res, count);
+        }
+        return res;
     }
-};
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int gap = Math.min(getMaxGap(hBars), getMaxGap(vBars));
+        return gap * gap;
+    }
+}
