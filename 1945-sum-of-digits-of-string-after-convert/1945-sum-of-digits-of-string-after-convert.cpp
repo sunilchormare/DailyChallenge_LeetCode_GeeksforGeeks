f38d538d25fb520e600334c89060299f3f7b3036
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-09-03 07:43:34 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (94.89%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumDigits(int num) {
+        int sum = 0;
+        while(num) {
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+    
+    int getLucky(string s, int k) {
+        int sum = 0;
+        for(char &ch : s) {
+		    int val = ch-'a'+1;
+		    sum += val < 10 ? val : (val%10 + val/10);
+		}
+
+        k -= 1; //Why ? We already did one transformation above (getting sum of character values)
+        while(k-- && sum >= 10) //sum >= 10 reduces unwanted Transform calls because if we are left with 1-digit,that will be our result
+            sum = sumDigits(sum);
+        
+        return sum;
+    }
+};
