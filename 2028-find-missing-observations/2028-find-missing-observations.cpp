a45f4e50f6d667ca6965275e35a3262b8f220a7e
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-09-05 06:53:04 +0530

    Time: 85 ms (89.46%), Space: 116.4 MB (69.12%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        int curSum = accumulate(rolls.begin(), rolls.end(), 0), m = rolls.size();
-        int missingSum = mean * (n + m) - curSum;
-        if (missingSum < n || missingSum > 6*n) return {};
-        
-        int part = missingSum / n, rem = missingSum % n;
-        vector<int> ans(n, part);
-        for (int i = 0; i < rem; ++i) 
-            ++ans[i];
-        return ans;
+        int sum = 0;
+        for (int i = 0; i < rolls.size(); i++) {
+            sum = sum + rolls[i];
+        }
+        // Find the remaining sum.
+        int remainingSum = mean * (n + rolls.size()) - sum;
+        // Check if sum is valid or not.
+        if (remainingSum > 6 * n or remainingSum < n) {
+            return {};
+        }
+        int distributeMean = remainingSum / n;
+        int mod = remainingSum % n;
+        // Distribute the remaining mod elements in nElements array.
+        vector<int> nElements(n, distributeMean);
+        for (int i = 0; i < mod; i++) {
+            nElements[i]++;
+        }
+        return nElements;
     }
 };
