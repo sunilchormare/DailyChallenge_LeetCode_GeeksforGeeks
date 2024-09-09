7843958e565e608567d11842c0e2faa1a99e798f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-09-09 07:30:37 +0530

    Time: 15 ms (62.50%), Space: 56.4 MB (7.03%) - LeetHub

--- 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
+++ 2567-minimum-score-by-changing-two-elements/2567-minimum-score-by-changing-two-elements.java
@@ -1,8 +1,7 @@
 class Solution {
-public:
-    int minimizeSum(vector<int>& A) {
-        int n = A.size();
-        sort(A.begin(), A.end());
-        return min({A[n - 1] - A[2], A[n - 3] - A[0], A[n - 2] - A[1]});
+    public int minimizeSum(int[] A) {
+        int n = A.length;
+        Arrays.sort(A);
+        return Math.min(Math.min(A[n - 1] - A[2], A[n - 3] - A[0]), A[n - 2] - A[1]);
     }
-};
+}
