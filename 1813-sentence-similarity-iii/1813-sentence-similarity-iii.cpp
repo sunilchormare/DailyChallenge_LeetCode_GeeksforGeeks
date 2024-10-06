348988766b91f81ff276cf1f9863965b29a8cbb9
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-10-06 08:34:13 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (66.36%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -1,16 +1,23 @@
 class Solution {
-     public boolean areSentencesSimilar(String s1, String s2) {
-        String[] words1 = s1.split(" "), words2 = s2.split(" ");
-        int i = 0, n1 = words1.length, n2 = words2.length;
-        if (n1 > n2) {
-            return areSentencesSimilar(s2, s1);
+public:
+    bool areSentencesSimilar(string s1, string s2) {
+        string temp = "";
+        deque<string>a,b;
+        for(char c:s1){
+            if(c == ' ') a.push_back(temp),temp = "";
+            else temp += c;
         }
-        while (i < n1 && words1[i].equals(words2[i])) {
-            ++i;
+        a.push_back(temp),temp = "";
+        for(char c:s2){
+            if(c == ' ') b.push_back(temp),temp = "";
+            else temp += c;
         }
-        while (i < n1 && words1[i].equals(words2[n2 - n1 + i])) {
-            ++i;
-        }
-        return i == n1;
+        b.push_back(temp),temp = "";
+		// removing common from front
+        while(a.size() != 0 && b.size() != 0 && (a.front() == b.front())) a.pop_front(),b.pop_front();
+        //removing common from back
+		while(a.size() != 0 && b.size() != 0 && (a.back() == b.back())) a.pop_back(),b.pop_back();
+		if(a.size() == 0 || b.size() == 0) return true;
+        return false;
     }
-}
+};
