c7075840300947484fa067187b49091e52df1249
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-11-20 09:14:14 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (87.70%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -1,29 +1,8 @@
 class Solution {
-public:
-    int countTestedDevices(vector<int>& A) {
-//         int tested=0;
-//         int n=bP.size();
-        
-//         for(int i=0;i<n;++i)
-//         {
-//             if(bP[i]>0)
-//             {
-//                 tested++;
-//                 int j=i+1;
-//                 while(j<n)
-//                 {
-//                     bP[j]=max(0,bP[j]-1);
-//                     j++;
-//                 }
-                
-//             }
-            
-//         }
-        
-//         return tested;
-         int k = 0;
+    public int countTestedDevices(int[] A) {
+       int k = 0;
         for (int a : A)
-            k += a > k;
+            k += a > k ?  1 : 0;
         return k;
     }
-};
+}
