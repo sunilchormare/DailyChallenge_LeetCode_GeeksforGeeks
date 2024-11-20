46358e234d0e7ddcece0b5a1c9d7c8fb3931f31a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-11-20 09:12:41 +0530

    Time: 0 ms (100.00%), Space: 20.4 MB (100.00%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int countTestedDevices(vector<int>& bP) {
-        int tested=0;
-        int n=bP.size();
+    int countTestedDevices(vector<int>& A) {
+//         int tested=0;
+//         int n=bP.size();
         
-        for(int i=0;i<n;++i)
-        {
-            if(bP[i]>0)
-            {
-                tested++;
-                int j=i+1;
-                while(j<n)
-                {
-                    bP[j]=max(0,bP[j]-1);
-                    j++;
-                }
+//         for(int i=0;i<n;++i)
+//         {
+//             if(bP[i]>0)
+//             {
+//                 tested++;
+//                 int j=i+1;
+//                 while(j<n)
+//                 {
+//                     bP[j]=max(0,bP[j]-1);
+//                     j++;
+//                 }
                 
-            }
+//             }
             
-        }
+//         }
         
-        return tested;
+//         return tested;
+         int k = 0;
+        for (int a : A)
+            k += a > k;
+        return k;
     }
 };
