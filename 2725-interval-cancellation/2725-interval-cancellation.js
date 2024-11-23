d0d892ca4baac759521399ed0bde5edffb15ab33
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-11-23 07:49:28 +0530

    Time: 65 ms (61.88%), Space: 49.4 MB (76.61%) - LeetHub

--- 2725-interval-cancellation/2725-interval-cancellation.js
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -1,20 +1,26 @@
-function cancellable(fn: Function, args: any[], t: number): Function {
-  let timerId = null;
-  fn(...args);
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let timerId = null;
+    fn(...args);
 
-  const startInterval = (): void => {
-    timerId = setTimeout(() => {
-      fn(...args);
-      startInterval();
-    }, t);
-  };
-  startInterval();
+    const startInterval = () => {
+        timerId = setTimeout(() => {
+            fn(...args);
+            startInterval();
+        }, t);
+    };
+    startInterval();
 
-  const cancelInterval: Function = (): void => {
-    if (timerId !== null) {
-      clearTimeout(timerId);
-    }
-  };
+    const cancelInterval = () => {
+        if (timerId !== null) { // not totally needed as clearTimeout is very forgiving fn
+            clearTimeout(timerId);
+        }
+    };
 
-  return cancelInterval;
-}
+    return cancelInterval;
+};
