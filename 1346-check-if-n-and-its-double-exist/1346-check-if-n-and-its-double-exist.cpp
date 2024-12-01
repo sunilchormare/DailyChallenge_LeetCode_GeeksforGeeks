b9999c39d21e99c2f503838a9e6db8a7f2021a8a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-12-01 09:16:30 +0530

    Time: 3 ms (24.95%), Space: 13.8 MB (16.30%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+           unordered_set<int> set;
+            for (int i : arr) {
+                if (set.find(2*i)!=set.end() || i%2 == 0 && set.find(i/2)!=set.end())
+                    return true;
+                set.insert(i);
+            }
+            return false;
+    }
+};
