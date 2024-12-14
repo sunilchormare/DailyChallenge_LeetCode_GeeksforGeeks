6e0bfd250f540fbe4143598f84d498d84850764b
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2024-12-14 09:12:18 +0530

    Time: 86 ms (88.62%), Space: 111.8 MB (91.88%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -1,22 +1,23 @@
-public class Solution {
-    public long continuousSubarrays(int[] nums) {
-        long result = 1;
-        int N = nums.length;
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+      long result = 1;
+        int N = nums.size();
         int l = nums[0] - 2, r = nums[0] + 2;
         int j = 0;
 
         for (int i = 1; i < N; i++) {
             if (nums[i] >= l && nums[i] <= r) {
-                l = Math.max(l, nums[i] - 2);
-                r = Math.min(r, nums[i] + 2);
+                l = max(l, nums[i] - 2);
+                r = min(r, nums[i] + 2);
             }
             else {
                 j = i - 1;
                 l = nums[i] - 2;
                 r = nums[i] + 2;
                 while (nums[j] >= l && nums[j] <= r) {
-                    l = Math.max(l, nums[j] - 2);
-                    r = Math.min(r, nums[j] + 2);
+                    l = max(l, nums[j] - 2);
+                    r = min(r, nums[j] + 2);
                     j--;
                 }
 
@@ -28,5 +29,4 @@ public class Solution {
 
         return result;
     }
-
-}
+};
