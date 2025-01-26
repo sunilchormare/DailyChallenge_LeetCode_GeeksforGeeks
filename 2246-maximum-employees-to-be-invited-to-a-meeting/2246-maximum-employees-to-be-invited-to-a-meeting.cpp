4c894f1bb933b3a9c2efcd8003d807588e8689af
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-01-26 07:39:32 +0530

    Time: 19 ms (82.79%), Space: 92.9 MB (91.8%) - LeetHub

---
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maximumInvitations(vector<int>& favorite) {
+        int n = favorite.size();
+        vector<int> inDegree(n, 0);
+
+        for (int person = 0; person < n; ++person) {
+            inDegree[favorite[person]]++;
+        }
+
+        queue<int> q;
+        for (int person = 0; person < n; ++person) {
+            if (inDegree[person] == 0) {
+                q.push(person);
+            }
+        }
+
+        vector<int> depth(n, 1);  
+        while (!q.empty()) {
+            int currentNode = q.front();
+            q.pop();
+            int nextNode = favorite[currentNode];
+            depth[nextNode] = max(depth[nextNode], depth[currentNode] + 1);
+            if (--inDegree[nextNode] == 0) {
+                q.push(nextNode);
+            }
+        }
+
+        int longestCycle = 0;
+        int twoCycleInvitations = 0;
+
+        for (int person = 0; person < n; ++person) {
+            if (inDegree[person] == 0) continue;  // Already processed
+
+            int cycleLength = 0;
+            int current = person;
+            while (inDegree[current] != 0) {
+                inDegree[current] = 0;  // Mark as visited
+                cycleLength++;
+                current = favorite[current];
+            }
+
+            if (cycleLength == 2) {
+                twoCycleInvitations += depth[person] + depth[favorite[person]];
+            } else {
+                longestCycle = max(longestCycle, cycleLength);
+            }
+        }
+
+        return max(longestCycle, twoCycleInvitations);
+    }
+};
