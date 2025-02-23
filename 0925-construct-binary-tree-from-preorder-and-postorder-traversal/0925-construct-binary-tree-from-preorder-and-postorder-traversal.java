0344488f0f4640532129711531ff66d69c91cad2
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-02-23 07:39:17 +0530

    Time: 1 ms (53.87%), Space: 43.8 MB (20.09%) - LeetHub

---
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+         Deque<TreeNode> s = new ArrayDeque<>();
+        s.offer(new TreeNode(pre[0]));
+        for (int i = 1, j = 0; i < pre.length; ++i) {
+            TreeNode node = new TreeNode(pre[i]);
+            while (s.getLast().val == post[j]) {
+                s.pollLast(); j++;
+            }
+            if (s.getLast().left == null) s.getLast().left = node;
+            else s.getLast().right = node;
+            s.offer(node);
+        }
+        return s.getFirst();
+    }
+}
