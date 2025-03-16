8aaa3bc9afdf3e203bf4f5cd60e1601c86ed4190
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-03-16 18:34:21 +0530

    Time: 55 ms (48.92%), Space: 56.4 MB (43.82%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -0,0 +1,15 @@
+class Solution {
+  public long repairCars(int[] A, int cars) {
+        long left = 1, right = 1L * A[0] * cars * cars;
+        while (left < right) {
+            long mid = (left + right) / 2, cur = 0;
+            for (int a : A)
+                cur += (long)(Math.sqrt(1.0 * mid / a));
+            if (cur < cars)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
