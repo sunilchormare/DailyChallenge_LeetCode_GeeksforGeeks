21acec4096061310f4d4f62db9f848d07fbc4133
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-09-01 07:47:04 +0530

    Time: 530 ms (13.99%), Space: 75.5 MB (94.3%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Cmp());
+        for(int[] c: classes) queue.offer(c);   // add all to priority queue
+        for(;extraStudents > 0;extraStudents--) {    // add extra student to class that will max increase the average
+            int[] c = queue.poll();
+            c[0]++; c[1]++;
+            queue.offer(c);
+        }
+        double result =0;
+        while(!queue.isEmpty()) {       // Calculate sum of pass ratios
+            int[] c = queue.poll();
+            result += (double)c[0]/c[1];
+        }
+        return result/classes.length;    // return average
+    }
+}
+class Cmp implements Comparator<int[]> {  // to sort in descending order of diff when 1 is added to class
+    public int compare(int[] x, int[] y) {
+        double xDiff = (double)(x[0]+1)/(x[1]+1) - (double)x[0]/x[1], yDiff = (double)(y[0]+1)/(y[1]+1) - (double)y[0]/y[1];
+        return xDiff > yDiff? -1 : 1;
+    }
+}
