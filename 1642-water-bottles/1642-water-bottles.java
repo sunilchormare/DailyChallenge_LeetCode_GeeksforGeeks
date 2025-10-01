3be2d8205ad52f4f1fc99a9d97585a2a300b7002
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-10-01 08:46:15 +0530

    Time: 0 ms (100%), Space: 40.3 MB (71.39%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+         int initial = numBottles;
+        while(numBottles >= numExchange){
+
+            initial += (numBottles/numExchange);
+            numBottles= (numBottles/numExchange) + (numBottles % numExchange);
+            
+        }
+        return initial;
+    }
+}
