84d783f5bb49700303f572854718b7d9ef0ec59a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-11-13 08:05:05 +0530

    Time: 7 ms (78.52%), Space: 47.8 MB (7.41%) - LeetHub

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public int maxOperations(String s) {
+        int countOne = 0;
+        int ans = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '0') {
+                while (i + 1 < s.length() && s.charAt(i + 1) == '0') {
+                    i++;
+                }
+                ans += countOne;
+            } else {
+                countOne++;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
