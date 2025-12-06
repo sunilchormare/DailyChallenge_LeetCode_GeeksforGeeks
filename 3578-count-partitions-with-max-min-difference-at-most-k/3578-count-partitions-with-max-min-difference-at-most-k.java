4c00ab02068778c26909c5c64dbef6af31831bb1
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2025-12-06 06:31:47 +0530

    Time: 221 ms (6.17%), Space: 86.8 MB (6.17%) - LeetHub

---
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int countPartitions(int[] nums, int k) {
+        int n = nums.length;
+        long mod = (long) 1e9 + 7;
+        long[] dp = new long[n + 1];
+        long[] prefix = new long[n + 1];
+        TreeMap<Integer, Integer> cnt = new TreeMap<>();
+
+        dp[0] = 1;
+        prefix[0] = 1;
+        for (int i = 0, j = 0; i < n; i++) {
+            cnt.put(nums[i], cnt.getOrDefault(nums[i], 0) + 1);
+            // adjust window
+            while (j <= i && cnt.lastKey() - cnt.firstKey() > k) {
+                cnt.put(nums[j], cnt.get(nums[j]) - 1);
+                if (cnt.get(nums[j]) == 0) {
+                    cnt.remove(nums[j]);
+                }
+                j++;
+            }
+
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        }
+
+        return (int) dp[n];
+    }
+}
