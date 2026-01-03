5b71d9874e57a4dd3302bfed9f2871f5f6dfef6d
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-01-03 18:32:39 +0530

    Time: 3 ms (93.85%), Space: 41.9 MB (91.79%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+       public int numOfWays(int n) {
+        long a121 = 6, a123 = 6, b121, b123, mod = (long)1e9 + 7;
+        for (int i = 1; i < n; ++i) {
+            b121 = a121 * 3 + a123 * 2;
+            b123 = a121 * 2 + a123 * 2;
+            a121 = b121 % mod;
+            a123 = b123 % mod;
+        }
+        return (int)((a121 + a123) % mod);
+    }
+}
