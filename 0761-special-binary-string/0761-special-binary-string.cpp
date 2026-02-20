0e89859fb1a3a70bb809d5345c2e71b3a20f52a1
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-02-20 07:33:29 +0530

    Time: 0 ms (100%), Space: 9.1 MB (90.83%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        
+        if(s.length()==0)
+            return ""; //return null string if size is zero
+
+        vector<string> ans; //list to store all current special substrings
+        int count=0,i=0; //keep track of special substring starting index using "i" and 
+                         //"count" to keep the track of special substring is over or not
+
+        for(int j=0;j<s.size();j++){
+            if(s[j] == '1')
+                count++;
+            else
+                count--;
+            
+            if(count==0){
+                //call recursively using mid special substring
+
+                ans.push_back('1' + makeLargestSpecial(s.substr(i+1,j-i-1)) + '0');
+                i = j+1;
+            }
+        }
+        //sort current substring stored list to fulfill the question demand
+
+        sort(ans.begin(),ans.end(),greater<string>());
+        string finalString = "";
+        for(i=0;i<ans.size();i++){
+            finalString += ans[i];
+        }
+        return finalString;
+    }
+};
