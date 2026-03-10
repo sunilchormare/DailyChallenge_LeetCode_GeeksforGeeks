3ba5686f70088efacc08440c62578be18a2731e4
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-03-10 07:54:28 +0530

    Time: 424 ms (5.75%), Space: 232.2 MB (72.41%) - LeetHub

---
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    static final int MOD = 1000000007;
+    int[][][] memo;
+    int limit;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        this.memo = new int[zero + 1][one + 1][2];
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+        this.limit = limit;
+        return (dp(zero, one, 0) + dp(zero, one, 1)) % MOD;
+    }
+
+    public int dp(int zero, int one, int lastBit) {
+        if (zero == 0) {
+            return (lastBit == 0 || one > limit) ? 0 : 1;
+        } else if (one == 0) {
+            return (lastBit == 1 || zero > limit) ? 0 : 1;
+        }
+
+        if (memo[zero][one][lastBit] == -1) {
+            int res = 0;
+            if (lastBit == 0) {
+                res = (dp(zero - 1, one, 0) + dp(zero - 1, one, 1)) % MOD;
+                if (zero > limit) {
+                    res = (res - dp(zero - limit - 1, one, 1) + MOD) % MOD;
+                }
+            } else {
+                res = (dp(zero, one - 1, 0) + dp(zero, one - 1, 1)) % MOD;
+                if (one > limit) {
+                    res = (res - dp(zero, one - limit - 1, 0) + MOD) % MOD;
+                }
+            }
+            memo[zero][one][lastBit] = res % MOD;
+        }
+        return memo[zero][one][lastBit];
+    }
+}
