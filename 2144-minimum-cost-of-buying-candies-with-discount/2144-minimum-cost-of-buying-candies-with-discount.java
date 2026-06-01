c893cea9e1546b8f084619c76ddf6682c8bd4cdd
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-06-01 07:37:02 +0530

    Time: 6 ms (41.78%), Space: 44.8 MB (17.37%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int res = 0;
+        int n = cost.length;
+        for (int i = n - 1; i >= 0; --i) {
+            if ((n - 1 - i) % 3 != 2) {
+                res += cost[i];
+            }
+        }
+        return res;
+    }
+}
