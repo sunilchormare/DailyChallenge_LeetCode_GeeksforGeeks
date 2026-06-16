a9b7222f95c47c6e1fb2c30fb5c96a1829f3cd54
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-06-16 06:38:06 +0530

    Time: 4 ms (76.47%), Space: 55.3 MB (58.82%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (result.length() > 0) result.deleteCharAt(
+                    result.length() - 1
+                );
+            } else if (ch == '#') {
+                result.append(result.toString());
+            } else if (ch == '%') {
+                result.reverse();
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
