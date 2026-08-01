446eeeb287a4cb7db0c41226befbd05cc965fddd
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-08-01 06:21:19 +0530

    Time: 64 ms (29.16%), Space: 42 MB (99.84%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,19 +1,16 @@
 class Solution {
-    public boolean PredictTheWinner(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = nums[i];
-        }
-        
-        for (int diff = 1; diff < n; ++diff) {
-            for (int left = 0; left < n - diff; ++left) {
-                int right = left + diff;
-                dp[left][right] = Math.max(nums[left] - dp[left + 1][right],
-                                          nums[right] - dp[left][right - 1]);
-            }
-        }
-        
-        return dp[0][n - 1] >= 0;
+    private int helper(int[] nums, int left, int right) {
+        if (left == right) return nums[left];
+        return Math.max(nums[left] - helper(nums, left + 1, right), 
+                        nums[right] - helper(nums, left, right - 1));
+    }
+    public boolean predictTheWinner(int[] nums) {
+        return helper(nums, 0, nums.length - 1) >= 0;
     }
 }
+
+/*
+Complexity:
+Time: O(2^n)
+Space: O(n) (recursion stack depth)
+*/
