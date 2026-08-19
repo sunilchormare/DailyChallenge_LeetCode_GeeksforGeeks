ad083088855da75456b44931df6b40a0276d1deb
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-08-19 08:12:24 +0530

    Time: 47 ms (60.21%), Space: 115.5 MB (94.61%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,32 +1,27 @@
+import java.util.Arrays;
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int count=0,n=intervals.length,left=0,right=1;
-        Arrays.sort(intervals,(a,b) -> a[1]-b[1]);
-        // for(int []i :intervals)
-        // {
-        //     System.out.println("{a,b} "+" { "+i[0]+","+i[1]+" } ");
-        // }
-        while(right<n)
-        {
-            if(intervals[left][1]<=intervals[right][0])
-            {
-                left=right;
-                right++;
-            }
-            else if(intervals[left][1]<=intervals[right][1])
-            {
-                count++;
-                right++;
-            }
-            else if(intervals[left][1]>intervals[right][1])
-            {
-                count++;
-                left=right;
-                right++;
-            }
-            
+        if (intervals == null || intervals.length == 0) {
+            return 0;
         }
-        
-        return count;
+
+        // Sort intervals based on their end times
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int removals = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            // If current interval starts before the previous accepted interval ends, it's an overlap
+            if (intervals[i][0] < prevEnd) {
+                removals++;
+            } else {
+                // No overlap, update the reference end time
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return removals;
     }
 }
