e053fbc98754cd24b11427407fd9104289fec061
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-08-30 11:21:46 +0530

    Time: 128 ms (5.04%), Space: 54.5 MB (50.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -5,21 +5,54 @@
 //     return Arrays.copyOfRange(points, 0, K);
 // }
 // }
+//MAX HEAP
+// class Solution{
+// public int[][] kClosest(int[][] points, int K) {
+//     PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+//     for (int[] p : points) {
+//         pq.offer(p);
+//         if (pq.size() > K) {
+//             pq.poll();
+//         }
+//     }
+//     int[][] res = new int[K][2];
+//     while (K > 0) {
+//         res[--K] = pq.poll();
+//     }
+//     return res;
+// }
 
+// }
+
+//QUICK SELECT
 class Solution{
 public int[][] kClosest(int[][] points, int K) {
-    PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
-    for (int[] p : points) {
-        pq.offer(p);
-        if (pq.size() > K) {
-            pq.poll();
+    int len =  points.length, l = 0, r = len - 1;
+    while (l <= r) {
+        int mid = helper(points, l, r);
+        if (mid == K) break;
+        if (mid < K) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
         }
     }
-    int[][] res = new int[K][2];
-    while (K > 0) {
-        res[--K] = pq.poll();
-    }
-    return res;
+    return Arrays.copyOfRange(points, 0, K);
 }
 
+private int helper(int[][] A, int l, int r) {
+    int[] pivot = A[l];
+    while (l < r) {
+        while (l < r && compare(A[r], pivot) >= 0) r--;
+        A[l] = A[r];
+        while (l < r && compare(A[l], pivot) <= 0) l++;
+        A[r] = A[l];
+    }
+    A[l] = pivot;
+    return l;
+}
+
+private int compare(int[] p1, int[] p2) {
+    return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+}
 }
