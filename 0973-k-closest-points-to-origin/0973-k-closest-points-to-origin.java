051d350b8ee4184074cd17d7f8b30fcab34a1d1f
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-08-30 11:13:36 +0530

    Time: 29 ms (71.35%), Space: 53.9 MB (61.22%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,7 +1,25 @@
-//BRUTE FORCE
-class Solution {
+// //BRUTE FORCE
+// class Solution {
+// public int[][] kClosest(int[][] points, int K) {
+//     Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
+//     return Arrays.copyOfRange(points, 0, K);
+// }
+// }
+
+class Solution{
 public int[][] kClosest(int[][] points, int K) {
-    Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
-    return Arrays.copyOfRange(points, 0, K);
+    PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+    for (int[] p : points) {
+        pq.offer(p);
+        if (pq.size() > K) {
+            pq.poll();
+        }
+    }
+    int[][] res = new int[K][2];
+    while (K > 0) {
+        res[--K] = pq.poll();
+    }
+    return res;
 }
+
 }
