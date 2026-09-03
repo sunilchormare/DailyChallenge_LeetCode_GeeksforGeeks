8a1c9e62341e7285f85351da14d72105c633ee23
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-09-03 06:54:14 +0530

    Time: 3 ms (91.3%), Space: 121.9 MB (30.44%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public boolean uniformArray(int[] nums1) {
+        int mn = nums1[0];
+        boolean hasOdd = false;
+        for (int v : nums1) {
+            if (v < mn) {
+                mn = v;
+            }
+            if ((v & 1) == 1) {
+                hasOdd = true;
+            }
+        }
+        if ((mn & 1) == 1) {
+            return true;
+        }
+        return !hasOdd;
+    }
+}
