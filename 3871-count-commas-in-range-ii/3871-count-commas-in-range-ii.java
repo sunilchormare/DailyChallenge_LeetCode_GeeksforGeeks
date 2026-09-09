541f140e5dff69787310eb128db801a273d192d3
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-09-09 06:15:45 +0530

    Time: 1 ms (99.36%), Space: 43 MB (11.54%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    public long countCommas(long n) {
+        long p = 1000, res = 0;
+        while (p <= n) {
+            res += n - p + 1;
+            p *= 1000;
+        }
+        return res;
+    }
+}
