bab515a0c6608543fb9ada019275d0f57f14ea3a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-09-26 20:04:19 +0530

    Time: 3 ms (89.3%), Space: 47.1 MB (16.68%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
@@ -1,20 +1,33 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        if (startFuel >= target) return 0;
-        Queue<Integer> queue = new PriorityQueue<>((a,b) -> b-a);
-        int i = 0, n = stations.length, stops = 0, maxDistance = startFuel;
-        while (maxDistance < target)
-        {
-            while (i < n && stations[i][0] <= maxDistance) {
-                queue.offer(stations[i++][1]);
+        // Max-heap to store the fuel of stations we have bypassed
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int currentReach = startFuel;
+        int stops = 0;
+        int index = 0;
+        
+        // We simulate the journey until our reach is >= the target
+        while (currentReach < target) {
+            // While there are stations within our current reach, add their fuel to the heap
+            while (index < stations.length && stations[index][0] <= currentReach) {
+                maxHeap.offer(stations[index][1]);
+                index++;
             }
             
-            if (queue.isEmpty()) return -1;
+            // If we can't reach the target and have no stations to refuel from, it's impossible
+            if (maxHeap.isEmpty()) {
+                return -1;
+            }
             
-            maxDistance += queue.poll();
-            
+            // Retrospectively refuel from the station with the most fuel
+            currentReach += maxHeap.poll();
             stops++;
         }
+        
         return stops;
     }
 }
