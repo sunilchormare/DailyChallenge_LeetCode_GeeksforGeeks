c0ea48096244b3f9723457306280af8c8d247d2a
Sunil Chormare <66204436+sunilchormare@users.noreply.github.com>
2026-09-26 20:49:54 +0530

    Time: 0 ms (100%), Space: 20.2 MB (51.51%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -3,7 +3,6 @@ public:
     int minRefuelStops(int target, int startFuel,
                        vector<vector<int>>& stations) {
 
-        // Max heap.
         priority_queue<int> maxHeap;
 
         long long fuel = startFuel;
@@ -15,29 +14,22 @@ public:
             int position = station[0];
             int stationFuel = station[1];
 
-            // Fuel consumed to reach this station.
             fuel -= position - previousPosition;
 
-            // If we cannot reach this station,
-            // use the largest previously available fuel.
             while (fuel < 0 && !maxHeap.empty()) {
                 fuel += maxHeap.top();
                 maxHeap.pop();
                 stops++;
             }
 
-            // Still cannot reach the station.
             if (fuel < 0) {
                 return -1;
             }
 
-            // We reached the station.
             maxHeap.push(stationFuel);
-
             previousPosition = position;
         }
 
-        // Try to reach destination.
         fuel -= target - previousPosition;
 
         while (fuel < 0 && !maxHeap.empty()) {
